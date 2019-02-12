3eb03fbfaf9f02996a8c9841d1a05cdd04160cb6
JJGonzalez <jesusjuangonzalez@gmail.com>
2019-02-12 10:52:10 +0100

    Component arch Implementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
+import { Router } from "react-router-dom";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
