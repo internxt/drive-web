b3f010355ac918d631ea082c25d42284d860e25d
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2019-03-05 12:08:50 +0100

    Activity Indicator component

---
+++ src/components/ActivityIndicator.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ActivityIndicator.css';
+
+import image from "../assets/Dashboard-Icons/Uploading.svg"
+
+const ActivityIndicator = props =>
+    <img className="ai-animation" src={image} />
+    ;
+
+export default ActivityIndicator;
