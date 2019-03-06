09681a9ec5db03a7fb40249e6715dd3672d062ff
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2019-03-06 18:51:41 +0100

    Progress bar usage

--- src/components/Plans.js
+++ src/components/Plans.js
@@ -10,11 +10,16 @@ class Plans extends React.Component {
         super(props);
 
         this.state = {
-            PlanDetails: []
+            PlanDetails: [],
+            barLimit: 1024* 1024* 1024,
+            barUsage: 0
         }
     }
 
     componentDidMount() {
+
+        // Load current plans
+
         fetch('/api/plans', {
             method: 'post'
         }).then(response => {
@@ -24,6 +29,43 @@ class Plans extends React.Component {
                 PlanDetails: pay
             });
         });
+
+        // Load current usage
+
+        let user = JSON.parse(localStorage.xUser).email;
+
+
+
+        fetch('/api/limit', {
+            method: 'post',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({
+                email: user
+            })
+        }
+        ).then(res => {
+            return res.json();
+        }).then(res2 => {
+            this.setState({ barLimit: res2.total })
+        }).catch(err => {
+            console.log(err);
+        });
+
+        fetch('/api/usage', {
+            method: 'post',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({
+                email: user
+            })
+        }
+        ).then(res => {
+            return res.json();
+        }).then(res2 => {
+            this.setState({ barUsage: res2.total })
+        }).catch(err => {
+            console.log(err);
+        });
+
     }
 
 
@@ -33,7 +75,7 @@ class Plans extends React.Component {
                 <Container className="mt-5" style={{ maxWidth: '784px' }}>
                     <h2><strong>Storage Space</strong></h2>
                     <p color="#404040" className="mt-3">Used storage space</p>
-                    <ProgressBar now={60} />
+                    <ProgressBar now={this.state.barUsage} max={this.state.barLimit}  />
 
                     <Row className="mt-3">
                         <Col xs={12} md={6} sm={6}>
