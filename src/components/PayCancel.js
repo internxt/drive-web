e276c82bcf6b499db08cb7a15a34f4735c004289
Alberto GT <45895462+JAlbertoGonzalez@users.noreply.github.com>
2019-09-06 09:31:49 +0200

    privisional payment endpoints

---
+++ src/components/PayCancel.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class PayCancel extends React.Component {
+    render() {
+        return <div>Pay Cancel</div>
+    }
+}
+
+export default PayCancel;
---
+++ src/components/PaySuccess.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class PaySucess extends React.Component {
+    render() {
+        return <div>Pay Success</div>
+    }
+}
+
+export default PaySucess;
