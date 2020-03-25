5b996a5ab4fdc9c601ce75676b415f57e6df24ba
Gonzalo Tormo <gonzalotormobas@gmail.com>
2020-03-25 11:02:27 +0100

    marked erase text on forgot password

--- src/components/forms/Remove.tsx
+++ src/components/forms/Remove.tsx
@@ -93,7 +93,7 @@ class Remove extends React.Component<RemoveProps, RemoveState> {
         const isValid = this.validateForm();
         return (<div className="container-register">
             <p className="container-title">X Cloud Security</p>
-            <p className="privacy-disclaimer">As specified during the sign up process, X Cloud encrypts your files, and only you have access to those. We never know your password, and thus, that way, only you can decrypt your account. For that reason, if you forget your password, we can't restore your account. What we can do, however, is to delete your account and erase all its files, so that you can sign up again. Please enter your email below so that we can process the account removal.</p>
+            <p className="privacy-disclaimer">As specified during the sign up process, X Cloud encrypts your files, and only you have access to those. We never know your password, and thus, that way, only you can decrypt your account. For that reason, if you forget your password, we can't restore your account. What we can do, however, is to <span style={{ fontWeight: 'bold' }}>delete your account and erase all its files</span>, so that you can sign up again. Please enter your email below so that we can process the account removal.</p>
 
             <Form>
                 <Form.Row style={{ paddingTop: '5px' }}>
