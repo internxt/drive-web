a646800806a8c8f59823c8911044d6eba67c5dcf
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2020-04-05 13:50:16 +0000

    Redirect cloud to drive temporally

--- src/index.tsx
+++ src/index.tsx
@@ -12,4 +12,11 @@ ReactDOM.render(<Router history={history}><App /></Router>, document.getElementB
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+if (window.location.hostname === 'cloud.internxt.com') {
+    localStorage.clear();
+    window.location.href = 'https://drive.internxt.com'
+    serviceWorker.unregister();
+}
+else {
+    serviceWorker.register();
+}
