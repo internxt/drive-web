f46e6a5904806d9e2f82a6c16e0cbe6610aefb94
Gonzalo Tormo <gonzalotormobas@gmail.com>
2020-04-22 12:31:26 +0200

    Multi link to share added

--- src/components/PopupShare.tsx
+++ src/components/PopupShare.tsx
@@ -19,7 +19,8 @@ interface PopupShareProps {
 
 class PopupShare extends React.Component<PopupShareProps> {
     state = {
-        link: null
+        link: null,
+        views: 1
     }
 
     generateShortLink = (url: string) => {
@@ -32,13 +33,14 @@ class PopupShare extends React.Component<PopupShareProps> {
         });
     }
 
-    generateShareLink = (fileId: string) => {
+    generateShareLink = (fileId: string, views: Number) => {
         return new Promise((resolve, reject) => {
             fetch(`/api/storage/share/file/${fileId}`, {
                 method: 'POST',
                 headers: getHeaders(true, true),
                 body: JSON.stringify({
-                    'isFolder': this.props.item.isFolder ? 'true' : 'false'
+                    'isFolder': this.props.item.isFolder ? 'true' : 'false',
+                    'views': views
                 })
             }).then((res: Response) => {
                 if (res.status !== 200) { throw res }
@@ -56,7 +58,7 @@ class PopupShare extends React.Component<PopupShareProps> {
     componentDidMount() {
         let fileId = this.props.item.isFolder ? this.props.item.id : this.props.item.fileId;
 
-        this.generateShareLink(fileId).then(link => {
+        this.generateShareLink(fileId, 1).then(link => {
             this.setState({ link: link });
         }).catch((err) => {
             if (err.status === 402) {
@@ -90,7 +92,40 @@ class PopupShare extends React.Component<PopupShareProps> {
                 </div>
 
                 <div className="ShareBody">
-                    <div>Share your Drive {this.props.item.isFolder ? 'folder' : 'file'} with this single-use private link</div>
+                    <div>
+                        Share your Drive {this.props.item.isFolder ? 'folder' : 'file'} with this private link. Or enter 
+                        the number of times you'd like the link to be valid:&nbsp;&nbsp;
+                        <input type="text" defaultValue={this.state.views} size={2} style={{
+                            display: 'inline',
+                            border: 'none',
+                            color: '#5c6066',
+                            backgroundColor: '#f8f9fa',
+                            textAlign: 'center',
+                            borderRadius: '3.9px'}} onKeyUp={(e: React.FormEvent<HTMLInputElement>) => {
+                                
+                                if (/^[1-9]$/.test(e.currentTarget.value)) {
+                                    let fileId = this.props.item.isFolder ? this.props.item.id : this.props.item.fileId;
+
+                                    this.generateShareLink(
+                                        fileId,
+                                        parseInt(e.currentTarget.value)
+                                    ).then(link => {
+                                        this.setState({ link: link });
+                                    }).catch((err) => {
+                                        if (err.status === 402) {
+                                            let itemType = this.props.item.isFolder ? 'older' : 'ile';
+                                            this.setState({ link: 'Unavailable link' });
+                                            toast.warn(`F${itemType} too large.\nYou can only share f${itemType}s of up to 200 MB through the web app`);
+                                        }
+                                    });   
+                                } else {
+                                    if (e.currentTarget.value != "") {
+                                        e.currentTarget.value = "1";
+                                    }
+                                }
+                            }}>
+                        </input>
+                    </div>
                 </div>
 
                 <div className="ShareFooter">
