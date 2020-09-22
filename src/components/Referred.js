8af96f955d8fa7df1fd36274fe255e7ea2e4ec3f
DarthegX <44780141+DarthegX@users.noreply.github.com>
2020-09-22 12:20:32 +0200

    Share button actions

--- src/components/Referred.js
+++ src/components/Referred.js
@@ -15,12 +15,16 @@ class Referred extends React.Component {
                 <div></div>
                 <Container className="social-share">
                     <Button className="social-button" type="button" 
-                            href="https://twitter.com/intent/tweet?text=Still havent known @Internxt? Register with my link and get benefits with me https://internxt.com/"
+                            // TODO: Change generated user referer link
+                            href="https://twitter.com/intent/tweet?text=Still havent known @Internxt? Register with my link and get benefits https://internxt.com/"
                             data-size="large"
                             data-url="https://internxt.com/"
                             data-lang="en"></Button>
-                    <Button className="social-button" type="button"></Button>
-                    <Button className="social-button" type="button"></Button>
+                    <Button className="social-button" type="button"
+                            // TODO: Change generated user referer link
+                            href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Finternxt.com%2F&amp;src=sdkpreparse&quote=Still havent known @Internxt? Register with my link and get benefits" target="_blank"></Button>
+                    <Button className="social-button" type="button"
+                            href="https://t.me/share/url?url=https://internxt.com/&text=Still havent known @Internxt? Register with my link and get benefits"></Button>
                 </Container>
                 <div></div>
                 <div className="user-credit">You have accumulated 15 €</div>
