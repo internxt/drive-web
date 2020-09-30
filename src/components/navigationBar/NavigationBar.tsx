02fee27f625ef7f8e7910d30a30f72a091cb0f18
Elena <elgarcan@inf.upv.es>
2020-09-30 12:37:24 +0200

    invite fix

--- src/components/navigationBar/NavigationBar.tsx
+++ src/components/navigationBar/NavigationBar.tsx
@@ -140,7 +140,7 @@ class NavigationBar extends React.Component<NavigationBarProps, NavigationBarSta
                                 <Dropdown.Item onClick={(e) => { history.push('/storage'); }}>Storage</Dropdown.Item>
                                 <Dropdown.Item onClick={(e) => { history.push('/settings'); }}>Settings</Dropdown.Item>
                                 <Dropdown.Item onClick={(e) => { history.push('/security'); }}>Security</Dropdown.Item>
-                                <Dropdown.Item onClick={(e) => { history.push('/Invite'); }}>Referrals</Dropdown.Item>
+                                <Dropdown.Item onClick={(e) => { history.push('/invite'); }}>Referrals</Dropdown.Item>
                                 <Dropdown.Item onClick={(e) => { 
                                     function getOperatingSystem() {
                                         let operatingSystem = 'Not known';
