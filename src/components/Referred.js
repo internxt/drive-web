0db3f74879cb9d37eb5f45973f5e693dd95f9c4c
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2020-10-07 12:38:41 +0200

    Referred styles fix

--- src/components/Referred.js
+++ src/components/Referred.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Container, Dropdown, DropdownButton } from 'react-bootstrap';
+import { Button, Container, Dropdown, DropdownButton, Form } from 'react-bootstrap';
 import NavigationBar from './navigationBar/NavigationBar';
 import './Referred.scss';
 import { getHeaders } from '../lib/auth';
@@ -127,60 +127,62 @@ class Referred extends React.Component {
         return <div>
             <NavigationBar navbarItems={<h5>Referrals</h5>} showSettingsButton={true} />
             <div className="Referred">
-                <Container className="referred-box">
+                <Container className="referred-box p-5">
                     <div className="referred-title">Earn money by referring friends</div>
-                    <div className="referred-description">Invite friends who aren't on Internxt yet. You'll both get €5 of Internxt credit as soon as they activate their account. Start earning money today!</div>
+                    <div className="referred-description py-3">Invite friends who aren't on Internxt yet. You'll both get €5 of Internxt credit as soon as they activate their account. Start earning money today!</div>
 
-                    <Container className="mail-container">
-                        <div>
-                            <input className="mail-box" type="email" placeholder="example@example.com" value={this.state.email} onChange={this.handleEmailChange} />
+                    <Container className="mail-container mt-3">
+                        <div className="row">
+                            <div className="col-10 pl-0">
+                                <Form.Control className="mail-box" type="email" placeholder="example@example.com" value={this.state.email} onChange={this.handleEmailChange} />
+                            </div>
+                            <Button className="invite-button col-2" type="button" onClick={() => {
+                                const mail = this.state.email;
+                                if (mail !== undefined && this.validateEmail(mail)) {
+                                    this.setState({ email: '' });
+                                    this.sendInvitationEmail(mail);
+                                } else {
+                                    toast.warn(`Please, enter a valid email before sending out the invite`);
+                                }
+                            }}>Invite</Button>
                         </div>
-                        <Button className="send-button" type="button" onClick={() => {
-                            const mail = this.state.email;
-                            if (mail !== undefined && this.validateEmail(mail)) {
-                                console.log("enviando");
-                                this.setState({ email: '' });
-                                this.sendInvitationEmail(mail);
-                            } else {
-                                toast.warn(`Please, enter a valid email before sending out the invite`);
-                            }
-                        }}>
-                            Invite
-                    </Button>
                     </Container>
-                    <div></div>
-                    <Container className="url-container">
-                        <div className="referred-url">
-                            <input type="text" readOnly value={`https://internxt.com/?ref=${user.uuid}`} />
+                    <Container className="row m-0 mt-4 p-0">
+                        <div className="col-8 px-0">
+                            <div className="referred-url">
+                                <input type="text" readOnly value={`https://internxt.com/?ref=${user.uuid}`} />
+                            </div>
                         </div>
-                        <Button type="button" className="copy-button" onClick={this.copyToClipboard}>
-                            {this.state.copySuccess}
-                        </Button>
-                        <DropdownButton className="share-container" name="menuShare" title="Share" type="toggle">
-                            <Dropdown.Item className="social-button"
-                                href={`https://twitter.com/intent/tweet?url=https://internxt.com/?ref=${user.uuid}&${this.parseUrl({ text: 'I\'ve made the switch to @Internxt a secure and free alternative to Dropbox that truly respects your privacy. Sign up using this exclusive link and get 2 GB free for life, and €5 that can be used if you ever decide to upgrade your Internxt storage plan!' })}`}
-                                target="_blank"
-                                data-size="large"
-                                original-referer={`https://internxt.com/?ref=${user.uuid}`}
-                                data-lang="en">
-                                <img src={twitter} alt="" />
-                            </Dropdown.Item>
-                            <Dropdown.Item className="social-button" data-href={`https://internxt.com/?ref=${user.uuid}`}
-                                href={`https://www.facebook.com/sharer/sharer.php?u=https://internxt.com/?ref=${user.uuid}&amp;src=sdkpreparse&${this.parseUrl({ quote: 'I\'ve made the switch to @Internxt a secure and free alternative to Dropbox that truly respects your privacy. Sign up using this exclusive link and get 2 GB free for life, and €5 that can be used if you ever decide to upgrade your Internxt storage plan!' })}`} target="_blank">
-                                <img src={facebook} alt="" />
-                            </Dropdown.Item>
-                            <Dropdown.Item className="social-button"
-                                href={`https://t.me/share/url?${this.parseUrl({ text: 'I\'ve made the switch to @Internxt a secure and free alternative to Dropbox that truly respects your privacy. Sign up using this exclusive link and get 2 GB free for life, and €5 that can be used if you ever decide to upgrade your Internxt storage plan!' })}&url=https://internxt.com/?ref=${user.uuid}`} target="_blank">
-                                <img src={telegram} alt="" />
-                            </Dropdown.Item>
-                        </DropdownButton>
+                        <div className="col-2 px-0 mx-0 d-flex">
+                            <Button type="button" className="copy-button m-auto" onClick={this.copyToClipboard}>
+                                {this.state.copySuccess}
+                            </Button>
+                        </div>
+                        <div className="col-2 d-flex p-0">
+                            <DropdownButton className="share-container m-auto" name="menuShare" title="Share" type="toggle">
+                                <Dropdown.Item className="social-button"
+                                    href={`https://twitter.com/intent/tweet?url=https://internxt.com/?ref=${user.uuid}&${this.parseUrl({ text: 'I\'ve made the switch to @Internxt a secure and free alternative to Dropbox that truly respects your privacy. Sign up using this exclusive link and get 2 GB free for life, and €5 that can be used if you ever decide to upgrade your Internxt storage plan!' })}`}
+                                    target="_blank"
+                                    data-size="large"
+                                    original-referer={`https://internxt.com/?ref=${user.uuid}`}
+                                    data-lang="en">
+                                    <img src={twitter} alt="" />
+                                </Dropdown.Item>
+                                <Dropdown.Item className="social-button" data-href={`https://internxt.com/?ref=${user.uuid}`}
+                                    href={`https://www.facebook.com/sharer/sharer.php?u=https://internxt.com/?ref=${user.uuid}&amp;src=sdkpreparse&${this.parseUrl({ quote: 'I\'ve made the switch to @Internxt a secure and free alternative to Dropbox that truly respects your privacy. Sign up using this exclusive link and get 2 GB free for life, and €5 that can be used if you ever decide to upgrade your Internxt storage plan!' })}`} target="_blank">
+                                    <img src={facebook} alt="" />
+                                </Dropdown.Item>
+                                <Dropdown.Item className="social-button"
+                                    href={`https://t.me/share/url?${this.parseUrl({ text: 'I\'ve made the switch to @Internxt a secure and free alternative to Dropbox that truly respects your privacy. Sign up using this exclusive link and get 2 GB free for life, and €5 that can be used if you ever decide to upgrade your Internxt storage plan!' })}&url=https://internxt.com/?ref=${user.uuid}`} target="_blank">
+                                    <img src={telegram} alt="" />
+                                </Dropdown.Item>
+                            </DropdownButton>
+                        </div>
                     </Container>
 
-                    <div></div>
+                    <div className="user-credit py-4">{`You have accumulated €${this.state.credit} `}</div>
 
-                    <div className="user-credit">{`You have accumulated €${this.state.credit} `}</div>
-
-                    <Button className="referred-button"
+                    <Button block className="referred-button"
                         type="button"
                         onClick={() => {
                             if (user.credit > 0) {
