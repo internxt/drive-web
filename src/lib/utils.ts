b0bbf9a5656a7e403bbf614bd4688a919eaa5f48
Joan Mora <elcorreudejoanmora@gmail.com>
2020-11-03 09:17:11 +0100

    Do not read cookies for referrals. Done by server

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -70,25 +70,6 @@ function decryptTextWithKey(encryptedText: string, keyToDecrypt: string) {
   }
 }
 
-function getCookie(name = 'REFERRAL') {
-  // Split cookie string and get all individual name=value pairs in an array
-  var cookieArr = document.cookie.split(";");
-
-  // Loop through the array elements
-  for (var i = 0; i < cookieArr.length; i++) {
-    var cookiePair = cookieArr[i].split("=");
-
-    /* Removing whitespace at the beginning of the cookie name
-    and compare it with the given string */
-    if (name == cookiePair[0].trim()) {
-      // Decode the cookie value and return
-      return decodeURIComponent(cookiePair[1]);
-    }
-  }
-
-  // Return null if not found
-  return null;
-}
 
 export {
   copyToClipboard,
@@ -98,5 +79,4 @@ export {
   decryptText,
   encryptTextWithKey,
   decryptTextWithKey,
-  getCookie
 }
