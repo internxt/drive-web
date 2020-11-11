84cc461b400cff37dde370c1a926e266ab2152de
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2020-11-11 13:49:03 +0100

    analytics page names

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -1,3 +1,12 @@
+export const PATH_NAMES = {
+    '/new': 'drive-web-register',
+    '/login': 'drive-web-login',
+    '/storage': 'drive-web-storage',
+    '/settings': 'drive-web-settings',
+    '/invite': 'drive-web-invite',
+    '/remove': 'drive-web-remove'
+}
+
 export function getUserData() {
     return JSON.parse(localStorage.getItem('xUser') || "{}")
 }
--- src/lib/history.ts
+++ src/lib/history.ts
@@ -1,5 +1,17 @@
 import { createBrowserHistory } from 'history';
+import { analytics, PATH_NAMES } from './analytics'
 
-const instance = createBrowserHistory()
+const instance = createBrowserHistory({ forceRefresh: false })
+
+instance.listen((nav) => {
+    const keys = Object.keys(PATH_NAMES)
+    const index = keys.indexOf(nav.pathname)
+
+    if (index > -1) {
+        const pageName = PATH_NAMES[keys[index]]
+        console.log('page', pageName)
+        analytics.page(pageName)
+    } 
+})
 
 export default instance;
