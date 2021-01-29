0cd6331213dcec6c045a6b8c5d46aef068c18071
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-01-29 10:20:08 +0100

    force socket to use ws transport without upgrade

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,4 +1,6 @@
 import io from 'socket.io-client';
-export const socket = io(process.env.NODE_ENV === 'production' ? '' : 'localhost:8000', {
-    path: '/api/sockets'
+export const socket = io(process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8000', {
+    transports: ['websocket'],
+    upgrade: false,
+    path: '/api/sockets', 
 });
