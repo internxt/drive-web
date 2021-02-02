0124a4bc16234ce84e9369a6c3ef1db38d76e67f
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2021-02-02 10:05:43 +0100

    loader spinner 06

--- src/components/xcloud/LoadingFileExplorer.tsx
+++ src/components/xcloud/LoadingFileExplorer.tsx
@@ -1,11 +1,9 @@
-import { LinearProgress } from '@material-ui/core'
 import React from 'react'
-import { Container } from 'react-bootstrap'
 import './LoadingFileExplorer.scss'
 
 export default function LoadingFileExplorer() {
-  return <Container className="loading-file-explorer">
-    <LinearProgress variant="indeterminate" />
-  </Container>
+  return <div className="loader-container">
+    <div className="loader06"></div>
+  </div>
 }
 
