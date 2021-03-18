859104f269a25430d745e898c1fe51d6d162981e
Siralde <aldemaro.gonzalez1@gmail.com>
2021-03-18 09:31:17 +0100

    remove console logs

--- src/components/forms/Reset.tsx
+++ src/components/forms/Reset.tsx
@@ -109,8 +109,6 @@ class Reset extends React.Component<ResetProps> {
   getSalt = () => {
     const email = Settings.getUser().email;
 
-    console.log('get Salt 3');
-
     return fetch('/api/login', {
       method: 'post',
       headers: getHeaders(false, false),
@@ -119,7 +117,6 @@ class Reset extends React.Component<ResetProps> {
       .then(res => res.json())
       .then(res => new Promise<void>(resolve => {
         this.setState({ salt: decryptText(res.sKey) }, () => {
-          console.log('get Salt 4');
           resolve();
         });
       }));
