bad0be0788dc292b4a886132bb46f9fc09b17978
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-06-04 17:18:35 +0200

    add getFilenameAndExt to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -75,6 +75,33 @@ function decryptTextWithKey(encryptedText: string, keyToDecrypt: string) {
   }
 }
 
+interface IGetFilenameAndExt {
+  filename: string,
+  extension: string
+}
+
+/**
+ * Separates entire filename in filename and extension
+ * @param entireFilename Filename + extension
+ * @returns Filename and extension splitted
+ */
+function getFilenameAndExt(entireFilename: string): IGetFilenameAndExt {
+  // based on path.parse native nodejs function
+  const re = /^(\/?|)([\s\S]*?)((?:\.{1,2}|[^\/]+?|)(\.[^.\/]*|))(?:[\/]*)$/;
+
+  const fileInfo = re.exec(entireFilename);
+
+  if (!fileInfo) {
+    return { filename: '', extension: '' };
+  }
+
+  const extensionWithDot = fileInfo[4];
+  const filename = fileInfo[3].substring(0, fileInfo[3].length - extensionWithDot.length);
+
+  const extension = extensionWithDot.split('.')[1];
+
+  return { filename, extension };
+}
 export {
   copyToClipboard,
   removeAccents,
@@ -82,5 +109,6 @@ export {
   encryptText,
   decryptText,
   encryptTextWithKey,
-  decryptTextWithKey
+  decryptTextWithKey,
+  getFilenameAndExt,
 };
