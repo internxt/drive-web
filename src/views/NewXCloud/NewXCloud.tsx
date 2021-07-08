1ff92163cbd59c66bf902780781d0773244b19ed
Aldimir <aldimirprincipal@gmail.com>
2021-07-08 12:36:01 +0200

    Check conn event listener + check conn fn()

--- src/views/NewXCloud/NewXCloud.tsx
+++ src/views/NewXCloud/NewXCloud.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import FileActivity from '../../components/FileActivity/FileActivity';
 import './NewXCloud.scss';
 import SideNavigator from '../../components/layout/SideNavigator/SideNavigator';
+import Header from '../../components/layout/Header';
 
 interface NewXCloudProps {
 }
@@ -12,11 +13,11 @@ const NewXCloud = (): NewXCloudProps => {
 
   return (
     <div className="App xcloud-layout flex">
-
-      {/* <div className="w-sidenav-1280 bg-l-neutral-20">1</div> */}
       <SideNavigator isVisible={isVisible} setIsVisible={setIsVisible} />
 
-      <div className="flex-grow">2</div>
+      <div className="flex-grow">
+        <Header />
+      </div>
       <FileActivity />
 
     </div>
