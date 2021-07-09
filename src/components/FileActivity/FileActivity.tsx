35d6587952ae964da6b2cc2d063cc6d975ac276e
Aldimir <aldimirprincipal@gmail.com>
2021-07-09 11:24:01 +0200

    Basic item

--- src/components/FileActivity/FileActivity.tsx
+++ src/components/FileActivity/FileActivity.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import FileLogger from '../FileLogger';
 
 import './FileActivity.scss';
 
@@ -17,27 +18,27 @@ class FileActivity extends React.Component<FileActivityProps, FileActivityState>
 
   render() {
     return (
-      <div className="w-activity-1280 bg-l-neutral-20 px-32px py-42px">
-        <div>
-
-          <div className="text-center">
-            <div className="file-icon bg-blue-30 m-auto">
-              ICON
-            </div>
-            <div>
-              <span className="text-neutral-900 block font-semibold text-base">Folder info</span>
-              <span className="text-neutral-500 block text-supporting-2">Folder name: FilesPending</span>
-            </div>
+      <div className="flex flex-col w-activity-1280 bg-l-neutral-20 px-32px py-42px border">
+        <div className="text-center">
+          <div className="file-icon bg-blue-30 m-auto">
+            ICON
           </div>
 
-          <div className="file-info-container text-left">
-            <span className="block text-neutral-500 text-xs">File: PDF</span>
-            <span className="block text-neutral-500 text-xs">Folder path: ../FilesPending</span>
-            <span className="block text-neutral-500 text-xs">Size: 55.7 MB</span>
-            <span className="block text-neutral-500 text-xs">Modified at: 24 Jun 2021</span>
-            <span className="block text-neutral-500 text-xs">Created at: 24 Jun 2021</span>
+          <div>
+            <span className="text-neutral-900 block font-semibold text-base">Folder info</span>
+            <span className="text-neutral-500 block text-supporting-2">Folder name: FilesPending</span>
           </div>
         </div>
+
+        <div className="file-info-container text-left">
+          <span className="block text-neutral-500 text-xs">File: PDF</span>
+          <span className="block text-neutral-500 text-xs">Folder path: ../FilesPending</span>
+          <span className="block text-neutral-500 text-xs">Size: 55.7 MB</span>
+          <span className="block text-neutral-500 text-xs">Modified at: 24 Jun 2021</span>
+          <span className="block text-neutral-500 text-xs">Created at: 24 Jun 2021</span>
+        </div>
+
+        <FileLogger />
       </div>
     );
   }
---
+++ src/components/FileLogger/Item.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { getIcon } from '../../helpers/getIcon';
+import { FileStatusTypes, IconTypes } from '../../models/interfaces';
+
+interface ItemProps {
+  isFolder: boolean
+  fileStatus: FileStatusTypes
+}
+
+const Item = ({ isFolder, fileStatus }: ItemProps) => {
+
+  const chooseIcon = (): string => {
+    if (isFolder) {
+      return getIcon(IconTypes.FolderBlue);
+    }
+    switch (fileStatus) {
+      case FileStatusTypes.Uploading:
+        return getIcon(IconTypes.ClockGray);
+
+      case FileStatusTypes.Success:
+        return getIcon(IconTypes.FileSuccessGreen);
+
+      case FileStatusTypes.Error:
+        return getIcon(IconTypes.FileErrorRed);
+
+      case FileStatusTypes.Encrypting:
+        return getIcon(IconTypes.FileEncryptingGray);
+
+      default:
+        return getIcon(IconTypes.ClockGray);
+    }
+  };
+
+  return (
+    <div className='flex items-center h-8 px-4 mt-2.5'>
+      <div className='flex items-center justify-center mr-2.5'>
+        <img src={chooseIcon()} alt="" />
+      </div>
+
+      <div className='flex flex-col justify-start'>
+        <span className='text-xs text-neutral-90'>
+          Folder
+        </span>
+
+        <span className='text-supporting-2 text-neutral-50'>
+          Downloading...
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Item;
