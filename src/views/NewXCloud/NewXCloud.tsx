2bd79354dcf07f6c2c9056b52e763c8cbaf905e3
Aldimir <aldimirprincipal@gmail.com>
2021-07-09 11:24:25 +0200

    Fixed unnecesary style

--- src/views/NewXCloud/NewXCloud.tsx
+++ src/views/NewXCloud/NewXCloud.tsx
@@ -5,20 +5,18 @@ import './NewXCloud.scss';
 import SideNavigator from '../../components/layout/SideNavigator/SideNavigator';
 
 interface NewXCloudProps {
+
 }
 
 const NewXCloud = (): NewXCloudProps => {
   const [isVisible, setIsVisible] = useState(true);
 
   return (
-    <div className="App xcloud-layout flex">
-
-      {/* <div className="w-sidenav-1280 bg-l-neutral-20">1</div> */}
+    <div className="flex xcloud-layout overflow-hidden">
       <SideNavigator isVisible={isVisible} setIsVisible={setIsVisible} />
 
       <div className="flex-grow">2</div>
       <FileActivity />
-
     </div>
   );
 };
