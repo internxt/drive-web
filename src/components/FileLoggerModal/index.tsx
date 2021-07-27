ca19f49831a5d51a3a743703f0218d57058fa3ac
Desislava Miroslavova <demidietsinf@gmail.com>
2021-07-27 18:03:32 +0200

    fixed more icons of merge

--- src/components/FileLoggerModal/index.tsx
+++ src/components/FileLoggerModal/index.tsx
@@ -49,13 +49,13 @@ const FileLoggerModal = (): JSX.Element => {
     <div className={`fixed bottom-0 right-80 flex flex-col w-64 transform duration-300 ${isMinimized ? 'h-9' : 'h-64'} bg-white mr-8 mb-11 rounded-md border border-gray-30 overflow-hidden ${!isOpen ? 'hidden' : ''} z-20`}>
       <div className='flex justify-between bg-neutral-900 px-4 py-2.5 rounded-md select-none'>
         <div className='flex w-max'>
-          <Button icon={IconType.DoubleArrowUpWhite} style={`transform duration-500 ${!isMinimized ? 'rotate-180' : 'rotate-0'}`} />
+          <Button icon={IconType.doubleArrowUpWhite} style={`transform duration-500 ${!isMinimized ? 'rotate-180' : 'rotate-0'}`} />
           <span className='text-xs font-semibold text-white ml-2.5'>Activity</span>
         </div>
 
         <div className='flex'>
-          <Button icon={IconType.DoubleArrowUpWhite} onClick={() => setIsMinized(!isMinimized)} style={`mr-1.5 transform duration-500 ${!isMinimized ? 'rotate-180' : 'rotate-0'}`} />
-          <Button icon={!hasFinished ? IconType.CrossNeutralBlue : IconType.CrossWhite} onClick={handleClose} />
+          <Button icon={IconType.doubleArrowUpWhite} onClick={() => setIsMinized(!isMinimized)} style={`mr-1.5 transform duration-500 ${!isMinimized ? 'rotate-180' : 'rotate-0'}`} />
+          <Button icon={!hasFinished ? IconType.crossNeutralBlue : IconType.crossWhite} onClick={handleClose} />
         </div>
       </div>
 
