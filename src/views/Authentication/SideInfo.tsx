bcc093167d680ed6c930ac5415333bf27eca8c6c
Aldimir <aldimirprincipal@gmail.com>
2021-07-28 11:01:56 +0200

    Fixed sideinfo background image and slider

--- src/views/Authentication/SideInfo.tsx
+++ src/views/Authentication/SideInfo.tsx
@@ -3,9 +3,12 @@ import '@brainhubeu/react-carousel/lib/style.css';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import './SideInfo.scss';
+import sideInfoBackground from '../../assets/images/sideinfo-background.jpg';
 
 const SideInfo = ({ texts }: { texts: { label: string, sublabel: string, reviews: { name: string, review: string }[] } }): JSX.Element => {
   const [value, setValue] = useState(0);
+
+  const onChange = (newValue: number) => setValue(newValue);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -17,14 +20,16 @@ const SideInfo = ({ texts }: { texts: { label: string, sublabel: string, reviews
   }, []);
 
   return (
-    <div className='flex flex-col justify-between items-start w-104 min-w-104 h-full bg-gradient-to-b from-blue-60 to-blue-80 text-white p-12 sideinfo_background'>
-      <div className='flex flex-col'>
+    <div className='flex flex-col justify-between items-start w-104 min-w-104 h-full bg-gradient-to-b from-blue-60 to-blue-80 text-white p-12 relative'>
+      <img className='absolute top-0 left-0 object-cover w-full h-full' src={sideInfoBackground} alt='' />
+
+      <div className='flex flex-col z-10'>
         <span className='text-xl font-bold tracking-0.3'>{texts.label}</span>
         <span className='text-supporting-2 tracking-0.3'>{texts.sublabel}</span>
       </div>
 
-      <div className='w-full'>
-        <Carousel value={value}>
+      <div className='w-full z-10'>
+        <Carousel draggable={false} value={value}>
           {texts.reviews.map((review, index) => (
             <div className='flex flex-col' key={index}>
               <span className='text-xl'>{review.name}</span>
@@ -33,7 +38,7 @@ const SideInfo = ({ texts }: { texts: { label: string, sublabel: string, reviews
             </div>
           ))}
         </Carousel>
-        <Dots value={value} number={texts.reviews.length} />
+        <Dots onChange={onChange} className='mt-2' value={value} number={texts.reviews.length} />
       </div>
     </div >
   );
