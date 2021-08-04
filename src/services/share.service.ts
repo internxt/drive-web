1eb80a1bdf37af6c082f14083f3de37e596f9532
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-08-04 16:06:30 +0200

    add share link and share info functionalities

--- src/services/share.service.ts
+++ src/services/share.service.ts
@@ -4,14 +4,44 @@ interface GenerateShareLinkResponse {
   token: string
 }
 
-export function generateShareLink(fileId: string, views: number, isFolder: boolean, encryptionKey: string, isTeam = false): Promise<string> {
+interface GenerateShareLinkRequestBody {
+  isFolder: boolean,
+  views: number,
+  encryptionKey: string,
+  fileToken: string,
+  bucket: string
+}
+
+interface GetShareInfoResponse {
+  user: string;
+  token: string;
+  file: string;
+  encryptionKey: string;
+  mnemonic: string;
+  isFolder: boolean;
+  views: number;
+  bucket: string;
+  fileToken: string;
+  fileMeta: {
+    folderId: string;
+    name: string;
+    type: string;
+    size: number;
+  }
+}
+
+export function generateShareLink(fileId: string, params: GenerateShareLinkRequestBody): Promise<string> {
   return fetch(`/api/storage/share/file/${fileId}`, {
     method: 'POST',
-    headers: getHeaders(true, true, isTeam),
-    body: JSON.stringify({ isFolder, views, encryptionKey })
+    headers: getHeaders(true, true),
+    body: JSON.stringify(params)
   }).then((res) => {
     return res.json();
   }).then((res: GenerateShareLinkResponse) => {
     return `${window.location.origin}/${res.token}`;
   });
+}
+
+export function getShareInfo(token: string): Promise<GetShareInfoResponse> {
+  return fetch(`/api/storage/share/${token}`).then<GetShareInfoResponse>((res) => res.json());
 }
