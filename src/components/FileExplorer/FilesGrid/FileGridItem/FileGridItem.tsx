ba9111700359e23c332ca0ef0136d301a6fc5426
Joan Mora <elcorreudejoanmora@gmail.com>
2021-08-05 13:17:11 +0200

    Drag and drop allow to drop over files

--- src/components/FileExplorer/FilesGrid/FileGridItem/FileGridItem.tsx
+++ src/components/FileExplorer/FilesGrid/FileGridItem/FileGridItem.tsx
@@ -246,18 +246,24 @@ class FileGridItem extends React.Component<FileGridItemProps, FileGridItemState>
       const namePathDestinationArray = this.props.namePath.map(level => level.name);
 
       namePathDestinationArray[0] = '';
-      const folderPath = namePathDestinationArray.join('/') + '/' + draggingTargetItemData.name;
 
       const itemsDragged = await getAllItems(e.dataTransfer);
       const { numberOfItems, rootList, files } = itemsDragged;
 
+      let folderPath = namePathDestinationArray.join('/');
+
+      folderPath = !draggingTargetItemData.isFolder ? folderPath : folderPath + '/' + draggingTargetItemData.name;
+      const parentFolderId = draggingTargetItemData.isFolder ? draggingTargetItemData.id : this.props.currentFolderId;
+
       if (files) {
         // files where dragged directly
-        await dispatch(storageThunks.uploadItemsThunk({ files, parentFolderId: draggingTargetItemData.id, folderPath: folderPath }));
+        await dispatch(storageThunks.uploadItemsThunk({ files, parentFolderId, folderPath }));
       }
       if (rootList) {
         for (const root of rootList) {
-          await dispatch(storageThunks.createFolderTreeStructureThunk({ root, currentFolderId: draggingTargetItemData.id }));
+          const currentFolderId = parentFolderId;
+
+          await dispatch(storageThunks.createFolderTreeStructureThunk({ root, currentFolderId }));
         }
       }
     }
