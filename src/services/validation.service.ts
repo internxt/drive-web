2c97dd64865f6e77eb3413d79005115bda78253f
Alex Menor <alex.menor@icloud.com>
2021-08-26 12:21:39 +0200

    remove unused code

--- src/services/validation.service.ts
+++ src/services/validation.service.ts
@@ -1,11 +1,4 @@
-import { auth } from '@internxt/lib';
-
 export const twoFactorRegexPattern = /^\d{3}(\s+)?\d{3}$/;
-export const illegalRe = /[\/\?<>\\:\*\|"]/g;
-export const controlRe = /[\x00-\x1f\x80-\x9f]/g;
-export const reservedRe = /^\.+$/;
-export const windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i;
-export const windowsTrailingRe = /[\. ]+$/;
 
 const validate2FA = (twoFactorCode: string): boolean => {
   const pattern = /^\d{3}(\s+)?\d{3}$/;
