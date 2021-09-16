ee30f8764e0c1d1117fe2464906a505f37f6ce0e
Carlos Sala Samper <carlos15695@hotmail.com>
2021-09-16 13:49:18 +0200

    eslint fix

--- src/store/slices/payment/index.ts
+++ src/store/slices/payment/index.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 import { RootState } from '../..';
 import { ProductPriceType, StripeSessionMode } from '../../../models/enums';
