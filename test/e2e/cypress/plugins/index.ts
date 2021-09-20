38b6925c8d61a820241eb09e571e7ccd02428894
Carlos Sala Samper <carlos15695@hotmail.com>
2021-09-20 18:03:23 +0200

    Cypress/plugins/ to typescript

--- test/e2e/cypress/plugins/index.ts
+++ test/e2e/cypress/plugins/index.ts
@@ -12,10 +12,8 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
+const pluginConfig: Cypress.PluginConfig = (on, config) => {
   return config;
 };
+
+export default pluginConfig;
