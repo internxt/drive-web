4a71d5f52d36eaa2a949c6e65db14b55927b96f3
Carlos Sala Samper <carlos15695@hotmail.com>
2021-09-22 11:27:03 +0200

    mini refactor limitService and usageService to format limit

--- src/components/PlanUsage/PlanUsage.tsx
+++ src/components/PlanUsage/PlanUsage.tsx
@@ -1,8 +1,9 @@
 import { AppView } from '../../models/enums';
 import i18n from '../../services/i18n.service';
+import limitService from '../../services/limit.service';
 import navigationService from '../../services/navigation.service';
 import { bytesToString } from '../../services/size.service';
-import { getUserLimitString } from '../../services/usage.service';
+import usageService from '../../services/usage.service';
 import { AccountViewTab } from '../../views/AccountView/tabs';
 
 export default function PlanUsage({
@@ -19,6 +20,7 @@ export default function PlanUsage({
   const onUpgradeButtonClicked = () => {
     navigationService.push(AppView.Account, { tab: AccountViewTab.Plans });
   };
+  const usagePercent = usageService.getUsagePercent(usage, limit);
 
   return (
     <div className={`flex flex-col justify-center w-full rounded-md ${className}`}>
@@ -26,11 +28,11 @@ export default function PlanUsage({
         <p>Loading...</p>
       ) : (
         <p className="text-sm text-m-neutral-70 m-0">
-          {bytesToString(usage) || '0'} of {getUserLimitString(limit)}
+          {bytesToString(usage) || '0'} of {limitService.formatLimit(limit)}
         </p>
       )}
       <div className="flex justify-start h-1.5 w-full bg-l-neutral-30 rounded-lg overflow-hidden mb-1.5">
-        <div className="h-full bg-blue-60" style={{ width: isLoading ? 0 : Math.ceil((usage / limit) * 100) }} />
+        <div className="h-full bg-blue-60" style={{ width: isLoading ? 0 : usagePercent }} />
       </div>
       <p onClick={onUpgradeButtonClicked} className="font-semibold text-blue-60 cursor-pointer">
         {i18n.get('action.upgrade')}
--- src/services/limit.service.ts
+++ src/services/limit.service.ts
@@ -1,8 +1,12 @@
 import httpService from './http.service';
 
+import { bytesToString } from './size.service';
+
 export interface FetchLimitResponse {
   maxSpaceBytes: number;
 }
+
+export const INFINITE_LIMIT = 108851651149824;
 
 async function fetchLimit(): Promise<number> {
   const response = await httpService.get<FetchLimitResponse>('/api/limit');
@@ -10,8 +14,19 @@ async function fetchLimit(): Promise<number> {
   return response.maxSpaceBytes;
 }
 
+const formatLimit = (limit: number): string => {
+  let result = '...';
+
+  if (limit > 0) {
+    result = limit < INFINITE_LIMIT ? bytesToString(limit) : '\u221E';
+  }
+
+  return result;
+};
+
 const limitService = {
   fetchLimit,
+  formatLimit,
 };
 
 export default limitService;
