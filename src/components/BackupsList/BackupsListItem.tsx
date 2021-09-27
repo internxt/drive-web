d02c4842d576aef91433bcaf347102554aeaf76d
Alex Menor <alex.menor@icloud.com>
2021-09-27 12:17:45 +0200

    add backup icon to backup list item

--- src/components/BackupsList/BackupsListItem.tsx
+++ src/components/BackupsList/BackupsListItem.tsx
@@ -1,7 +1,7 @@
 import { Backup } from '../../models/interfaces';
 import * as Unicons from '@iconscout/react-unicons';
 import dateService from '../../services/date.service';
-import { ReactComponent as LightFolder } from '../../assets/icons/light/folder.svg';
+import { ReactComponent as BackupIcon } from '../../assets/icons/light/folder-backup.svg';
 import sizeService from '../../services/size.service';
 
 export default function BackupsListItem({ backup }: { backup: Backup }): JSX.Element {
@@ -10,7 +10,7 @@ export default function BackupsListItem({ backup }: { backup: Backup }): JSX.Ele
   return (
     <div className="py-3.5 border-b border-l-neutral-30 flex items-center hover:bg-blue-20">
       <div className="w-0.5/12 px-3 flex items-center justify-center box-content">
-        <LightFolder className="w-6 h-6" />
+        <BackupIcon className="w-6 h-6" />
       </div>
       <p className="flex-grow pr-3">{basename}</p>
       <div className="w-2/12 hidden items-center xl:flex"></div>
