1cbf5fbad1be8ea1d36fdf25aa92b6049f887ac3
Alex Menor <alex.menor@icloud.com>
2021-09-28 10:23:30 +0200

    filter null values from devices

--- src/services/backups.service.ts
+++ src/services/backups.service.ts
@@ -3,11 +3,12 @@ import { Backup, Device } from '../models/interfaces';
 import { aes } from '@internxt/lib';
 
 const backupsService = {
-  getAllDevices(): Promise<Device[]> {
-    return fetch(`${process.env.REACT_APP_API_URL}/api/backup/device`, {
+  async getAllDevices(): Promise<Device[]> {
+    const devices = await fetch(`${process.env.REACT_APP_API_URL}/api/backup/device`, {
       method: 'GET',
       headers: getHeaders(true, false),
     }).then((res) => res.json());
+    return devices.filter((device) => device.id);
   },
   async getAllBackups(mac: string): Promise<Backup[]> {
     const backups = await fetch(`${process.env.REACT_APP_API_URL}/api/backup/${mac}`, {
