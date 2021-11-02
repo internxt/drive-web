718d09a4903bb3e9e9f9696d588404de7113c5ab
Carlos Sala Samper <carlos15695@hotmail.com>
2021-11-02 12:56:57 +0100

    Newsletter subscription from server

--- src/app/newsletter/services/newsletterService.ts
+++ src/app/newsletter/services/newsletterService.ts
@@ -1,23 +1,13 @@
 import httpService from 'app/core/services/http.service';
 
-const MAILERLITE_API_KEY = 'bbcd6c365d78a339a63df27b93ebd323';
 const GROUP_ID = '103406410';
 
 const newsletterService = {
   subscribe(email: string) {
-    httpService.post<{ email: string; resubscribe: boolean; autoresponders: boolean }, void>(
-      `https://api.mailerlite.com/api/v2/groups/${GROUP_ID}/subscribers`,
-      { email, resubscribe: true, autoresponders: true },
-      {
-        headers: {
-          Accept: 'application/json',
-          'X-MailerLite-ApiDocs': 'true',
-          'Content-Type': 'application/json',
-          'X-MailerLite-ApiKey': MAILERLITE_API_KEY,
-          Authorization: undefined,
-        },
-      },
-    );
+    return httpService.post<{ email: string; groupId: string }, void>('/api/newsletter/subscribe', {
+      email,
+      groupId: GROUP_ID,
+    });
   },
 };
 
