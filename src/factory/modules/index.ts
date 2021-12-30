c989753a7a78c90530354638dc40f43e83a2a0dd
sandro <devspuig@pm.me>
2021-12-30 11:57:53 +0100

    Inject mnemonic to Storage module client construction

--- src/factory/modules/index.ts
+++ src/factory/modules/index.ts
@@ -4,18 +4,28 @@ import localStorageService from '../../app/core/services/local-storage.service';
 import { LocalStorageItem, Workspace } from '../../app/core/types';
 
 export function createStorageClient(): Storage {
-  const token = getToken();
-  return Storage.client(process.env.REACT_APP_API_URL, packageJson.name, packageJson.version, token);
+  const workspace = getWorkspace();
+  const token = getToken(workspace);
+  const mnemonic = getMnemonic(workspace);
+  return Storage.client(process.env.REACT_APP_API_URL, packageJson.name, packageJson.version, token, mnemonic);
 }
 
-function getToken(): Token {
+function getWorkspace(): string {
+  return (localStorageService.get(LocalStorageItem.Workspace) as Workspace) || Workspace.Individuals;
+}
+
+function getMnemonic(workspace: string): string {
+  const mnemonicByWorkspace: { [key in Workspace]: string } = {
+    [Workspace.Individuals]: localStorageService.get('xMnemonic') || '',
+    [Workspace.Business]: localStorageService.getTeams()?.bridge_mnemonic || '',
+  };
+  return mnemonicByWorkspace[workspace];
+}
+
+function getToken(workspace: string): Token {
   const tokenByWorkspace: { [key in Workspace]: string } = {
     [Workspace.Individuals]: localStorageService.get('xToken') || '',
     [Workspace.Business]: localStorageService.get('xTokenTeam') || '',
   };
-  const workspace =
-    (localStorageService.get(LocalStorageItem.Workspace) as Workspace) ||
-    Workspace.Individuals;
-
   return tokenByWorkspace[workspace];
 }
