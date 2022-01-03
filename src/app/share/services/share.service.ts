bbbdf67b91532784828254c95b57ba49d789e0bd
sandro <devspuig@pm.me>
2022-01-03 11:18:42 +0100

    Replace share related call with sdk Share module

--- src/app/share/services/share.service.ts
+++ src/app/share/services/share.service.ts
@@ -1,51 +1,21 @@
-import httpService from '../../core/services/http.service';
 import errorService from '../../core/services/error.service';
+import { createShareClient } from '../../../factory/modules';
+import { ShareTypes } from '@internxt/sdk';
 
-interface GenerateShareLinkResponse {
-  token: string;
-}
-
-interface GenerateShareLinkRequestBody {
-  isFolder: boolean;
-  views: number;
-  encryptionKey: string;
-  fileToken: string;
-  bucket: string;
-}
-
-export interface GetShareInfoResponse {
-  user: string;
-  token: string;
-  file: string;
-  encryptionKey: string;
-  mnemonic: string;
-  isFolder: boolean;
-  views: number;
-  bucket: string;
-  fileToken: string;
-  fileMeta: {
-    folderId: string;
-    name: string;
-    type: string;
-    size: number;
-  };
-}
-
-export function generateShareLink(fileId: string, params: GenerateShareLinkRequestBody): Promise<string> {
-  return httpService
-    .post<GenerateShareLinkRequestBody, GenerateShareLinkResponse>(`/api/storage/share/file/${fileId}`, params)
-    .then((response: GenerateShareLinkResponse) => {
+export function generateShareLink(params: ShareTypes.GenerateShareLinkPayload): Promise<string> {
+  const shareClient = createShareClient();
+  return shareClient.generateShareLink(params)
+    .then(response => {
       return `${window.location.origin}/${response.token}`;
     });
 }
 
-export function getShareInfo(token: string): Promise<GetShareInfoResponse> {
-  try {
-    return httpService.get(`${process.env.REACT_APP_API_URL}/api/storage/share/${token}`);
-  } catch (err: unknown) {
-    const castedError = errorService.castError(err);
-    throw castedError;
-  }
+export function getShareInfo(token: string): Promise<ShareTypes.GetShareInfoResponse> {
+  const shareClient = createShareClient();
+  return shareClient.getShareTokenInfo(token)
+    .catch(error => {
+      throw errorService.castError(error);
+    });
 }
 
 const shareService = {
