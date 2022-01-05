9823b56a14e46cf41ad49c9a44c5fff3dc4bd357
Joan Mora <elcorreudejoanmora@gmail.com>
2022-01-05 18:08:17 +0100

    Add tracking payments conversions

--- src/app/analytics/types.ts
+++ src/app/analytics/types.ts
@@ -20,4 +20,40 @@ export enum AnalyticsTrack {
   UserResetPasswordRequest = 'user-reset-password-request',
   FileUploadBucketIdUndefined = 'file-upload-bucketid-undefined',
   ShareLinkBucketIdUndefined = 'share-link-bucketid-undefined',
+  PaymentConversionEvent = 'Payment Conversion'
+}
+
+export interface PriceMetadata {
+  maxSpaceBytes: string,
+  name: string,
+  planType: string,
+  show?: string
+}
+
+export interface RecurringPrice {
+  aggregate_usage?: unknown,
+  interval: 'month' | 'year',
+  interval_count: number,
+  trial_period_days: number,
+  usage_type: string
+}
+
+export interface PriceData {
+  active: boolean,
+  billing_schema: string,
+  created: number,
+  currency: string,
+  id: string,
+  livemode: boolean,
+  lookup_key?: string,
+  metadata: PriceMetadata,
+  nickname: string,
+  object: string,
+  product: string,
+  recurring?: RecurringPrice,
+  tax_behaviour: string,
+  transform_quantity: unknown,
+  type: 'recurring' | 'one_time',
+  unit_amount: number,
+  unit_amount_decimal: string
 }
