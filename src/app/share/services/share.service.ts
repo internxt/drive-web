cc40599e4b307e5cb15db4647a1b205ab7a532a6
sandro <devspuig@pm.me>
2022-01-10 16:51:35 +0100

    Update function names

--- src/app/share/services/share.service.ts
+++ src/app/share/services/share.service.ts
@@ -4,7 +4,7 @@ import { ShareTypes } from '@internxt/sdk/dist/drive';
 
 export function generateShareLink(params: ShareTypes.GenerateShareLinkPayload): Promise<string> {
   const shareClient = createShareClient();
-  return shareClient.generateShareLink(params)
+  return shareClient.createShareLink(params)
     .then(response => {
       return `${window.location.origin}/${response.token}`;
     });
@@ -12,7 +12,7 @@ export function generateShareLink(params: ShareTypes.GenerateShareLinkPayload): 
 
 export function getShareInfo(token: string): Promise<ShareTypes.GetShareInfoResponse> {
   const shareClient = createShareClient();
-  return shareClient.getShareTokenInfo(token)
+  return shareClient.getShareByToken(token)
     .catch(error => {
       throw errorService.castError(error);
     });
