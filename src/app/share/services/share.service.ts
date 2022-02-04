187890ee83c84cce6d285224d4171632d00aa544
sandro <devspuig@pm.me>
2022-02-04 09:36:59 +0100

    Add methods to fetch shared folder data

--- src/app/share/services/share.service.ts
+++ src/app/share/services/share.service.ts
@@ -2,6 +2,7 @@ import errorService from '../../core/services/error.service';
 import { ShareTypes } from '@internxt/sdk/dist/drive';
 import { SdkFactory } from '../../core/factory/sdk';
 
+
 export function generateShareFileLink(params: ShareTypes.GenerateShareFileLinkPayload): Promise<string> {
   const shareClient = SdkFactory.getInstance().createShareClient();
   return shareClient.createShareFileLink(params)
@@ -26,6 +27,23 @@ export function getSharedFileInfo(token: string): Promise<ShareTypes.SharedFileI
     .catch(error => {
       throw errorService.castError(error);
     });
+}
+
+export function getSharedFolderInfo(token: string): Promise<ShareTypes.SharedFolderInfo> {
+  const shareClient = SdkFactory.getInstance().createShareClient();
+  return shareClient.getSharedFolderByToken(token);
+}
+
+export function getSharedDirectoryFolders(payload: ShareTypes.GetSharedDirectoryPayload):
+  Promise<ShareTypes.SharedDirectoryFolders> {
+  const shareClient = SdkFactory.getInstance().createShareClient();
+  return shareClient.getSharedDirectoryFolders(payload);
+}
+
+export function getSharedDirectoryFiles(payload: ShareTypes.GetSharedDirectoryPayload):
+  Promise<ShareTypes.SharedDirectoryFiles> {
+  const shareClient = SdkFactory.getInstance().createShareClient();
+  return shareClient.getSharedDirectoryFiles(payload);
 }
 
 const shareService = {
