b15c141346d047b7336e5acdd5159f382df6f023
Sergio Gutiérrez Villalba <gv.sergio@gmail.com>
2022-02-10 11:24:29 +0100

    WIP on share folder view

--- src/app/share/views/ShareView/ShareFolderView.tsx
+++ src/app/share/views/ShareView/ShareFolderView.tsx
@@ -10,7 +10,7 @@ import UilCheck from '@iconscout/react-unicons/icons/uil-check';
 import {
   getSharedDirectoryFiles,
   getSharedDirectoryFolders,
-  getSharedFolderInfo
+  getSharedFolderInfo,
 } from 'app/share/services/share.service';
 
 import { ReactComponent as Spinner } from 'assets/icons/spinner.svg';
@@ -25,26 +25,29 @@ import { IDownloadParams, Network } from '../../../drive/services/network';
 import 'react-toastify/dist/ReactToastify.css';
 import './ShareView.scss';
 
+// eslint-disable-next-line max-len
+import { downloadSharedFolderUsingStreamSaver } from 'app/drive/services/download.service/downloadFolder/downloadSharedFolderUsingStreamSaver';
+
 export interface ShareViewProps {
   match: match<{
-    token: string,
-    code: string,
+    token: string;
+    code: string;
   }>;
 }
 
 interface FolderPackage {
-  folderId: number
-  pack: JSZip
+  folderId: number;
+  pack: JSZip;
 }
 
 interface ShareViewState {
-  token: string
-  error: string | null
-  progress: number
-  ready: boolean
-  info: ShareTypes.SharedFolderInfo
-  completedFolders: FolderPackage[]
-  rootPackage: JSZip
+  token: string;
+  error: string | null;
+  progress: number;
+  ready: boolean;
+  info: ShareTypes.SharedFolderInfo;
+  completedFolders: FolderPackage[];
+  rootPackage: JSZip;
 }
 
 class ShareFolderView extends Component<ShareViewProps, ShareViewState> {
@@ -61,35 +64,132 @@ class ShareFolderView extends Component<ShareViewProps, ShareViewState> {
       name: '',
       size: 0,
       bucket: '',
-      bucketToken: ''
+      bucketToken: '',
     },
     completedFolders: [],
-    rootPackage: new JSZip()
+    rootPackage: new JSZip(),
   };
 
   componentDidMount(): void {
-    this.loadFolderInfo()
-      .then(() => {
-        this.setState({
-          ready: true
-        });
-      })
-      .catch((err) => {
-        this.setState({
-          error: err.message
-        });
-      });
+    this.loadFolderInfo().then(() => {
+      console.log('download finished');
+    });
+    // .then(() => {
+    //   this.setState({
+    //     ready: true,
+    //   });
+    // })
+    // .catch((err) => {
+    //   this.setState({
+    //     error: err.message,
+    //   });
+    // });
   }
 
   updateProgress = (progress) => {
     this.setState({
-      progress: Math.max(TaskProgress.Min, progress * 100)
+      progress: Math.max(TaskProgress.Min, progress * 100),
     });
-  }
+  };
 
   /**
    * Fetches general info and directory folders structure
    */
+  // loadFolderInfo = async () => {
+  //   if (navigator.userAgent.match('CriOS')) {
+  //     // ! iOS Chrome is not supported
+  //     throw new Error('Chrome iOS not supported. Use Safari to proceed');
+  //   }
+
+  //   const folderInfo = await getSharedFolderInfo(this.state.token);
+  //   const rootFolderId = folderInfo.folderId;
+  //   this.setState({
+  //     info: {
+  //       folderId: rootFolderId,
+  //       name: folderInfo.name,
+  //       size: folderInfo.size,
+  //       bucket: folderInfo.bucket,
+  //       bucketToken: folderInfo.bucketToken,
+  //     },
+  //   });
+
+  //   let currentOffset = 0;
+  //   const zip = new JSZip();
+  //   const writableStream = streamSaver.createWriteStream(`${folderInfo.name}.zip`, {});
+  //   const writer = writableStream.getWriter();
+
+  //   const rootFolder: FolderPackage = {
+  //     folderId: rootFolderId,
+  //     pack: zip,
+  //   };
+  //   const pendingFolders: FolderPackage[] = [rootFolder];
+
+  //   let folderDownloadCompleted = false;
+
+  //   const { bucket, bucketToken } = this.state.info;
+
+  //   do {
+  //     const folderToDownload = pendingFolders.shift() as FolderPackage;
+
+  //     console.log('downloading folder %s', folderToDownload.folderId);
+
+  //     while (!folderDownloadCompleted) {
+  //       const payload: ShareTypes.GetSharedDirectoryFoldersPayload = {
+  //         token: this.state.token,
+  //         directoryId: folderToDownload.folderId,
+  //         offset: currentOffset,
+  //         limit: this.FOLDERS_LIMIT_BY_REQUEST,
+  //       };
+
+  //       const nextFoldersChunkPromise = getSharedDirectoryFolders(payload).then((foldersResponse) => {
+  //         foldersResponse.folders.forEach(({ id, name }) => {
+  //           pendingFolders.push({
+  //             folderId: id,
+  //             pack: folderToDownload.pack.folder(name),
+  //           });
+  //         });
+
+  //         return foldersResponse;
+  //       });
+
+  //       await putDirectoryFilesInsideZip(folderToDownload.pack, bucket, bucketToken, {
+  //         token: this.state.token,
+  //         code: this.state.code,
+  //         directoryId: folderToDownload.folderId,
+  //         limit: this.FILES_LIMIT_BY_REQUEST,
+  //       });
+
+  //       const { last } = await nextFoldersChunkPromise;
+
+  //       folderDownloadCompleted = last;
+  //       currentOffset += this.FOLDERS_LIMIT_BY_REQUEST;
+  //     }
+  //   } while (pendingFolders.length > 0);
+
+  //   return new Promise<void>((resolve, reject) => {
+  //     const folderStream = zip.generateInternalStream({
+  //       type: 'uint8array',
+  //       streamFiles: true,
+  //       compression: 'DEFLATE',
+  //     }) as Readable;
+  //     folderStream
+  //       ?.on('data', (chunk: Buffer) => {
+  //         console.log('folder data here');
+  //         writer.write(chunk);
+  //       })
+  //       .on('error', (err) => {
+  //         reject(err);
+  //       })
+  //       .on('end', () => {
+  //         writer.close();
+  //         window.removeEventListener('unload', writer.abort);
+  //         resolve();
+  //       });
+
+  //     folderStream.resume();
+  //   });
+  // };
+
   loadFolderInfo = async () => {
     if (navigator.userAgent.match('CriOS')) {
       // ! iOS Chrome is not supported
@@ -97,66 +197,37 @@ class ShareFolderView extends Component<ShareViewProps, ShareViewState> {
     }
 
     const folderInfo = await getSharedFolderInfo(this.state.token);
-    const rootFolderId = folderInfo.folderId;
-    this.setState({
-      info: {
-        folderId: rootFolderId,
+
+    return downloadSharedFolderUsingStreamSaver(
+      {
         name: folderInfo.name,
-        size: folderInfo.size,
-        bucket: folderInfo.bucket,
-        bucketToken: folderInfo.bucketToken,
+        code: this.state.code,
+        id: folderInfo.folderId,
+        token: this.state.token,
       },
-    });
-    let currentOffset = 0;
-
-    const pendingFolders: FolderPackage[] = [{
-      folderId: rootFolderId,
-      pack: this.state.rootPackage
-    }];
-    const completedFolders: FolderPackage[] = [];
-
-    while (pendingFolders.length > 0) {
-      const { folderId, pack } = pendingFolders.shift() as FolderPackage;
-      let completed = false;
-      while (!completed) {
-        const payload: ShareTypes.GetSharedDirectoryFoldersPayload = {
-          token: this.state.token,
-          directoryId: folderId,
-          offset: currentOffset,
-          limit: this.FOLDERS_LIMIT_BY_REQUEST,
-        };
-        const foldersResponse = await getSharedDirectoryFolders(payload);
-        foldersResponse.folders.map(folder => {
-          pendingFolders.push({
-            folderId: folder.id,
-            pack: pack.folder(folder.name)
-          });
-        });
-        completed = foldersResponse.last;
-        currentOffset += this.FOLDERS_LIMIT_BY_REQUEST;
-      }
-      completedFolders.push({ folderId, pack });
-    }
-
-    this.setState({
-      completedFolders: completedFolders
-    });
-  }
+      folderInfo.bucket,
+      folderInfo.bucketToken,
+      {
+        filesLimit: this.FILES_LIMIT_BY_REQUEST,
+        foldersLimit: this.FOLDERS_LIMIT_BY_REQUEST,
+      },
+    );
+  };
 
   isFileSystemApiAvailable = (): boolean => {
-    return ('showSaveFilePicker' in window);
-  }
+    return 'showSaveFilePicker' in window;
+  };
 
   /**
    * Triggered when user starts the shared folder download
    * Decides if download should happen using streams or BLOBs
    */
   download = async (): Promise<void> => {
-    if (this.isFileSystemApiAvailable()) {
-      await this.downloadWithStreams();
-    } else {
-      await this.downloadWithBlobs();
-    }
+    // if (this.isFileSystemApiAvailable()) {
+    //   await this.downloadWithStreams();
+    // } else {
+    //   await this.downloadWithBlobs();
+    // }
   };
 
   /**
@@ -164,17 +235,8 @@ class ShareFolderView extends Component<ShareViewProps, ShareViewState> {
    */
   downloadWithBlobs = async (): Promise<void> => {
     await this.downloadDirectory(
-      async (
-        network: Network,
-        pack: JSZip,
-        file: SharedDirectoryFile,
-        params: IDownloadParams
-      ) => {
-        const [fileBlobPromise] = network.downloadFile(
-          this.state.info.bucket,
-          file.id,
-          params
-        );
+      async (network: Network, pack: JSZip, file: SharedDirectoryFile, params: IDownloadParams) => {
+        const [fileBlobPromise] = network.downloadFile(this.state.info.bucket, file.id, params);
         const fileBlob = await fileBlobPromise;
         pack.file(`${file.name}.${file.type}`, fileBlob);
       },
@@ -182,57 +244,57 @@ class ShareFolderView extends Component<ShareViewProps, ShareViewState> {
         await this.state.rootPackage.generateAsync({ type: 'blob' }).then((content) => {
           fileDownload(content, `${this.state.info.name}.zip`, 'application/zip');
         });
-      }
+      },
     );
   };
 
   /**
    * Launches the download logic configuring the packaging to use streams
    */
-  downloadWithStreams = async (): Promise<void> => {
-    await this.downloadDirectory(
-      async (
-        network: Network,
-        pack: JSZip,
-        file: SharedDirectoryFile,
-        params: IDownloadParams
-      ) => {
-        const [fileStreamPromise] = network.getFileDownloadStream(
-          this.state.info.bucket,
-          file.id,
-          params
-        );
-        const fileStream = await fileStreamPromise;
-        pack.file(`${file.name}.${file.type}`, fileStream, { compression: 'DEFLATE' });
-      },
-      async () => {
-        const writableStream = streamSaver.createWriteStream(`${this.state.info.name}.zip`, {});
-        const writer = writableStream.getWriter();
+  // downloadWithStreams = async (): Promise<void> => {
+  //   await this.downloadDirectory(
+  //     async (
+  //       network: Network,
+  //       pack: JSZip,
+  //       file: SharedDirectoryFile,
+  //       params: IDownloadParams
+  //     ) => {
+  //       const [fileStreamPromise] = network.getFileDownloadStream(
+  //         this.state.info.bucket,
+  //         file.id,
+  //         params
+  //       );
+  //       const fileStream = await fileStreamPromise;
+  //       pack.file(`${file.name}.${file.type}`, fileStream, { compression: 'DEFLATE' });
+  //     },
+  //     async () => {
+  //       const writableStream = streamSaver.createWriteStream(`${this.state.info.name}.zip`, {});
+  //       const writer = writableStream.getWriter();
 
-        await new Promise<void>((resolve, reject) => {
-          const folderStream = this.state.rootPackage.generateInternalStream({
-            type: 'uint8array',
-            streamFiles: true,
-            compression: 'DEFLATE',
-          }) as Readable;
-          folderStream
-            ?.on('data', (chunk: Buffer) => {
-              console.log('folder data here');
-              writer.write(chunk);
-            })
-            .once('error', (err) => {
-              writer.close();
-              reject(err);
-            })
-            .once('end', () => {
-              writer.close();
-              window.removeEventListener('unload', writer.abort);
-              resolve();
-            }).resume();
-        });
-      }
-    );
-  };
+  //       await new Promise<void>((resolve, reject) => {
+  //         const folderStream = this.state.rootPackage.generateInternalStream({
+  //           type: 'uint8array',
+  //           streamFiles: true,
+  //           compression: 'DEFLATE',
+  //         }) as Readable;
+  //         folderStream
+  //           ?.on('data', (chunk: Buffer) => {
+  //             console.log('folder data here');
+  //             writer.write(chunk);
+  //           })
+  //           .once('error', (err) => {
+  //             writer.close();
+  //             reject(err);
+  //           })
+  //           .once('end', () => {
+  //             writer.close();
+  //             window.removeEventListener('unload', writer.abort);
+  //             resolve();
+  //           }).resume();
+  //       });
+  //     }
+  //   );
+  // };
 
   /**
    * Performs the common logic of fetching files details and downloading data from network
@@ -240,11 +302,11 @@ class ShareFolderView extends Component<ShareViewProps, ShareViewState> {
    * @param packFiles
    */
   downloadDirectory = async (
-    downloadFile: (
+    downloadFileFunction: (
       network: Network,
       pack: JSZip,
       file: SharedDirectoryFile,
-      params: IDownloadParams
+      params: IDownloadParams,
     ) => Promise<void>,
     packFiles: () => Promise<void>,
   ) => {
@@ -268,11 +330,10 @@ class ShareFolderView extends Component<ShareViewProps, ShareViewState> {
           offset: currentOffset,
           limit: this.FILES_LIMIT_BY_REQUEST,
         };
-        const filesResponse = await getSharedDirectoryFiles(payload);
+        const { files, last } = await getSharedDirectoryFiles(payload);
 
-        for (const file of filesResponse.files) {
-          /** Download file data from network */
-          await downloadFile(network, pack, file, {
+        for (const file of files) {
+          await downloadFileFunction(network, pack, file, {
             fileEncryptionKey: Buffer.from(file.encryptionKey, 'hex'),
             fileToken: this.state.info.bucketToken,
             progressCallback: (fileProgress) => {
@@ -284,7 +345,7 @@ class ShareFolderView extends Component<ShareViewProps, ShareViewState> {
           });
         }
 
-        completed = filesResponse.last;
+        completed = last;
         currentOffset += this.FILES_LIMIT_BY_REQUEST;
       }
     }
@@ -322,37 +383,34 @@ class ShareFolderView extends Component<ShareViewProps, ShareViewState> {
           </h1>
         </div>
         <p className="text-l-neutral-50 text-sm mt-1">{sizeService.bytesToString(this.state.info?.size)}</p>
-        <div className="h-12 mt-5">
-          {
-            this.state.progress
-              ? ProgressComponent(this.state.progress)
-              : DownloadButton}
-        </div>
-      </div>
+        <div className="h-12 mt-5">{this.state.progress ? ProgressComponent(this.state.progress) : DownloadButton}</div>
+      </div>,
     );
   }
 
   renderBody = (body): JSX.Element => {
-    return <div className="flex justify-center items-center h-screen bg-gray-10 relative">
-      <Logo className="absolute top-5 left-5 h-auto w-32"></Logo>
-      {body}
-    </div>;
-  }
+    return (
+      <div className="flex justify-center items-center h-screen bg-gray-10 relative">
+        <Logo className="absolute top-5 left-5 h-auto w-32"></Logo>
+        {body}
+      </div>
+    );
+  };
 }
 
 function ProgressComponent(progress) {
   const progressBarPixelsTotal = 100;
   const progressBarPixelsCurrent = (progress * progressBarPixelsTotal) / 100;
-  return progress < 100
-    ?
+  return progress < 100 ? (
     <div style={{ width: `${progressBarPixelsTotal}px` }} className="bg-l-neutral-20">
       <div
         style={{ width: `${progressBarPixelsCurrent}px` }}
         className="border-t-8 rounded border-l-neutral-50 transition-width duration-1000"
       ></div>
     </div>
-    :
-    <UilCheck className="text-green-50" height="40" width="40"></UilCheck>;
+  ) : (
+    <UilCheck className="text-green-50" height="40" width="40"></UilCheck>
+  );
 }
 
 export default ShareFolderView;
