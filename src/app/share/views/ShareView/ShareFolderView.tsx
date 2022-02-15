5e41df0271d2ead477eea4ffc9707622c28b494b
sandro <devspuig@pm.me>
2022-02-15 13:49:05 +0100

    Extract Spinner element

--- src/app/share/views/ShareView/ShareFolderView.tsx
+++ src/app/share/views/ShareView/ShareFolderView.tsx
@@ -29,6 +29,7 @@ import {
 import {
   downloadSharedFolderUsingBlobs
 } from '../../../drive/services/download.service/downloadFolder/downloadSharedFolderUsingBlobs';
+import Spinner from '../../../shared/components/Spinner/Spinner';
 
 
 interface ShareViewProps extends ShareViewState {
@@ -68,19 +69,6 @@ const ShareFolderView = (props: ShareViewProps): JSX.Element => {
     };
     getInfo();
   }, []);
-
-  const Spinner = (
-    <>
-      <svg className="animate-spin" xmlns="http://www.w3.org/2000/svg"
-           fill="none" viewBox="0 0 24 24">
-        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-        <path className="opacity-75" fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824
-              3 7.938l3-2.647z">
-        </path>
-      </svg>
-    </>
-  );
 
   const getAvatarLetters = () => {
     const initials = user && (`${user['name'].charAt(0)}${user['lastname'].charAt(0)}`).toUpperCase();
@@ -250,7 +238,9 @@ const ShareFolderView = (props: ShareViewProps): JSX.Element => {
                 (
                   <>
                     {/* Download in progress */}
-                    <div className="h-5 w-5 text-white mr-1">{Spinner}</div>
+                    <div className="h-5 w-5 text-white mr-1">
+                      <Spinner/>
+                    </div>
                     <span>{i18n.get('actions.downloading')}</span>
                     <span className="font-normal text-blue-20">{progress}%</span>
                   </>
@@ -278,7 +268,9 @@ const ShareFolderView = (props: ShareViewProps): JSX.Element => {
     );
   } else {
     body = (
-      <div className="h-8 w-8 text-cool-gray-30">{Spinner}</div>
+      <div className="h-8 w-8 text-cool-gray-30">
+        <Spinner/>
+      </div>
     );
   }
 
