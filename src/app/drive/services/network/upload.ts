76912e04f2e298b39ba4449e2a6620b3b72a38c3
Sergio Gutiérrez Villalba <gv.sergio@gmail.com>
2022-02-15 12:10:29 +0100

    feat(network): upload up to 3GB

--- src/app/drive/services/network/upload.ts
+++ src/app/drive/services/network/upload.ts
@@ -4,7 +4,7 @@ import { Sha256 } from 'asmcrypto.js';
 import EventEmitter from 'events';
 
 export interface Abortable {
-  stop: () => void
+  stop: () => void;
 }
 
 export async function calculateEncryptedFileHash(plainFile: File, cipher: Cipher): Promise<string> {
@@ -28,6 +28,145 @@ export async function calculateEncryptedFileHash(plainFile: File, cipher: Cipher
   return createHash('ripemd160').update(Buffer.from(hasher.result!)).digest('hex');
 }
 
+// export function uploadFile(plainFile: File, cipher: Cipher, url: string): [Promise<void>, Abortable] {
+//   const readable = plainFile.stream().getReader();
+//   const formattedUrl = new URL(url);
+//   const eventEmitter = new EventEmitter();
+//   const { size: totalBytes } = plainFile;
+
+//   let progress = 0;
+//   let uploadedBytes = 0;
+//   let aborted = false;
+
+//   const uploadFinishedPromise = new Promise<void>((resolve, reject) => {
+//     const req = request(
+//       {
+//         headers: {
+//           'Content-Type': 'application/octet-stream',
+//         },
+//         hostname: formattedUrl.hostname,
+//         port: formattedUrl.port,
+//         protocol: formattedUrl.protocol,
+//         path: formattedUrl.pathname + '?' + formattedUrl.searchParams.toString(),
+//         method: 'PUT',
+//       },
+//       (res) => {
+//         let rawResponse = '';
+
+//         res
+//           .on('data', (chunk) => {
+//             rawResponse += chunk;
+//           })
+//           .once('end', () => {
+//             res.statusCode !== 200 ? reject(JSON.parse(rawResponse)) : resolve();
+//           });
+//       },
+//     );
+
+//     eventEmitter.once('abort', () => {
+//       aborted = true;
+//       req.abort();
+//       readable.cancel();
+//     });
+
+//     let done = false;
+
+//     try {
+//       (async () => {
+//         while (!done && !aborted) {
+//           const status = await readable.read();
+
+//           if (!status.done) {
+//             const overloaded = !req.write(cipher.update(status.value));
+
+//             uploadedBytes += (status.value as Uint8Array).length;
+
+//             if (overloaded) {
+//               await new Promise((res) => req.once('drain', res));
+//             }
+//           }
+
+//           progress = uploadedBytes / totalBytes;
+
+//           console.log('Progress: ' + (progress * 100).toFixed(2) + '%');
+
+//           done = status.done;
+//         }
+
+//         req.end();
+//       })();
+//     } catch (err) {
+//       if (aborted) {
+//         return resolve();
+//       }
+//       reject(err);
+//     }
+//   });
+
+//   return [
+//     uploadFinishedPromise,
+//     {
+//       stop: () => {
+//         eventEmitter.emit('abort');
+//       },
+//     },
+//   ];
+// }
+
+// export function uploadFile(plainFile: File, cipher: Cipher, url: string): [Promise<void>, Abortable] {
+//   const readable = plainFile.stream().getReader();
+//   const formattedUrl = new URL(url);
+//   const eventEmitter = new EventEmitter();
+//   const { size: totalBytes } = plainFile;
+
+//   let progress = 0;
+//   let uploadedBytes = 0;
+//   let aborted = false;
+
+//   eventEmitter.once('abort', () => {
+//     aborted = true;
+//     readable.cancel();
+//   });
+
+//   let done = false;
+//   const blobParts: ArrayBuffer[] = [];
+
+//   const uploadFinishedPromise = (async () => {
+//     while (!done && !aborted) {
+//       const status = await readable.read();
+
+//       if (!status.done) {
+//         blobParts.push(cipher.update(status.value));
+
+//         uploadedBytes += (status.value as Uint8Array).length;
+//       }
+
+//       progress = uploadedBytes / totalBytes;
+
+//       console.log('Progress: ' + (progress * 100).toFixed(2) + '%');
+
+//       done = status.done;
+//     }
+//     const headers = new Headers();
+//     headers.append('Content-Type', 'application/octet-stream');
+
+//     await fetch(formattedUrl.toString(), {
+//       method: 'PUT',
+//       headers,
+//       body: new Blob(blobParts, { type: 'application/octet-stream' }),
+//     });
+//   })();
+
+//   return [
+//     uploadFinishedPromise,
+//     {
+//       stop: () => {
+//         eventEmitter.emit('abort');
+//       },
+//     },
+//   ];
+// }
+
 export function uploadFile(plainFile: File, cipher: Cipher, url: string): [Promise<void>, Abortable] {
   const readable = plainFile.stream().getReader();
   const formattedUrl = new URL(url);
@@ -38,69 +177,76 @@ export function uploadFile(plainFile: File, cipher: Cipher, url: string): [Promi
   let uploadedBytes = 0;
   let aborted = false;
 
-  const uploadFinishedPromise = new Promise<void>((resolve, reject) => {
-    const req = request({
-      headers: {
-        'Content-Type': 'application/octet-stream',
-      },
-      hostname: formattedUrl.hostname,
-      port: formattedUrl.port,
-      protocol: formattedUrl.protocol,
-      path: formattedUrl.pathname + '?' + formattedUrl.searchParams.toString(),
-      method: 'PUT',
-    }, (res) => {
-      let rawResponse = '';
-
-      res.on('data', (chunk) => {
-        rawResponse += chunk;
-      }).once('end', () => {
-        res.statusCode !== 200 ? reject(JSON.parse(rawResponse)) : resolve();
-      });
-    });
-
-    eventEmitter.once('abort', () => {
-      aborted = true;
-      req.abort();
-      readable.cancel();
-    });
-
-    let done = false;
-
-    try {
-      (async () => {
-        while (!done && !aborted) {
-          const status = await readable.read();
-
-          uploadedBytes += (status.value as Uint8Array).length;
-
-          if (!status.done) {
-            const overloaded = !req.write(cipher.update(status.value));
-
-            if (overloaded) {
-              await new Promise(res => req.once('drain', res));
-            }
-          }
-
-          progress = uploadedBytes / totalBytes;
-
-          console.log('Progress: ' + (progress * 100).toFixed(2) + '%');
-
-          done = status.done;
-        }
-
-        req.end();
-      })();
-    } catch (err) {
-      if (aborted) {
-        return resolve();
-      }
-      reject(err);
-    }
+  eventEmitter.once('abort', () => {
+    aborted = true;
+    readable.cancel();
   });
 
-  return [uploadFinishedPromise, {
-    stop: () => {
-      eventEmitter.emit('abort');
+  let done = false;
+  const blobParts: ArrayBuffer[] = [];
+
+  const uploadFinishedPromise = (async () => {
+    while (!done && !aborted) {
+      const status = await readable.read();
+
+      if (!status.done) {
+        blobParts.push(cipher.update(status.value));
+
+        uploadedBytes += (status.value as Uint8Array).length;
+      }
+
+      progress = uploadedBytes / totalBytes;
+
+      console.log('Encrypt Progress: ' + (progress * 100).toFixed(2) + '%');
+
+      done = status.done;
     }
-  }];
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/octet-stream');
+
+    const uploadRequest = new XMLHttpRequest();
+
+    const upload = new Promise((resolve, reject) => {
+      uploadRequest.onload = resolve;
+      uploadRequest.onerror = reject;
+    });
+    uploadRequest.upload.addEventListener('progress', (e) => {
+      console.log('Upload Progress: ' + ((e.loaded / e.total) * 100).toFixed(2) + '%');
+    });
+    uploadRequest.addEventListener('loadstart', (e) => {
+      console.log('load start');
+    });
+    uploadRequest.addEventListener('abort', (e) => {
+      console.log('abort');
+    });
+    uploadRequest.addEventListener('error', (e) => {
+      console.log('error');
+    });
+    uploadRequest.addEventListener('timeout', (e) => {
+      console.log('timeout');
+    });
+    uploadRequest.addEventListener('loadend', (e) => {
+      console.log('loadend');
+    });
+    uploadRequest.addEventListener('load', function (e) {
+      // HTTP status message (200, 404 etc)
+      console.log(uploadRequest.status);
+
+      // request.response holds response from the server
+      console.log(uploadRequest.response);
+    });
+
+    uploadRequest.open('PUT', formattedUrl.toString());
+    uploadRequest.send(new Blob(blobParts, { type: 'application/octet-stream' }));
+    await upload;
+  })();
+
+  return [
+    uploadFinishedPromise,
+    {
+      stop: () => {
+        eventEmitter.emit('abort');
+      },
+    },
+  ];
 }
