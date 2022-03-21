9c957a390029c8ed3b4c00202285314ed7ac9c33
Alex Menor <alex.menor@icloud.com>
2022-03-21 16:41:52 +0100

    add photos store slice

---
+++ src/app/store/slices/photos/index.ts
@@ -0,0 +1,43 @@
+import { Photo, PhotoId } from '@internxt/sdk/dist/photos';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface PhotosState {
+  isLoading: boolean;
+  page: number;
+  thereIsMore: boolean;
+  items: Photo[];
+  selectedItems: PhotoId[];
+}
+
+const initialState: PhotosState = {
+  isLoading: false,
+  page: 0,
+  thereIsMore: true,
+  items: [],
+  selectedItems: [],
+};
+
+export const photosSlice = createSlice({
+  name: 'photos',
+  initialState,
+  reducers: {
+    setIsLoading: (state: PhotosState, action: PayloadAction<boolean>) => {
+      state.isLoading = action.payload;
+    },
+    push: (state: PhotosState, action: PayloadAction<Photo[]>) => {
+      state.items.push(...action.payload);
+    },
+    toggleSelect: (state: PhotosState, action: PayloadAction<PhotoId>) => {
+      const id = action.payload;
+      const isAlreadySelected = state.selectedItems.some((el) => el === id);
+
+      if (isAlreadySelected) {
+        state.selectedItems = state.selectedItems.filter((el) => el !== id);
+      } else {
+        state.selectedItems.push(id);
+      }
+    },
+  },
+});
+
+export default photosSlice.reducer;
