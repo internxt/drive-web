edb4dcbeac6385ff5ed540f04a865ee3fff511c8
Alex Menor <alex.menor@icloud.com>
2022-04-21 15:40:06 +0200

    remove empty component link in photos view

--- src/app/photos/Empty.tsx
+++ src/app/photos/Empty.tsx
@@ -1,5 +1,4 @@
 import picture from '../../assets/images/empty-photos.png';
-import { ArrowSquareOut } from 'phosphor-react';
 
 export default function Empty({ className = '' }: { className?: string }): JSX.Element {
   return (
@@ -13,20 +12,8 @@ export default function Empty({ className = '' }: { className?: string }): JSX.E
           <div className="flex flex-col items-center space-y-2">
             <h2 className="text-3xl font-semibold text-gray-100">Your gallery is empty</h2>
 
-            <p className="text-lg text-gray-60">
-              Start using Internxt mobile app to sync all your photos
-            </p>
+            <p className="text-lg text-gray-60">Start using Internxt mobile app to sync all your photos</p>
           </div>
-
-          <a
-            href=""
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex flex-row items-center text-lg font-medium text-primary no-underline hover:text-primary-dark space-x-1.5"
-          >
-            <p>Sync all your photos using Internxt app</p>
-            <ArrowSquareOut size={24} />
-          </a>
         </div>
       </div>
     </div>
