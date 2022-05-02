e1852b96381c21f84b0b965d493d2b47f8c6856b
Alex Menor <alex.menor@icloud.com>
2022-05-02 17:24:37 +0200

    extract empty states in different views to one empty component

--- src/app/drive/components/DriveExplorer/DriveExplorer.tsx
+++ src/app/drive/components/DriveExplorer/DriveExplorer.tsx
@@ -11,8 +11,8 @@ import { ConnectDropTarget, DropTarget, DropTargetCollector, DropTargetSpec } fr
 
 import DriveExplorerList from './DriveExplorerList/DriveExplorerList';
 import DriveExplorerGrid from './DriveExplorerGrid/DriveExplorerGrid';
-import folderEmptyImage from 'assets/images/folder-empty.svg';
-import noResultsSearchImage from 'assets/images/no-results-search.svg';
+import folderEmptyImage from 'assets/icons/light/folder-open.svg';
+import filesEmptyImage from 'assets/icons/light/files.svg';
 import DriveExplorerOverlay from './DriveExplorerOverlay/DriveExplorerOverlay';
 import { transformDraggedItems } from 'app/core/services/drag-and-drop.service';
 import { StorageFilters } from 'app/store/slices/storage/storage.model';
@@ -32,6 +32,7 @@ import { planSelectors } from '../../../store/slices/plan';
 import { DriveItemData, FileViewMode, FolderPath } from '../../types';
 import i18n from '../../../i18n/services/i18n.service';
 import { UserSettings } from '@internxt/sdk/dist/shared/types/userSettings';
+import { UploadSimple } from 'phosphor-react';
 
 interface DriveExplorerProps {
   title: JSX.Element | string;
@@ -167,6 +168,8 @@ class DriveExplorer extends Component<DriveExplorerProps, DriveExplorerState> {
     };
     const ViewModeComponent = viewModes[viewMode];
 
+    const isRecents = title === 'Recents';
+
     return connectDropTarget(
       <div className="flex h-full flex-grow flex-col px-8" data-test="drag-and-drop-area">
         {isDeleteItemsDialogOpen && <DeleteItemsDialog onItemsDeleted={onItemsDeleted} />}
@@ -234,23 +237,38 @@ class DriveExplorer extends Component<DriveExplorerProps, DriveExplorerState> {
 
               {
                 /* EMPTY FOLDER */
-                !this.hasFilters && !this.hasItems && !isLoading ? (
-                  <DriveExplorerOverlay
-                    icon={<img alt="" src={folderEmptyImage} className="m-auto w-full" />}
-                    title="This folder is empty"
-                    subtitle="Drag and drop here or click on upload button"
-                  />
-                ) : null
-              }
-
-              {
-                /* NO SEARCH RESULTS */
-                this.hasFilters && !this.hasItems && !isLoading ? (
-                  <DriveExplorerOverlay
-                    icon={<img alt="" src={noResultsSearchImage} className="m-auto w-full" />}
-                    title="There are no results for this search"
-                    subtitle="Drag and drop here or click on upload button"
-                  />
+                !this.hasItems && !isLoading ? (
+                  this.hasFilters ? (
+                    <DriveExplorerOverlay
+                      icon={<img className="w-36" alt="" src={filesEmptyImage} />}
+                      title="There are no results for this search"
+                      subtitle="Drag and drop here or click on upload button"
+                      action={{
+                        icon: UploadSimple,
+                        style: 'elevated',
+                        text: 'Upload files',
+                        onClick: this.onUploadButtonClicked,
+                      }}
+                    />
+                  ) : isRecents ? (
+                    <DriveExplorerOverlay
+                      icon={<img className="w-36" alt="" src={filesEmptyImage} />}
+                      title="No recents files to show"
+                      subtitle="Recent uploads or files you recently interacted with will show up here automatically"
+                    />
+                  ) : (
+                    <DriveExplorerOverlay
+                      icon={<img className="w-36" alt="" src={folderEmptyImage} />}
+                      title="This folder is empty"
+                      subtitle="Drag and drop files or click to select files and upload"
+                      action={{
+                        icon: UploadSimple,
+                        style: 'elevated',
+                        text: 'Upload files',
+                        onClick: this.onUploadButtonClicked,
+                      }}
+                    />
+                  )
                 ) : null
               }
 
--- src/app/drive/components/DriveExplorer/DriveExplorerOverlay/DriveExplorerOverlay.tsx
+++ src/app/drive/components/DriveExplorer/DriveExplorerOverlay/DriveExplorerOverlay.tsx
@@ -1,21 +1,45 @@
-import './DriveExplorerOverlay.scss';
+import { Upload } from 'phosphor-react';
+import { ReactNode } from 'react';
 
 interface DriveExplorerOverlayProps {
   icon: JSX.Element;
   title: string;
   subtitle: string;
+  action?: {
+    text: string;
+    icon: typeof Upload;
+    style: 'plain' | 'elevated';
+    onClick: () => void;
+  };
 }
 
-function DriveExplorerOverlay({ icon, title, subtitle }: DriveExplorerOverlayProps): JSX.Element {
+function DriveExplorerOverlay({ icon, title, subtitle, action }: DriveExplorerOverlayProps): JSX.Element {
+  let button: ReactNode = null;
+
+  if (action) {
+    button = (
+      <button
+        onClick={action.onClick}
+        className={`mx-auto flex items-center rounded-lg ${
+          action.style === 'elevated' ? 'mt-5 bg-primary' : 'mt-2.5 bg-transparent'
+        }  bg-opacity-10 px-6 py-2.5 font-medium text-primary hover:bg-opacity-15 active:bg-opacity-20`}
+      >
+        {action.text}
+        <action.icon className="ml-2" size={20} weight="bold" />
+      </button>
+    );
+  }
+
   return (
-    <div className="pointer-events-none p-8 bg-white h-full w-full">
-      <div className="h-full flex items-center justify-center rounded-12px">
+    <div className="h-full w-full  p-8">
+      <div className="flex h-full items-center justify-center rounded-12px">
         <div className="mb-28">
-          <div className="file-explorer-overlay-icon-container flex m-auto bg-blue-10 rounded-1/2 mb-10">{icon}</div>
-          <div className="text-center">
-            <span className="font-semibold text-2xl text-neutral-500 block mb-2">{title}</span>
-            <span className="text-base text-neutral-500 block">{subtitle}</span>
+          <div className="mx-auto mb-10 w-max">{icon}</div>
+          <div className="pointer-events-none text-center">
+            <p className="mb-1 block text-3xl font-semibold text-gray-100">{title}</p>
+            <p className="block text-lg text-gray-60">{subtitle}</p>
           </div>
+          {button}
         </div>
       </div>
     </div>
