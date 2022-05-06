8e59e08b97020d6f9b8d3145452c34c75e786d99
Alex Menor <alex.menor@icloud.com>
2022-05-06 16:58:37 +0200

    add custom input component and use it in invite a friend component

--- src/app/core/views/Preferences/tabs/Account/InviteAFriend.tsx
+++ src/app/core/views/Preferences/tabs/Account/InviteAFriend.tsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import Button from '../../../../../shared/components/Button/Button';
 import Card from '../../../../../shared/components/Card';
+import Input from '../../../../../shared/components/Input';
 import Section from '../../components/Section';
 
 export default function InviteAFriend({ className = '' }: { className?: string }): JSX.Element {
+  const [email, setEmail] = useState('');
+
   return (
     <Section className={className} title="Invite a friend">
       <Card>
         <p className="text-gray-80">
           Spread the word, get 1GB per friend invited for free. You can get up to 4GB in total.
         </p>
+        <Input
+          className="mt-3"
+          value={email}
+          onChange={setEmail}
+          label="Friend email address"
+          placeholder="Enter friend email"
+        />
         <div className="mt-5 flex">
           <Button variant="secondary">Send invitation</Button>
           <button className="ml-4 font-medium text-primary underline">See all invitations</button>
---
+++ src/app/shared/components/Input/index.tsx
@@ -0,0 +1,146 @@
+import { Eye, EyeSlash, MagnifyingGlass, X, WarningOctagon, Warning, CheckCircle } from 'phosphor-react';
+import { useState } from 'react';
+
+export default function Input({
+  className = '',
+  label,
+  variant = 'default',
+  accent,
+  disabled,
+  placeholder,
+  value,
+  onChange,
+  onClear,
+  message,
+}: {
+  className?: string;
+  label?: string;
+  variant?: 'default' | 'search' | 'password';
+  accent?: 'error' | 'warning' | 'valid';
+  disabled?: boolean;
+  placeholder?: string;
+  value?: string;
+  onChange?: (v: string) => void;
+  onClear?: () => void;
+  message?: string;
+}): JSX.Element {
+  let focusColor: string;
+
+  switch (accent) {
+    case 'error':
+      focusColor = 'focus:border-red-std ring-red-std';
+      break;
+    case 'warning':
+      focusColor = 'focus:border-orange ring-orange';
+      break;
+    case 'valid':
+      focusColor = 'focus:border-green ring-green';
+      break;
+    default:
+      focusColor = 'focus:border-primary ring-primary';
+      break;
+  }
+
+  const borderColor =
+    variant === 'search' ? 'border-transparent' : 'border-gray-20 disabled:border-gray-10 hover:border-gray-30';
+
+  const backgroundColor =
+    variant === 'search' ? 'bg-gray-5 focus:bg-white disabled:bg-gray-5' : 'bg-white disabled:bg-white';
+
+  const placeholderColor = variant === 'search' ? 'placeholder-gray-40' : 'placeholder-gray-30';
+
+  const textWeight = variant === 'search' ? 'font-normal' : 'font-medium';
+
+  const padding = variant === 'search' ? 'pr-4 pl-10' : 'px-4';
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+
+  const input = (
+    <div className="relative">
+      <input
+        disabled={disabled}
+        className={`inxt-input outline-none h-11 w-full rounded-md border text-lg text-gray-80 ring-opacity-10 focus:ring-2 disabled:text-gray-40 disabled:placeholder-gray-20 
+				${borderColor} ${focusColor} ${placeholderColor} ${backgroundColor} ${textWeight} ${padding}`}
+        type={variant === 'password' && !showPassword ? 'password' : 'text'}
+        placeholder={placeholder}
+        onChange={(e) => onChange && onChange(e.target.value)}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        value={value}
+      />
+      {variant === 'password' && isFocused && (
+        <div
+          role="button"
+          tabIndex={0}
+          onMouseDown={(e) => {
+            e.preventDefault();
+            setShowPassword(!showPassword);
+          }}
+          className={`absolute top-1/2 right-4 -translate-y-1/2 transform cursor-pointer py-2 pl-2 text-gray-80 ${backgroundColor}`}
+        >
+          {showPassword ? <Eye size={24} /> : <EyeSlash size={24} />}
+        </div>
+      )}
+      {variant === 'search' && (
+        <MagnifyingGlass
+          className={`absolute top-1/2 left-4 -translate-y-1/2 transform ${disabled ? 'text-gray-20' : 'text-gray-40'}`}
+          size={20}
+        />
+      )}
+      {variant === 'search' && value && !disabled && (
+        <div
+          role="button"
+          tabIndex={0}
+          onMouseDown={(e) => {
+            e.preventDefault();
+            if (onClear) onClear();
+          }}
+          className={`absolute top-1/2 right-4 -translate-y-1/2 transform cursor-pointer py-2 pl-2 text-gray-40  ${
+            isFocused ? 'bg-white' : 'bg-gray-5'
+          }`}
+        >
+          <X size={20} />
+        </div>
+      )}
+    </div>
+  );
+
+  let messageColor: string;
+  let MessageIcon: typeof WarningOctagon | undefined;
+
+  switch (accent) {
+    case 'valid':
+      messageColor = 'text-green';
+      MessageIcon = CheckCircle;
+      break;
+    case 'warning':
+      messageColor = 'text-orange';
+      MessageIcon = Warning;
+      break;
+    case 'error':
+      messageColor = 'text-red-std';
+      MessageIcon = WarningOctagon;
+      break;
+    default:
+      messageColor = 'text-gray-80';
+  }
+
+  return (
+    <div className={`${className}`}>
+      {label ? (
+        <label className={`text-sm ${disabled ? 'text-gray-40' : 'text-gray-80'}`}>
+          {label} {input}
+        </label>
+      ) : (
+        input
+      )}
+      {message && (
+        <div className={`flex items-center ${messageColor}`}>
+          {MessageIcon && <MessageIcon size={16} />}
+          <p className="ml-1 text-sm">{message}</p>
+        </div>
+      )}
+    </div>
+  );
+}
