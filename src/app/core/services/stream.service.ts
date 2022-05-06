20ef789a5de9e8dc1765336bd1b28f20987345b2
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-05-06 15:03:55 +0200

    feat(photos): infinite zip download + working on Firefox

--- src/app/core/services/stream.service.ts
+++ src/app/core/services/stream.service.ts
@@ -1,3 +1,7 @@
+import createZipReadable from 'app/drive/services/download.service/downloadFolder/zipStream';
+import streamSaver from 'streamsaver';
+import { loadWritableStreamPonyfill } from 'app/network/download';
+
 type BinaryStream = ReadableStream<Uint8Array>;
 
 export async function binaryStreamToBlob(stream: BinaryStream): Promise<Blob> {
@@ -72,4 +76,73 @@ export function joinReadableBinaryStreams(streams: BinaryStream[]): ReadableStre
   });
 
   return stream;
+}
+
+type FlatFolderZipOpts = {
+  abortController?: AbortController;
+  progress?: (loadedBytes: number) => void;
+}
+
+export class FlatFolderZip {
+  private finished!: Promise<void>;
+  private zip: {
+    addFile: (name: string, source: ReadableStream<Uint8Array>) => void,
+    stream: ReadableStream<Uint8Array>,
+    end: () => void
+  }
+
+  constructor(folderName: string, opts: FlatFolderZipOpts) {
+    this.zip = createFolderWithFilesWritable();
+
+    const passThrough = opts.progress ?
+      buildProgressStream(this.zip.stream, opts.progress) :
+      this.zip.stream;
+
+    const isFirefox = navigator.userAgent.indexOf('Firefox') != -1;
+
+    if (isFirefox) {
+      loadWritableStreamPonyfill().then(() => {
+        streamSaver.WritableStream = window.WritableStream;
+
+        this.finished = passThrough.pipeTo(
+          streamSaver.createWriteStream(folderName + '.zip'),
+          { signal: opts.abortController?.signal }
+        );
+      });
+    } else {
+      this.finished = passThrough.pipeTo(
+        streamSaver.createWriteStream(folderName + '.zip'),
+        { signal: opts.abortController?.signal }
+      );
+    }
+  }
+
+  addFile(name: string, source: ReadableStream<Uint8Array>): void {
+    this.zip.addFile(name, source);
+  }
+
+  async close(): Promise<void> {
+    this.zip.end();
+    await this.finished;
+  }
+}
+
+function createFolderWithFilesWritable(): ({
+  addFile: (name: string, source: ReadableStream<Uint8Array>) => void,
+  stream: ReadableStream<Uint8Array>,
+  end: () => void
+}) {
+  let controller;
+
+  const zipStream = createZipReadable({ start(ctrl) { controller = ctrl; } });
+
+  return {
+    addFile: (name: string, source: ReadableStream<Uint8Array>): void => {
+      controller.enqueue({ name, stream: () => source });
+    },
+    stream: zipStream,
+    end: () => {
+      controller.close();
+    }
+  };
 }
