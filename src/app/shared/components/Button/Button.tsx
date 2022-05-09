f06c4ec45f29b8030bcce224ab940a88db6636ad
Alex Menor <alex.menor@icloud.com>
2022-05-09 11:56:44 +0200

    add loading state to button component

--- src/app/shared/components/Button/Button.tsx
+++ src/app/shared/components/Button/Button.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import Spinner from '../Spinner/Spinner';
 
 export default function Button({
   variant = 'primary',
@@ -7,6 +8,7 @@ export default function Button({
   disabled = false,
   onClick = () => undefined,
   size = 'default',
+  loading,
 }: {
   variant?: 'primary' | 'accent' | 'secondary';
   children: ReactNode;
@@ -14,33 +16,38 @@ export default function Button({
   disabled?: boolean;
   onClick?: () => void;
   size?: 'medium' | 'default';
+  loading?: boolean;
 }): JSX.Element {
   let styles = '';
 
   if (variant === 'primary' && !disabled) {
-    styles = 'bg-primary active:bg-primary-dark text-white';
+    styles = `${loading ? 'bg-primary-dark' : 'bg-primary'} active:bg-primary-dark text-white`;
   } else if (variant === 'primary' && disabled) {
     styles = 'bg-gray-30 text-white';
   } else if (variant === 'accent' && !disabled) {
-    styles = 'bg-red-std active:bg-red-dark text-white';
+    styles = `${loading ? 'bg-red-dark' : 'bg-red-std'} active:bg-red-dark text-white`;
   } else if (variant === 'accent' && disabled) {
     styles = 'bg-gray-30 text-white';
   } else if (variant === 'secondary' && !disabled) {
-    styles = 'bg-gray-5 hover:bg-gray-10 active:bg-gray-20 text-gray-80';
+    styles = `${loading ? 'bg-gray-20' : 'bg-gray-5'} hover:bg-gray-10 active:bg-gray-20 text-gray-80`;
   } else if (variant === 'secondary' && disabled) {
     styles = 'bg-gray-5 text-gray-30';
   }
   return (
     <button
       onClick={onClick}
-      disabled={disabled}
+      disabled={disabled || loading}
       type="button"
       className={`${
         size === 'default' ? 'h-10 px-5' : 'h-8 px-3.5'
-      } select-none rounded-lg text-base font-medium ${styles} ${className}`}
+      } relative select-none rounded-lg text-base font-medium ${styles} ${className}`}
     >
-      {children}
-      <div className=""></div>
+      {loading && (
+        <div className="absolute left-1/2 top-1/2 h-4 w-4 -translate-x-1/2 -translate-y-1/2 transform">
+          <Spinner />
+        </div>
+      )}
+      <div className={loading ? 'opacity-0' : ''}>{children}</div>
     </button>
   );
 }
