2457d191a3c7e02761684137e9f6c8856a0e7e90
Alex Menor <alex.menor@icloud.com>
2022-05-09 09:40:50 +0200

    add 'limit reached' state for invite a friend component

--- src/app/core/views/Preferences/tabs/Account/InviteAFriend.tsx
+++ src/app/core/views/Preferences/tabs/Account/InviteAFriend.tsx
@@ -1,3 +1,4 @@
+import { Info } from 'phosphor-react';
 import { useState } from 'react';
 import Button from '../../../../../shared/components/Button/Button';
 import Card from '../../../../../shared/components/Card';
@@ -7,22 +8,31 @@ import Section from '../../components/Section';
 export default function InviteAFriend({ className = '' }: { className?: string }): JSX.Element {
   const [email, setEmail] = useState('');
 
+  const canInviteMore = true;
+
   return (
     <Section className={className} title="Invite a friend">
       <Card>
         <p className="text-gray-80">
           Spread the word, get 1GB per friend invited for free. You can get up to 4GB in total.
         </p>
-        <Input
-          className="mt-3"
-          value={email}
-          onChange={setEmail}
-          label="Friend email address"
-          placeholder="Enter friend email"
-        />
+        <div className="mt-3">
+          {canInviteMore ? (
+            <Input value={email} onChange={setEmail} label="Friend email address" placeholder="Enter friend email" />
+          ) : (
+            <div className="flex h-9 items-center rounded-lg bg-gray-5 px-3 py-2.5 text-gray-80">
+              <Info size={18} />
+              <p className="ml-1.5 text-sm">You reached the limit of 10 invitations/day</p>
+            </div>
+          )}
+        </div>
         <div className="mt-5 flex">
-          <Button variant="secondary">Send invitation</Button>
-          <button className="ml-4 font-medium text-primary underline">See all invitations</button>
+          {canInviteMore && (
+            <Button className="mr-4" variant="secondary">
+              Send invitation
+            </Button>
+          )}
+          <button className="font-medium text-primary underline">See all invitations</button>
         </div>
       </Card>
     </Section>
