a796408e4c76c27c3b151d672603c6e51c68cfa9
Alex Menor <alex.menor@icloud.com>
2022-05-11 17:07:06 +0200

    wire BackupKey section

--- src/app/core/views/Preferences/tabs/Security/BackupKey.tsx
+++ src/app/core/views/Preferences/tabs/Security/BackupKey.tsx
@@ -1,8 +1,21 @@
+import { saveAs } from 'file-saver';
+import notificationsService, { ToastType } from '../../../../../notifications/services/notifications.service';
 import Button from '../../../../../shared/components/Button/Button';
 import Card from '../../../../../shared/components/Card';
+import localStorageService from '../../../../services/local-storage.service';
 import Section from '../../components/Section';
 
 export default function BackupKey({ className = '' }: { className?: string }): JSX.Element {
+  function handleExport() {
+    const mnemonic = localStorageService.getUser()?.mnemonic;
+    if (!mnemonic) {
+      notificationsService.show({ text: 'We could not generate your backup key', type: ToastType.Error });
+    } else {
+      saveAs(new Blob([mnemonic], { type: 'text/plain' }), 'INTERNXT-BACKUP-KEY.txt');
+      notificationsService.show({ text: 'Backup key downloaded succesfully', type: ToastType.Success });
+    }
+  }
+
   return (
     <Section className={className} title="Backup key">
       <Card>
@@ -10,7 +23,9 @@ export default function BackupKey({ className = '' }: { className?: string }): J
           In case you forget your password you can use your backup key to recover your account. Never share this code
           with anyone.
         </p>
-        <Button className="mt-3">Export backup key</Button>
+        <Button onClick={handleExport} className="mt-3">
+          Export backup key
+        </Button>
       </Card>
     </Section>
   );
