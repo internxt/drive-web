2faa2f60ee2229e63fb368345a6e8f7aa11124c1
Alex Menor <alex.menor@icloud.com>
2022-05-12 17:34:20 +0200

    add Enable modal in TwoFA component

--- src/app/core/views/Preferences/tabs/Security/TwoFA.tsx
+++ src/app/core/views/Preferences/tabs/Security/TwoFA.tsx
@@ -1,10 +1,33 @@
-import { CheckCircle } from 'phosphor-react';
+import { CheckCircle, Warning } from 'phosphor-react';
+import { useEffect, useState } from 'react';
+import authService, { generateNew2FA, userHas2FAStored } from '../../../../../auth/services/auth.service';
 import Button from '../../../../../shared/components/Button/Button';
 import Card from '../../../../../shared/components/Card';
+import Modal from '../../../../../shared/components/Modal';
+import Spinner from '../../../../../shared/components/Spinner/Spinner';
 import Section from '../../components/Section';
+import appStoreIcon from '../../../../../../assets/icons/app-store.svg';
+import playStoreIcon from '../../../../../../assets/icons/play-store.svg';
+import useEffectAsync from '../../../../hooks/useEffectAsync';
+import Copyable from '../../../../../shared/components/Copyable';
+import i18n from '../../../../../i18n/services/i18n.service';
+import notificationsService, { ToastType } from '../../../../../notifications/services/notifications.service';
+import Input from '../../../../../shared/components/Input';
 
 export default function TwoFA({ className = '' }: { className?: string }): JSX.Element {
-  const isEnabled = false;
+  const [status, setStatus] = useState<'loading' | 'enabled' | 'disabled'>('loading');
+
+  useEffect(() => {
+    userHas2FAStored().then(({ tfaEnabled }) => {
+      if (tfaEnabled) {
+        setStatus('enabled');
+      } else {
+        setStatus('disabled');
+      }
+    });
+  }, []);
+
+  const [enableModalOpen, setEnableModalOpen] = useState(false);
 
   return (
     <Section className={className} title="Two Factor Authentication (2FA)">
@@ -14,7 +37,7 @@ export default function TwoFA({ className = '' }: { className?: string }): JSX.E
           in. In adittion to your password, you'll also need a generated code.
         </p>
         <div className="mt-3">
-          {isEnabled ? (
+          {status === 'enabled' ? (
             <div className="flex">
               <div className="flex items-center font-medium text-green">
                 <CheckCircle size={20} weight="fill" />
@@ -24,11 +47,151 @@ export default function TwoFA({ className = '' }: { className?: string }): JSX.E
                 </Button>
               </div>
             </div>
+          ) : status === 'disabled' ? (
+            <Button onClick={() => setEnableModalOpen(true)}>Enable</Button>
           ) : (
-            <Button>Enable</Button>
+            <div className="flex h-10 items-center">
+              <Spinner className="block h-5 w-5 text-primary" />
+            </div>
           )}
         </div>
       </Card>
+      <EnableModal isOpen={enableModalOpen} onClose={() => setEnableModalOpen(false)} />
     </Section>
+  );
+}
+
+function EnableModal({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }): JSX.Element {
+  useEffect(() => {
+    if (isOpen) {
+      setStep(0);
+      setQR(null);
+      setActivateState('ready');
+      setActivateValue('');
+    }
+  }, [isOpen]);
+
+  const [step, setStep] = useState(0);
+  const steps = ['Download Authy', 'Scan QR with the 2FA app', 'Save backup key', 'Confirm 2FA activation'];
+
+  const downloadStep = (
+    <div className="pt-2 pb-5">
+      <p className="text-gray-80">
+        We recomend using Authy, but you can setup the 2FA process with any other 2FA of your choice
+      </p>
+      <div className="mt-2 flex flex-row items-center justify-center space-x-4">
+        <a href="https://apps.apple.com/us/app/authy/id494168017" target="_blank" rel="noreferrer">
+          <img src={appStoreIcon} height={40} width={135} alt="App Store" />
+        </a>
+
+        <a href="https://play.google.com/store/apps/details?id=com.authy.authy" target="_blank" rel="noreferrer">
+          <img src={playStoreIcon} height={40} width={135} alt="Google Play" />
+        </a>
+      </div>
+    </div>
+  );
+
+  const [qr, setQR] = useState<null | { img: string; key: string }>(null);
+
+  useEffectAsync(async () => {
+    if (!isOpen) return;
+
+    const { qr: img, backupKey: key } = await generateNew2FA();
+    setQR({ img, key });
+  }, [isOpen]);
+
+  const qrStep = (
+    <div className="mb-1 flex h-40 items-center justify-center">
+      {qr ? (
+        <>
+          <img className="-m-3" src={qr.img} alt="Bidi code" />
+          <p className="ml-8 text-gray-50">OR</p>
+          <div className="ml-8 flex flex-col items-center">
+            <Copyable className="w-60" text={qr.key} />
+            <p className="mt-2 px-2 text-center text-sm text-gray-60">
+              If you are unable to scan the QR, insert this code into the app
+            </p>
+          </div>
+        </>
+      ) : (
+        <Spinner className="h-7 w-7 text-gray-50" />
+      )}
+    </div>
+  );
+
+  const backupKeyStep = (
+    <div className="py-5">
+      {qr && <Copyable className="mx-auto w-60" text={qr.key} />}
+      <div className="mt-2 flex items-center">
+        <Warning size={24} className="text-yellow" style={{ marginLeft: '83px' }} weight="fill" />
+        <p className="ml-2 w-64 text-sm text-gray-60">
+          Copy this code and keep it secure, you will need it in case you lose your device.
+        </p>
+      </div>
+    </div>
+  );
+
+  const [activateState, setActivateState] = useState<'ready' | 'error' | 'loading'>('ready');
+  const [activateValue, setActivateValue] = useState('');
+
+  async function handleActivate() {
+    if (qr) {
+      try {
+        setActivateState('loading');
+        await authService.store2FA(qr.key, activateValue);
+        notificationsService.show({ text: i18n.get('success.twoFactorAuthActivated'), type: ToastType.Success });
+        onClose();
+      } catch (err) {
+        setActivateState('error');
+      }
+    }
+  }
+
+  const activateStep = (
+    <div className="py-5">
+      <Input
+        label="Two-factor authenticacion code"
+        value={activateValue}
+        disabled={activateState === 'loading'}
+        accent={activateState === 'error' ? 'error' : undefined}
+        onChange={setActivateValue}
+        message={activateState === 'error' ? 'Incorrect authentication code, please try again' : undefined}
+      />
+    </div>
+  );
+
+  const parts = [downloadStep, qrStep, backupKeyStep, activateStep];
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <h1 className="text-2xl font-medium text-gray-80">Two factor authentication</h1>
+      <h2 className="mt-5 flex items-center">
+        <p className="mt-0.5 text-gray-50">
+          {step + 1} of {steps.length}
+        </p>
+        <p className="ml-2 text-xl font-medium text-gray-80">{steps[step]}</p>
+      </h2>
+      {parts[step]}
+      <div className="flex justify-end">
+        <Button
+          variant="secondary"
+          onClick={step === 0 ? onClose : () => setStep(step - 1)}
+          disabled={activateState === 'loading'}
+        >
+          {step === 0 ? 'Cancel' : 'Back'}
+        </Button>
+        <div className="ml-2">
+          {step !== steps.length - 1 ? (
+            <Button onClick={() => setStep(step + 1)} disabled={qr === null && step === 1}>
+              Next
+            </Button>
+          ) : (
+            <Button onClick={handleActivate} disabled={activateValue.length < 6} loading={activateState === 'loading'}>
+              Enable 2FA
+            </Button>
+          )}
+        </div>
+      </div>
+    </Modal>
   );
 }
