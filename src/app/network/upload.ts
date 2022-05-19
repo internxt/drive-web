740f2308471f9f23512193b6550be6c6e788b17d
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-05-19 08:42:46 +0200

    feat(network): upload-file-v2

--- src/app/network/upload.ts
+++ src/app/network/upload.ts
@@ -29,19 +29,23 @@ interface IUploadParams {
   creds: NetworkCredentials;
   mnemonic: string;
   progressCallback: UploadProgressCallback;
+  abortController?: AbortController;
 }
 
 export function uploadFileBlob(
   encryptedFile: Blob,
   url: string,
   opts: {
-    progressCallback: UploadProgressCallback
+    progressCallback: UploadProgressCallback,
+    abortController?: AbortController
   },
-): [Promise<void>, Abortable] {
+): Promise<void> {
   const uploadRequest = new XMLHttpRequest();
-  const eventEmitter = new EventEmitter().once('abort', () => {
+
+  opts.abortController?.signal.addEventListener('abort', () => {
+    console.log('aborting');
     uploadRequest.abort();
-  });
+  }, { once: true });
 
   uploadRequest.upload.addEventListener('progress', (e) => {
     opts.progressCallback(e.total, e.loaded);
@@ -66,14 +70,7 @@ export function uploadFileBlob(
   uploadRequest.open('PUT', url);
   uploadRequest.send(encryptedFile);
 
-  return [
-    uploadFinishedPromise,
-    {
-      abort: () => {
-        eventEmitter.emit('abort');
-      },
-    },
-  ];
+  return uploadFinishedPromise;
 }
 
 function getAuthFromCredentials(creds: NetworkCredentials): { username: string, password: string } {
@@ -83,129 +80,33 @@ function getAuthFromCredentials(creds: NetworkCredentials): { username: string, 
   };
 }
 
-export function uploadFile(bucketId: string, params: IUploadParams): [Promise<string>, Abortable | undefined] {
-  let aborted = false;
-  let uploadAbortable: Abortable;
-
+export function uploadFile(bucketId: string, params: IUploadParams): Promise<string> {
   const file: File = params.filecontent;
-  const eventEmitter = new EventEmitter().once('abort', () => {
-    aborted = true;
-    uploadAbortable?.abort();
+
+  const auth = getAuthFromCredentials({
+    user: params.creds.user,
+    pass: params.creds.pass
   });
 
-  const uploadPromise = (async () => {
-    const index = randomBytes(32);
-    const iv = index.slice(0, 16);
-
-    const frameId = await prepareUpload(bucketId, params.creds);
-    if (aborted) {
-      throw new UploadAbortedError();
-    }
-
-    const encryptionKey = await Environment.utils.generateFileKey(params.mnemonic, bucketId, index);
-    if (aborted) {
-      throw new UploadAbortedError();
-    }
-
-    const [encryptedFile, fileHash] = await getEncryptedFile(file, createAES256Cipher(encryptionKey, iv));
-
-    const shardMeta = {
-      hash: fileHash,
-      index: 0,
-      parity: false,
-      size: params.filesize,
-    };
-    if (aborted) {
-      throw new UploadAbortedError();
-    }
-
-    const uploadUrl = new URL(await getUploadUrl(frameId, shardMeta, params.creds));
-    if (aborted) {
-      throw new UploadAbortedError();
-    }
-
-    const useProxy = !uploadUrl.hostname.includes('internxt');
-
-    const [uploadPromise, uploadFileAbortable] = await uploadFileBlob(
-      encryptedFile,
-      (useProxy && process.env.REACT_APP_PROXY + '/') + uploadUrl.toString(),
+  return new NetworkFacade(
+    Network.client(
+      process.env.REACT_APP_STORJ_BRIDGE as string,
       {
-        progressCallback: (totalBytes, uploadedBytes) => params.progressCallback(totalBytes, uploadedBytes),
+        clientName: 'drive-web',
+        clientVersion: '1.0'
       },
-    );
-
-    uploadAbortable = uploadFileAbortable;
-    await uploadPromise;
-
-    const encryptedFilename = await encryptFilename(params.mnemonic, bucketId, v4());
-    if (aborted) {
-      throw new UploadAbortedError();
-    }
-
-    return finishUpload(
-      params.mnemonic,
-      bucketId,
-      frameId,
-      encryptedFilename,
-      index,
-      encryptionKey,
-      shardMeta,
-      params.creds,
-    );
-  })();
-
-  return [
-    uploadPromise,
-    {
-      abort: () => {
-        eventEmitter.emit('abort');
-      },
-    },
-  ];
-}
-
-export function uploadFileV2(bucketId: string, params: IUploadParams): [Promise<string>, Abortable | undefined] {
-  let uploadAbortable: Abortable;
-
-  const file: File = params.filecontent;
-  const eventEmitter = new EventEmitter().once('abort', () => {
-    uploadAbortable?.abort();
-  });
-
-  const uploadPromise = (() => {
-    const auth = getAuthFromCredentials({
-      user: params.creds.user,
-      pass: params.creds.pass
-    });
-
-    return new NetworkFacade(
-      Network.client(
-        process.env.REACT_APP_STORJ_BRIDGE as string,
-        {
-          clientName: 'drive-web',
-          clientVersion: '1.0'
-        },
-        {
-          bridgeUser: auth.username,
-          userId: auth.password
-        }
-      ),
-    ).upload(
-      bucketId,
-      params.mnemonic,
-      file,
       {
-        uploadingCallback: params.progressCallback
+        bridgeUser: auth.username,
+        userId: auth.password
       }
-    );
-  })();
-
-  return [
-    uploadPromise,
+    ),
+  ).upload(
+    bucketId,
+    params.mnemonic,
+    file,
     {
-      abort: () => {
-        eventEmitter.emit('abort');
-      },
-    },
-  ];
+      uploadingCallback: params.progressCallback,
+      abortController: params.abortController
+    }
+  );
 }
