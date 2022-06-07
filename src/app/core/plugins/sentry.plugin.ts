ff80a015a958c20a80f609af101433c4268fd0ca
carlos.lopez <carlos.lopez@voicemod.net>
2022-06-07 19:06:04 +0200

    feat(sentry): added sentry integration

---
+++ src/app/core/plugins/sentry.plugin.ts
@@ -0,0 +1,16 @@
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
+import { AppPlugin } from '../../core/types';
+
+const sentryPlugin: AppPlugin = {
+  install(): void {
+    Sentry.init({
+      dsn: process.env.REACT_APP_SENTRY_DSN,
+      integrations: [new BrowserTracing()],
+      tracesSampleRate: 1.0,
+      debug: false
+    });
+  },
+};
+
+export default sentryPlugin;
