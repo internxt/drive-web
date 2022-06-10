8a320028d51a2d42044180229a0e45891ac1115d
Alex Menor <alex.menor@icloud.com>
2022-06-10 15:52:21 +0200

    dont allow deleting account if there is an ongoing subscription

--- src/app/core/views/Preferences/tabs/Account/DeleteAccount.tsx
+++ src/app/core/views/Preferences/tabs/Account/DeleteAccount.tsx
@@ -1,18 +1,30 @@
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import authService from '../../../../../auth/services/auth.service';
 import notificationsService, { ToastType } from '../../../../../notifications/services/notifications.service';
 import Button from '../../../../../shared/components/Button/Button';
 import Card from '../../../../../shared/components/Card';
 import Dialog from '../../../../../shared/components/Dialog/Dialog';
+import { RootState } from '../../../../../store';
+import { PlanState } from '../../../../../store/slices/plan';
 import errorService from '../../../../services/error.service';
 import Section from '../../components/Section';
 
 export default function DeleteAccount({ className = '' }: { className?: string }): JSX.Element {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const plan = useSelector<RootState, PlanState>((state) => state.plan);
 
   function onClose() {
     setIsDialogOpen(false);
+  }
+
+  function onClick() {
+    if (plan.subscription?.type === 'subscription') {
+      notificationsService.show({ text: 'Please, cancel your subscription first', type: ToastType.Info });
+    } else {
+      setIsDialogOpen(true);
+    }
   }
 
   async function onConfirm() {
@@ -35,7 +47,7 @@ export default function DeleteAccount({ className = '' }: { className?: string }
         <p className="text-gray-80">
           If you delete your account, your data will be gone forever. This action cannot be undone.
         </p>
-        <Button className="mt-5" variant="secondary" onClick={() => setIsDialogOpen(true)}>
+        <Button className="mt-5" variant="secondary" onClick={onClick}>
           Delete account
         </Button>
       </Card>
