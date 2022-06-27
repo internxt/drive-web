cb71505d2cf45914912a6e106409487872953ba4
aboredvaro <34165099+aboredvaro@users.noreply.github.com>
2022-06-27 14:14:10 +0200

    Fixed some responsiveness issues

--- src/app/core/views/Preferences/tabs/Account/index.tsx
+++ src/app/core/views/Preferences/tabs/Account/index.tsx
@@ -8,7 +8,7 @@ export default function AccountTab({ className = '' }: { className?: string }): 
   return (
     <div className={className}>
       <UserHeader />
-      <div className="mt-8 flex flex-wrap gap-y-8 gap-x-10">
+      <div className="mt-8 flex flex-col xl:flex-row flex-wrap gap-y-8 gap-x-10">
         <div className="flex flex-1 flex-col space-y-8">
           <Usage />
           <InviteAFriend />
