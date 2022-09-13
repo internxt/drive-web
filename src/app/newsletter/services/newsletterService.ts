fd9748388cf82b59e9e712caad4eeeb22ccd6e59
Carlos Lopez <cloaction@gmail.com>
2022-09-13 11:16:24 +0200

    update newsletter without groupId

--- src/app/newsletter/services/newsletterService.ts
+++ src/app/newsletter/services/newsletterService.ts
@@ -1,15 +1,10 @@
 import errorService from 'app/core/services/error.service';
 import httpService from 'app/core/services/http.service';
 
-const GROUP_ID = '51650193869768251';
-
 const newsletterService = {
   subscribe(email: string): Promise<void> {
     try {
-      return httpService.post<{ email: string; groupId: string }, void>('/api/newsletter/subscribe', {
-        email,
-        groupId: GROUP_ID,
-      });
+      return httpService.post<{ email: string }, void>('/api/newsletter/subscribe', { email });
     } catch (err) {
       const castedError = errorService.castError(err);
 
