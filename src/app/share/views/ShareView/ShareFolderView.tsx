897471f674b1ad84311a38d360e98ed04e1ae1c3
Larry <larry@internxt.com>
2022-09-15 11:54:15 +0200

    fixed download shared folder

--- src/app/share/views/ShareView/ShareFolderView.tsx
+++ src/app/share/views/ShareView/ShareFolderView.tsx
@@ -131,7 +131,7 @@ export default function ShareFolderView(props: ShareViewProps): JSX.Element {
             token: token,
           },
           folderInfo.bucket,
-          folderInfo.token,
+          (folderInfo as any).fileToken,
           {
             filesLimit: FILES_LIMIT_BY_REQUEST,
             foldersLimit: FOLDERS_LIMIT_BY_REQUEST,
