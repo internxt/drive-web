317c5227a67917ba49563a20c31012c1b9bf8827
Fabio Espinosa <fa.espinosa10@uniandes.edu.co>
2022-09-22 10:36:29 +0200

    fix(SurveyDialog): render only if user is defined

--- src/app/survey/components/SurveyDialog/SurveyDialog.tsx
+++ src/app/survey/components/SurveyDialog/SurveyDialog.tsx
@@ -7,38 +7,37 @@ import { uiActions } from 'app/store/slices/ui';
 import BaseDialog from 'app/shared/components/BaseDialog/BaseDialog';
 import { UserSettings } from '@internxt/sdk/dist/shared/types/userSettings';
 
-
 const SurveyDialog = (props: { isOpen: boolean }): JSX.Element => {
-    const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
 
-    const onClose = (): void => {
-        dispatch(uiActions.setIsSurveyDialogOpen(false));
-    };
+  const onClose = (): void => {
+    dispatch(uiActions.setIsSurveyDialogOpen(false));
+  };
 
-    const user = useSelector((state: RootState) => state.user.user) as UserSettings;
+  const user = useSelector((state: RootState) => state.user.user) as UserSettings;
 
-    return (
-        <BaseDialog
-            isOpen={props.isOpen}
-            title={''}
-            panelClasses="px-6 py-8 w-156"
-            onClose={onClose}
-            bgColor={'bg-transparent'}
-        >
-            <div className='py-10'>
-                <Widget 
-                    id='yM3EyqJE' 
-                    height={500} 
-                    hidden={{
-                        email: user.email,
-                        uuid: user.uuid
-                    }}
-                />
-            </div>
-        </BaseDialog>
-    );
+  return (
+    <BaseDialog
+      isOpen={props.isOpen}
+      title={''}
+      panelClasses="px-6 py-8 w-156"
+      onClose={onClose}
+      bgColor={'bg-transparent'}
+    >
+      {user && (
+        <div className="py-10">
+          <Widget
+            id="yM3EyqJE"
+            height={500}
+            hidden={{
+              email: user.email,
+              uuid: user.uuid,
+            }}
+          />
+        </div>
+      )}
+    </BaseDialog>
+  );
 };
 
-
 export default SurveyDialog;
-
