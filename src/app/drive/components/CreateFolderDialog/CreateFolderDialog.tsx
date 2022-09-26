3d539d34f0b070250d1755e5aebbfaa8a922175b
Larry <larry@internxt.com>
2022-09-26 16:58:19 +0200

    redesign and some fixes on createFolderDialog

--- src/app/drive/components/CreateFolderDialog/CreateFolderDialog.tsx
+++ src/app/drive/components/CreateFolderDialog/CreateFolderDialog.tsx
@@ -9,6 +9,8 @@ import { uiActions } from 'app/store/slices/ui';
 import BaseButton from 'app/shared/components/forms/BaseButton';
 import storageThunks from 'app/store/slices/storage/storage.thunks';
 import storageSelectors from 'app/store/slices/storage/storage.selectors';
+import i18n from 'app/i18n/services/i18n.service';
+import Spinner from 'app/shared/components/Spinner/Spinner';
 interface CreateFolderDialogProps {
   onFolderCreated?: () => void;
   currentFolderId: number;
@@ -16,28 +18,42 @@ interface CreateFolderDialogProps {
 
 const CreateFolderDialog = ({ onFolderCreated, currentFolderId }: CreateFolderDialogProps) => {
   const [folderName, setFolderName] = useState('');
+  const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const dispatch = useAppDispatch();
   const isOpen = useAppSelector((state: RootState) => state.ui.isCreateFolderDialogOpen);
   const onClose = (): void => {
+    setError('');
+    setIsLoading(false);
     dispatch(uiActions.setIsCreateFolderDialogOpen(false));
   };
   const createFolder = async () => {
-    setIsLoading(true);
-    await dispatch(storageThunks.createFolderThunk({ folderName, parentFolderId: currentFolderId }))
-      .unwrap()
-      .then(() => {
-        setIsLoading(false);
-        onClose();
-        onFolderCreated && onFolderCreated();
-      })
-      .catch((e) => {
-        setIsLoading(false);
-        return e;
-      });
+    if (folderName && folderName.trim().length > 0) {
+      setIsLoading(true);
+      await dispatch(storageThunks.createFolderThunk({ folderName, parentFolderId: currentFolderId }))
+        .unwrap()
+        .then(() => {
+          setIsLoading(false);
+          onClose();
+          onFolderCreated && onFolderCreated();
+        })
+        .catch((e) => {
+          const errorMessage = e?.message?.includes('already exists')
+            ? i18n.get('error.folderAlreadyExists')
+            : i18n.get('error.creatingFolder');
+          setError(errorMessage);
+          setIsLoading(false);
+          return e;
+        });
+    } else {
+      setError(i18n.get('error.folderCannotBeEmpty'));
+    }
   };
   const onCreateButtonClicked = () => {
-    createFolder();
+    if (!isLoading) {
+      setError('');
+      createFolder();
+    }
   };
   const onKeyPressed: KeyboardEventHandler<HTMLInputElement> = (e) => {
     if (e.key === 'Enter') {
@@ -47,29 +63,38 @@ const CreateFolderDialog = ({ onFolderCreated, currentFolderId }: CreateFolderDi
 
   return (
     <BaseDialog isOpen={isOpen} title="Create folder" onClose={onClose}>
-      <div className="w-64 self-center mt-4">
+      <div className="mt-4 px-5">
+        <span className={'text-sm'}>Name</span>
         <input
           autoFocus
           type="text"
           placeholder="Enter folder name"
+          disabled={isLoading}
           value={folderName}
-          onChange={(e) => setFolderName(e.target.value)}
+          onChange={(e) => { setFolderName(e.target.value); setError(''); }}
           onKeyPress={onKeyPressed}
-          className="w-full py-2 px-2.5"
+          className={`w-full py-2 px-2.5 ${error !== '' ? 'error' : ''}`}
         />
+        {error !== '' && <span className={'text-sm error text-red-std'}>&#9888; {error}</span>}
       </div>
 
-      <div className="flex justify-center items-center bg-neutral-20 py-6 mt-6">
+      <div className="flex justify-end items-center py-6 px-5">
         <div className="flex w-64">
-          <BaseButton className="cancel w-full mr-4" onClick={onClose}>
+          <BaseButton className="w-full cancel" onClick={onClose}>
             Cancel
           </BaseButton>
-          <BaseButton className="w-full primary border" disabled={isLoading} onClick={onCreateButtonClicked}>
-            Create
+          <BaseButton className="w-full primary border" onClick={onCreateButtonClicked}>
+            {isLoading ?
+              <>
+                Creating <Spinner className="h-4 w-4 text-white ml-2" />
+              </> :
+              'Create'
+            }
+
           </BaseButton>
         </div>
       </div>
-    </BaseDialog>
+    </BaseDialog >
   );
 };
 
--- src/app/shared/components/BaseDialog/BaseDialog.tsx
+++ src/app/shared/components/BaseDialog/BaseDialog.tsx
@@ -1,5 +1,3 @@
-import UilTimes from '@iconscout/react-unicons/icons/uil-times';
-
 interface BaseDialogProps {
   isOpen: boolean;
   title: string;
@@ -21,24 +19,16 @@ const BaseDialog = ({
 }: BaseDialogProps): JSX.Element => {
   return (
     <div
-      className={`${isOpen ? 'flex' : 'hidden'} ${
-        classes || ''
-      } absolute top-0 left-0 right-0 bottom-0 z-50 bg-neutral-100 bg-opacity-80`}
+      className={`${isOpen ? 'flex' : 'hidden'} ${classes || ''} absolute top-0 left-0 right-0 bottom-0 z-50 bg-neutral-100 bg-opacity-80`}
+      onClick={onClose}
     >
       <div
         className={`${panelClasses || ''} absolute top-1/2 left-1/2 flex w-104 -translate-y-1/2
         -translate-x-1/2 transform flex-col overflow-hidden rounded-lg bg-white pt-8 text-neutral-900`}
+        onClick={(e) => { e.stopPropagation(); }}
       >
-        <UilTimes
-          className="absolute right-8 cursor-pointer text-blue-60 transition
-           duration-200 ease-in-out hover:text-blue-70"
-          onClick={onClose}
-        />
-
         <span
-          className={`${
-            titleClasses || ''
-          } overflow-hidden overflow-ellipsis whitespace-nowrap px-16 text-center text-xl`}
+          className={`${titleClasses || ''} overflow-hidden overflow-ellipsis whitespace-nowrap px-5 text-left text-xl`}
         >
           {title}
         </span>
