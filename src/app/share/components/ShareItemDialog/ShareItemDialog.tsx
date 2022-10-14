75ddb83596901d32593577b3997f0f5769537f98
Fabio Espinosa <fa.espinosa10@uniandes.edu.co>
2022-10-14 15:12:32 +0200

    refactor thunks

--- src/app/share/components/ShareItemDialog/ShareItemDialog.tsx
+++ src/app/share/components/ShareItemDialog/ShareItemDialog.tsx
@@ -1,42 +1,39 @@
-import UilClipboardAlt from '@iconscout/react-unicons/icons/uil-clipboard-alt';
 import { useEffect, useState } from 'react';
 
+import copy from 'copy-to-clipboard';
 import { DriveItemData } from 'app/drive/types';
 import { uiActions } from 'app/store/slices/ui';
 import BaseDialog from 'app/shared/components/BaseDialog/BaseDialog';
-import shareService from 'app/share/services/share.service';
 import './ShareItemDialog.scss';
 import { storageActions } from 'app/store/slices/storage';
-import { trackShareLinkBucketIdUndefined } from 'app/analytics/services/analytics.service';
-import { userThunks } from 'app/store/slices/user';
 import i18n from 'app/i18n/services/i18n.service';
 import notificationsService, { ToastType } from 'app/notifications/services/notifications.service';
 import { aes, items } from '@internxt/lib';
-import navigationService from 'app/core/services/navigation.service';
-import { AppView } from 'app/core/types';
-import errorService from 'app/core/services/error.service';
 import { useAppDispatch, useAppSelector } from 'app/store/hooks';
-import { referralsThunks } from 'app/store/slices/referrals';
-import { ShareTypes } from '@internxt/sdk/dist/drive';
-import crypto from 'crypto';
-import { Environment } from '@internxt/inxt-js';
 import PasswordInput from './components/PasswordInput';
 import { Check, Copy } from 'phosphor-react';
 import dateService from 'app/core/services/date.service';
+import shareService from 'app/share/services/share.service';
+import { sharedThunks } from 'app/store/slices/sharedLinks';
+import localStorageService from 'app/core/services/local-storage.service';
+import { ShareLink } from '@internxt/sdk/dist/drive/share/types';
 
 interface ShareItemDialogProps {
+  share: ShareLink;
   item: DriveItemData;
 }
 
-const DEFAULT_VIEWS = -1;
+function copyShareLink(type: string, code: string, token: string) {
+  copy(`${document.location.origin}/s/${type}/${token}/${code}`);
+  notificationsService.show({ text: i18n.get('shared-links.toast.copy-to-clipboard'), type: ToastType.Success });
+}
 
-const ShareItemDialog = ({ item }: ShareItemDialogProps): JSX.Element => {
+const ShareItemDialog = ({ share, item }: ShareItemDialogProps): JSX.Element => {
+  const isSavedAlreadyWithPassword = !!share?.hashed_password;
   const dispatch = useAppDispatch();
-  const user = useAppSelector((state) => state.user.user);
-  const [linkToCopy, setLinkToCopy] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
-  const [isPasswordProtected, setIsPasswordProtected] = useState(true);
-  const [numberOfAttempts] = useState(DEFAULT_VIEWS);
+  const [itemPassword, setItemPassword] = useState('');
+  const [passwordInputVirgin, setPasswordInputVirgin] = useState(true);
+  const [isPasswordProtected, setIsPasswordProtected] = useState(isSavedAlreadyWithPassword);
   const [isLinkCopied, setIsLinkCopied] = useState(false);
   const isOpen = useAppSelector((state) => state.ui.isShareItemDialogOpen);
 
@@ -49,77 +46,22 @@ const ShareItemDialog = ({ item }: ShareItemDialogProps): JSX.Element => {
     dispatch(storageActions.setItemToShare(null));
   };
 
-  const itemFullName = items.getItemDisplayName(item);
+  const onSubmit = async () => {
+    await shareService.updateShareLink({
+      itemId: share.id,
+      plainPassword: isPasswordProtected ? itemPassword : null,
+    });
+    notificationsService.show({ text: 'Link updated', type: ToastType.Info });
 
-  const handleShareLink = async (views: number) => {
-    try {
-      if (!user) {
-        return navigationService.push(AppView.Login);
-      }
-
-      const { bucket, bridgeUser, userId, mnemonic } = user;
-
-      if (!bucket) {
-        trackShareLinkBucketIdUndefined({ email: bridgeUser });
-        close();
-        notificationsService.show({ text: i18n.get('error.shareLinkMissingBucket'), type: ToastType.Error });
-        dispatch(userThunks.logoutThunk());
-
-        return;
-      }
-
-      const code = crypto.randomBytes(32).toString('hex');
-
-      const encryptedMnemonic = aes.encrypt(mnemonic, code);
-      const encryptedCode = aes.encrypt(code, mnemonic);
-
-      const payload: ShareTypes.GenerateShareLinkPayload = {
-        itemId: item.id.toString(),
-        type: item.isFolder ? 'folder' : 'file',
-        bucket: bucket,
-        itemToken: await new Environment({
-          bridgePass: userId,
-          bridgeUser,
-          bridgeUrl: process.env.REACT_APP_STORJ_BRIDGE,
-        }).createFileToken(bucket, item.fileId, 'PULL'),
-        timesValid: views,
-        encryptedMnemonic,
-        encryptedCode,
-      };
-
-      const link = await shareService.createShareLink(code, mnemonic, payload);
-
-      dispatch(referralsThunks.refreshUserReferrals());
-
-      // window.analytics.track('file-share');
-      setLinkToCopy(link);
-    } catch (err: unknown) {
-      const castedError = errorService.castError(err);
-
-      if (castedError.message === 'unauthenticated') {
-        return navigationService.push(AppView.Login);
-      }
-      notificationsService.show({ text: castedError.message, type: ToastType.Error });
-
-      setLinkToCopy(i18n.get('error.unavailableLink'));
-    } finally {
-      setIsLoading(false);
-    }
+    close();
   };
 
-  useEffect(() => {
-    setIsLoading(true);
-    const delay = setTimeout(() => {
-      handleShareLink(numberOfAttempts);
-    }, 750);
-
-    return () => clearTimeout(delay);
-  }, [numberOfAttempts]);
+  const itemFullName = items.getItemDisplayName(item);
 
   return (
     <BaseDialog
       isOpen={isOpen}
-      title={'Share settings'}
+      title="Share settings"
       subTitle={itemFullName}
       dialogRounded={true}
       textLeft={true}
@@ -133,14 +75,26 @@ const ShareItemDialog = ({ item }: ShareItemDialogProps): JSX.Element => {
         <div className="mx-5">
           <div className="justify-left flex flex-col">
             <p className="text-base font-medium">Access</p>
-            <div className="flex flex-row items-center">
-              <input type={'checkbox'} onChange={() => setIsPasswordProtected(!isPasswordProtected)} />
-              <p className="ml-2 text-base font-medium">Protect with password</p>
+            <div>
+              <label className="flex flex-row items-center">
+                <input
+                  type="checkbox"
+                  checked={isPasswordProtected}
+                  onChange={() => setIsPasswordProtected(!isPasswordProtected)}
+                />
+                <p className="ml-2 text-base font-medium">Protect with password</p>
+              </label>
             </div>
             <p className="px-6 text-sm font-normal">Secure this link by setting up a password</p>
           </div>
           <div className="ml-6 mt-3 w-80">
-            <PasswordInput placeholder="Password" disabled={isPasswordProtected} />
+            <PasswordInput
+              value={isSavedAlreadyWithPassword ? (passwordInputVirgin ? 'xxxxxxxxx' : itemPassword) : itemPassword}
+              placeholder="Password"
+              disabled={!isPasswordProtected}
+              onChange={(evt) => setItemPassword(evt.target.value)}
+              onFocus={() => setPasswordInputVirgin(false)}
+            />
           </div>
           <hr className="border-t-1 my-6 border-neutral-40" />
           <div className="mb-8 flex flex-row justify-between">
@@ -163,8 +117,11 @@ const ShareItemDialog = ({ item }: ShareItemDialogProps): JSX.Element => {
                 setTimeout(() => {
                   setIsLinkCopied(false);
                 }, 4000);
-                navigator.clipboard.writeText(linkToCopy);
-                notificationsService.show({ text: i18n.get('success.linkCopied'), type: ToastType.Info });
+                const temporaryShare = share as ShareLink & { is_folder: boolean };
+                const itemType = share.isFolder || temporaryShare.is_folder ? 'folder' : 'file';
+                const encryptedCode = share.code;
+                const plainCode = aes.decrypt(encryptedCode, localStorageService.getUser()!.mnemonic);
+                copyShareLink(itemType, plainCode, share.token);
               }}
             >
               {isLinkCopied ? (
@@ -183,7 +140,9 @@ const ShareItemDialog = ({ item }: ShareItemDialogProps): JSX.Element => {
               <button onClick={onClose} className="mr-2 rounded-lg bg-gray-5 px-5 py-2 text-base font-medium">
                 Cancel
               </button>
-              <button className="rounded-lg bg-primary px-5 py-2 text-base font-medium text-white">Save</button>
+              <button onClick={onSubmit} className="rounded-lg bg-primary px-5 py-2 text-base font-medium text-white">
+                Save
+              </button>
             </div>
           </div>
         </div>
