ea51ebe154fe36e72f0c339bdbeeccb500718208
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2022-10-25 15:07:34 +0200

    Account updated

--- src/app/core/views/Preferences/tabs/Account/index.tsx
+++ src/app/core/views/Preferences/tabs/Account/index.tsx
@@ -30,12 +30,14 @@ export default function AccountTab({ className = '' }: { className?: string }): 
       ) : (
         <div className={className}>
           <UserHeader />
-          <div className="mt-8 flex flex-col flex-wrap gap-y-8 gap-x-10 xl:flex-row">
-            <div className="flex w-screen flex-col space-y-8">
-              <Usage />
-            </div>
-            <div className="flex flex-1 flex-col space-y-8">
-              <AccountDetails />
+          <div className="mt-8 flex flex-col flex-wrap gap-y-8 gap-x-10">
+            <div className="flex flex-1 flex-row justify-center space-x-8">
+              <div className="flex flex-1 flex-col space-y-8">
+                <Usage />
+              </div>
+              <div className="flex flex-1 flex-col space-y-8">
+                <AccountDetails />
+              </div>
             </div>
             <div className="flex flex-1 flex-col space-y-8">
               <DeleteAccount />
