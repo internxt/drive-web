10fcad2fe44470b887b22b7cb510bf66800fe9e2
Ramon Candel Segura <ramoncandelsegura@gmail.com>
2022-10-31 16:54:09 +0100

    Removed console logs

--- src/app/core/services/dynamicEnv.service.ts
+++ src/app/core/services/dynamicEnv.service.ts
@@ -3,11 +3,8 @@ class EnvService {
 
   constructor() {
     if (process.env.NODE_ENV === 'development' && !!window.__RUNTIME_CONFIG__.REACT_APP_API_URL) {
-      console.log('RUNTIME ENV EXECUTED');
       this.#selectedEnv = window.__RUNTIME_CONFIG__;
     } else {
-      console.log('NORMAL ENV EXECUTED');
-
       this.#selectedEnv = process.env;
     }
   }
