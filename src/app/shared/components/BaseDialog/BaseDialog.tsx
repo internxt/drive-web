2e109bc77eb66717bd474894c1c60eda4b9aa0b8
larry-internxt <larry@internxt.com>
2022-11-02 16:32:08 +0100

    fixed baseDialog close icon

--- src/app/shared/components/BaseDialog/BaseDialog.tsx
+++ src/app/shared/components/BaseDialog/BaseDialog.tsx
@@ -30,18 +30,14 @@ const BaseDialog = ({
   bgColor,
 }: BaseDialogProps): JSX.Element => {
   return (
-    <div
-      className={`${isOpen ? 'flex' : 'hidden'} ${
-        classes || ''
+    <div className={`${isOpen ? 'flex' : 'hidden'} ${classes || ''
       } absolute top-0 left-0 right-0 bottom-0 z-10 bg-black bg-opacity-40`}
     >
-      <div
-        className={`${panelClasses || ''} absolute top-1/2 left-1/2 flex w-104 -translate-y-1/2
-        -translate-x-1/2 transform flex-col overflow-hidden  ${
-          dialogRounded ? 'rounded-2xl' : 'rounded-lg pt-8'
+      <div className={`${panelClasses || ''} absolute top-1/2 left-1/2 flex w-104 -translate-y-1/2
+        -translate-x-1/2 transform flex-col overflow-hidden  ${dialogRounded ? 'rounded-2xl' : 'rounded-lg pt-8'
         } text-neutral-900 ${bgColor || 'bg-white'}`}
       >
-        <div className={`${subTitle ? 'justify-between bg-neutral-10 p-5' : ''} flex flex-row items-start space-x-5`}>
+        <div className={`${subTitle ? 'justify-between bg-neutral-10 p-5' : ''} flex flex-row items-start`}>
           <div className="max-w-fit relative flex flex-1 flex-col truncate">
             <p
               className={`${titleClasses || ''} overflow-hidden overflow-ellipsis whitespace-nowrap text-xl`}
@@ -50,9 +46,8 @@ const BaseDialog = ({
             </p>
             <p className="max-w-fit flex-1 truncate text-base font-normal text-neutral-100">{subTitle}</p>
           </div>
-          <div
-            className={`relative cursor-pointer transition
-           duration-200 ease-in-out ${closeClass || 'text-blue-60 hover:text-blue-70'} `}
+          <div className={`relative cursor-pointer transition
+           duration-200 ease-in-out ml-auto ${closeClass || 'text-blue-60 hover:text-blue-70'} `}
           >
             <X onClick={onClose} size={28} weight={weightIcon} />
           </div>
