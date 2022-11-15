73c532a62e379be36361febf8d8c634ba0ab619e
Rafa <rafijv@gmail.com>
2022-11-15 16:05:47 +0100

    add rename file and download file tests

--- test/e2e/cypress/integration/recents-panel.spec.ts
+++ test/e2e/cypress/integration/recents-panel.spec.ts
@@ -1,19 +1,47 @@
+import { join } from 'path';
+
 describe('Recents panel', () => {
-    beforeEach(() => {
-      cy.clearLocalStorage();
-      cy.login();
-      cy.visit('/app/recents');
-    });
+  const filename = 'example.txt';
+  const filenameRenamed = 'example2';
+  const downloadsFolder = Cypress.config('downloadsFolder');
+  const fixturesFolder = Cypress.config('fixturesFolder');
+  const downloadedFileFullPath = join(downloadsFolder, filename);
+  
+  beforeEach(() => {
+    cy.clearLocalStorage();
+    cy.login();
+    cy.visit('/app/recents');
+  });
 
-    it('Should list recents', () => {
-      cy.contains('example.txt');
-    });
+  it('Should list recents', () => {
+    cy.contains('example.txt');
+  });
 
-    it('Should get link', () => {
-      cy.get('button.file-list-item-actions-button').click();
-      cy.get('a').contains('Get link').click();
-      cy.get('button.file-list-item-actions-button').click();
-      cy.contains('Delete link');
-    });
+  it('Should get link', () => {
+    cy.get('button.file-list-item-actions-button').click();
+    cy.get('a').contains('Get link').click();
+    cy.get('button.file-list-item-actions-button').click();
+    cy.contains('Delete link');
+  });
+
+  it('Should download a single file', () => {
+    cy.get('[data-test=download-file-button]').click({ force: true })
+      .then(() => {
+        cy.readFile(join(fixturesFolder as string, filename)).then((originalFile) => {
+          cy.readFile(downloadedFileFullPath).should('eq', originalFile);
+        });
+      });
+  });
+
+  after(() => {
+    cy.task('removeFile', downloadedFileFullPath);
+  });
+
+  it('Should rename file', () => {
+    cy.get('button.file-list-item-actions-button').click();
+    cy.get('a').contains('Rename').click();
+    cy.get('input[name=fileName]').clear().type(filenameRenamed).type('{enter}');
+    cy.contains(filenameRenamed);
+  });
 
 });
