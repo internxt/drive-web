b4667c5946e11d55df1cbaa79f343b40402fb9ae
Rafa <rafijv@gmail.com>
2023-03-08 11:11:11 +0100

    [WT-280] fix move items dialog ux

---
+++ src/app/store/slices/storage/storage.thunks/fetchDialogContentThunk.ts
@@ -0,0 +1,52 @@
+import { ActionReducerMapBuilder, createAsyncThunk } from '@reduxjs/toolkit';
+import _ from 'lodash';
+
+import { storageActions } from '..';
+import { RootState } from '../../..';
+import { StorageState } from '../storage.model';
+import notificationsService, { ToastType } from '../../../../notifications/services/notifications.service';
+import databaseService, { DatabaseCollection } from '../../../../database/services/database.service';
+import { DriveItemData } from '../../../../drive/types';
+import { SdkFactory } from '../../../../core/factory/sdk';
+import { t } from 'i18next';
+
+export const fetchDialogContentThunk = createAsyncThunk<void, number, { state: RootState }>(
+  'storage/fetchDialogContentThunk',
+  async (folderId, { dispatch }) => {
+    const storageClient = SdkFactory.getInstance().createStorageClient();
+    const [responsePromise] = storageClient.getFolderContent(folderId);
+    const databaseContent = await databaseService.get<DatabaseCollection.Levels>(DatabaseCollection.Levels, folderId);
+
+    dispatch(storageActions.resetOrder());
+
+    if (databaseContent) {
+      dispatch(
+        storageActions.setItems({
+          folderId,
+          items: databaseContent,
+        }),
+      );
+    } else {
+      await responsePromise;
+    }
+
+    responsePromise.then((response) => {
+      const folders = response.children.map((folder) => ({ ...folder, isFolder: true }));
+      const items = _.concat(folders as DriveItemData[], response.files as DriveItemData[]);
+      dispatch(
+        storageActions.setItems({
+          folderId,
+          items,
+        }),
+      );
+      databaseService.put(DatabaseCollection.Levels, folderId, items);
+    });
+  },
+);
+
+export const fetchDialogContentThunkExtraReducers = (builder: ActionReducerMapBuilder<StorageState>): void => {
+  builder.addCase(fetchDialogContentThunk.rejected, (state, action) => {
+    state.loadingFolders[action.meta.arg] = false;
+    notificationsService.show({ text: t('error.fetchingFolderContent'), type: ToastType.Error });
+  });
+};
