858ba5117a7002c937ac700dbd1d7f172b8c2c25
Ramon Candel Segura <ramoncandelsegura@gmail.com>
2023-03-22 15:04:17 +0100

    Added trash and delete items messages and fix delete logic

--- src/use_cases/trash/clear-trash.ts
+++ src/use_cases/trash/clear-trash.ts
@@ -3,18 +3,39 @@ import { storageActions } from '../../app/store/slices/storage';
 import { store } from '../../app/store';
 import notificationsService, { ToastType } from '../../app/notifications/services/notifications.service';
 import { t } from 'i18next';
+import errorService from '../../app/core/services/error.service';
 
 const ClearTrash = async (): Promise<void> => {
-  const trashClient = await SdkFactory.getNewApiInstance().createTrashClient();
-  await trashClient.clearTrash();
+  let deletingItemsToastId;
 
-  store.dispatch(storageActions.resetTrash());
-  store.dispatch(storageActions.clearSelectedItems());
+  try {
+    deletingItemsToastId = notificationsService.show({
+      type: ToastType.Loading,
+      text: t('drive.deletingItems'),
+      duration: 100000,
+      closable: false,
+    });
 
-  notificationsService.show({
-    type: ToastType.Success,
-    text: t('trash.clearTrash'),
-  });
+    const trashClient = await SdkFactory.getNewApiInstance().createTrashClient();
+    await trashClient.clearTrash();
+
+    store.dispatch(storageActions.resetTrash());
+    store.dispatch(storageActions.clearSelectedItems());
+
+    notificationsService.dismiss(deletingItemsToastId);
+    notificationsService.show({
+      type: ToastType.Success,
+      text: t('trash.clearTrash'),
+    });
+  } catch (error) {
+    notificationsService.dismiss(deletingItemsToastId);
+    notificationsService.show({
+      text: t('error.errorDeletingFromTrash'),
+      type: ToastType.Error,
+    });
+
+    errorService.reportError(error);
+  }
 };
 
 export default ClearTrash;
