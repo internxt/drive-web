f20b7ec0c5ea19f5bc97d64472a4a596564dac83
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2023-03-23 13:44:24 +0100

    Update design

--- src/app/auth/views/Auth/SignupBlog.tsx
+++ src/app/auth/views/Auth/SignupBlog.tsx
@@ -1,4 +1,5 @@
 import { SignupComponent } from './AuthView';
+import InternxtDevices from '../../../../assets/images/banner/Internxt-secure-cloud-storage.webp';
 
 const textContent = {
   email: 'Email',
@@ -26,10 +27,28 @@ const SignupAuth = () => {
 
 export default function SignupBlog(): JSX.Element {
   return (
-    <div className="flex flex-col items-center justify-center py-3 px-5 lg:py-16 lg:px-40">
-      <div className="flex h-full w-full flex-col items-center justify-center space-y-7 px-5 lg:flex-row lg:space-y-0 lg:space-x-48">
-        <div className="flex w-full max-w-md flex-col justify-between space-y-3 lg:space-y-10">
-          <SignupAuth />
+    <div className="flex flex-col items-center justify-center   py-3 px-5 lg:py-16 lg:px-40">
+      <div className="flex flex-row items-center justify-center overflow-hidden bg-gradient-to-br from-blue-20 to-white">
+        <div className="mt-11 mb-11 ml-11 flex w-full max-w-lg flex-col items-center justify-center space-y-10">
+          <p className="text-4xl font-semibold">
+            Keep those now clean files safe, <span className="text-primary">join Internxt for free</span>
+          </p>
+          <div className="flex w-full">
+            <SignupAuth />
+          </div>
+        </div>
+        <div className="-ml-40 flex items-center">
+          <div className="relative left-56 top-5 flex flex-col overflow-hidden bg-contain">
+            <img
+              src={InternxtDevices}
+              width={434}
+              height={240}
+              // draggable="false"
+              about="desktop, laptop and phone with Internxt app"
+              loading="eager"
+              alt="desktop, laptop and phone with Internxt app"
+            />
+          </div>
         </div>
       </div>
     </div>
