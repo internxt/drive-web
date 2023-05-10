34d52137c72f466e23cbf9d6e6d666e7efb1571b
Ramon Candel Segura <ramoncandelsegura@gmail.com>
2023-05-10 11:51:33 +0200

    Added to signup end to end test content check

--- test/e2e/cypress/integration/signup.spec.ts
+++ test/e2e/cypress/integration/signup.spec.ts
@@ -1,14 +1,18 @@
 import * as crypto from 'crypto';
+import { PAGINATION_ENDPOINT_REGEX } from '../constans';
 
 describe('Signup user', () => {
   const username = `${crypto.randomBytes(32).toString('hex')}@inxt.com`;
   const password = `Pw4${crypto.randomBytes(4).toString('hex')}`;
 
   beforeEach(() => {
+    cy.intercept('GET', PAGINATION_ENDPOINT_REGEX.FOLDERS, (req) => {
+      delete req.headers['if-none-match'];
+    }).as('getFolders');
     cy.visit('/');
   });
 
-  it('Should signup an user correctly and redirect new users to /app', () => {
+  it('Should signup an user correctly and load folders correctly in /app', () => {
     cy.visit('/new');
 
     cy.contains('Create account');
@@ -20,6 +24,12 @@ describe('Signup user', () => {
     cy.get('button[type=submit]').click();
 
     cy.url().should('include', '/app');
+
+    cy.wait('@getFolders', { timeout: 60000 }).then((foldersInterception) => {
+      expect(foldersInterception.response.statusCode).to.equal(200);
+      const foldersResponseItems = foldersInterception.response.body.result;
+      expect(foldersResponseItems).to.have.lengthOf(2);
+    });
 
     cy.writeFile(
       './cypress/fixtures/test-user.json',
