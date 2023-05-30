8bf2bf9f88cfd674ca194f159fa8fd6df65106c0
Ramon Candel Segura <ramoncandelsegura@gmail.com>
2023-05-30 08:06:25 +0200

    Changed sentry webpack plugin url

--- craco.config.js
+++ craco.config.js
@@ -15,7 +15,7 @@ module.exports = {
       sentryWebpackPlugin({
         org: 'internxt',
         project: 'internxt-web',
-        url: 'https://51b6d35a8b0b4a73b918be0e851c6f07@sentry.internxt.com/',
+        url: 'https://sentry.internxt.com/',
         authToken: process.env.SENTRY_AUTH_TOKEN,
         include: 'build',
         sourcemaps: {
@@ -23,7 +23,7 @@ module.exports = {
         },
         debug: true,
         // Optionally uncomment the line below to override automatic release name detection
-        // release: packageJson.name + '@' + packageJson.version,
+        // release: { name: 'drive-web@v1.0.91' },
       }),
     ],
   },
