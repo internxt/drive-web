8af2419fb2568eb9229e7fe039461d91aa939fd1
Ramon Candel Segura <ramoncandelsegura@gmail.com>
2023-05-31 17:07:51 +0200

    Added folder download without % and added new translations to signup view

--- src/app/tasks/services/tasks.service/index.ts
+++ src/app/tasks/services/tasks.service/index.ts
@@ -199,6 +199,10 @@ class TaskManagerService {
       return task.subtitle;
     }
 
+    const notExistProgress = task.progress && task.progress === Infinity;
+    if (task.action === TaskType.DownloadFolder && task.status === TaskStatus.InProcess && notExistProgress)
+      return t(`tasks.${task.action}.status.in-process-without-progress`);
+
     return t(`tasks.${task.action}.status.${task.status}`, {
       progress: task.progress ? (task.progress * 100).toFixed(0) : 0,
     });
