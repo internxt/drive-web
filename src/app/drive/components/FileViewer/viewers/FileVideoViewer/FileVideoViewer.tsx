abbb9e83e22f3a95d7591ee64206eb45e898a73e
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2023-07-14 15:03:42 +0200

    feat(video): basic file video viewer

--- src/app/drive/components/FileViewer/viewers/FileVideoViewer/FileVideoViewer.tsx
+++ src/app/drive/components/FileViewer/viewers/FileVideoViewer/FileVideoViewer.tsx
@@ -1,6 +1,43 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { DriveFileData } from '@internxt/sdk/dist/drive/storage/types';
 
-const FileVideoViewer = ({ file }) => {
+import { loadVideoIntoPlayer } from 'app/core/services/video.service';
+import { getEnvironmentConfig } from 'app/drive/services/network.service';
+import { VideoExtensions } from 'app/drive/types/file-types';
+
+const FileVideoViewer = ({ file }: { file: DriveFileData }) => {
+  const { bridgeUser, bridgePass, encryptionKey } = getEnvironmentConfig(false);
+
+  useEffect(() => {
+    const videoId = 'video-Inxt';
+    const video = document.getElementById(videoId) as HTMLVideoElement;
+
+    loadVideoIntoPlayer(
+      video,
+      {
+        bucketId: file.bucket,
+        fileId: file.fileId,
+        creds: {
+          pass: bridgePass,
+          user: bridgeUser,
+        },
+        mnemonic: encryptionKey,
+        options: {
+          notifyProgress: (totalBytes, downloadedBytes) => {
+            // setUpdateProgress(downloadedBytes / totalBytes);
+          },
+          // abortController,
+        },
+      },
+      file.type as keyof VideoExtensions,
+    ).then((meta) => {
+      console.log('Video meta', meta);
+    }).catch((err) => {
+      // TODO: Handle it.
+
+    });
+  }, []);
+
   return (
     <div>
       <video id="video-Inxt" controls></video>
