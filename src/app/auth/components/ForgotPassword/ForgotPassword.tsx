dccac396d5f62f52a606ec4c6f5e4a0739d70c01
Rafa <rafijv@gmail.com>
2023-07-14 10:54:41 +0200

    [PB-695] change forgot password view

--- src/app/auth/components/ForgotPassword/ForgotPassword.tsx
+++ src/app/auth/components/ForgotPassword/ForgotPassword.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
 import { Link } from 'react-router-dom';
-import { CaretLeft, WarningCircle } from '@phosphor-icons/react';
+import { WarningCircle, Envelope } from '@phosphor-icons/react';
 import userService from '../../services/user.service';
 
 import { IFormValues } from 'app/core/types';
@@ -26,20 +26,20 @@ function ForgotPassword(): JSX.Element {
   const [emailErrors, setEmailErrors] = useState('');
 
   const sendEmail = async (email: string) => {
-    try {
-      setIsLoading(true);
-      await userService.sendDeactivationEmail(email);
-      notificationsService.show({ text: translate('success.accountDeactivationEmailSent'), type: ToastType.Success });
-      if (showErrors === false) {
-        setStep(2);
-      }
-    } catch (err: unknown) {
-      notificationsService.show({ text: translate('error.deactivatingAccount'), type: ToastType.Error });
-      setEmailErrors(translate('error.deactivatingAccount') as string);
-      setShowErrors(true);
-    } finally {
-      setIsLoading(false);
-    }
+    // try {
+    //   setIsLoading(true);
+    //   await userService.sendDeactivationEmail(email);
+    //   notificationsService.show({ text: translate('success.accountDeactivationEmailSent'), type: ToastType.Success });
+    //   if (showErrors === false) {
+    //     setStep(2);
+    //   }
+    // } catch (err: unknown) {
+    //   notificationsService.show({ text: translate('error.deactivatingAccount'), type: ToastType.Error });
+    //   setEmailErrors(translate('error.deactivatingAccount') as string);
+    //   setShowErrors(true);
+    // } finally {
+    //   setIsLoading(false);
+    // }
   };
 
   const onSubmit: SubmitHandler<IFormValues> = (formData, event) => {
@@ -49,85 +49,61 @@ function ForgotPassword(): JSX.Element {
   };
 
   return (
-    <div className="flex h-fit w-96 flex-col items-center justify-center rounded-2xl bg-white px-8 py-10 sm:shadow-soft">
-      <div className="flex flex-col space-y-3">
-        <div className="flex flex-col space-y-1">
-          <Link
-            to="/login"
-            className="flex cursor-pointer flex-row items-center space-x-0.5 text-primary no-underline focus:text-primary-dark"
-          >
-            <CaretLeft className="h-4 w-4" weight="bold" />
-            <span className="font-medium">Log in</span>
-          </Link>
+    <>
+      {step === 1 ? (
+        <div className="flex w-96 flex-col rounded-2xl p-8 shadow-soft">
+          <h1 className="text-2xl font-medium text-gray-100">{translate('auth.forgotPassword.title')}</h1>
+          <form className="mt-5 w-full" onSubmit={handleSubmit(onSubmit)}>
+            <label className="space-y-1">
+              <span className="font-regular text-sm text-gray-80">{translate('auth.email')}</span>
+              <TextInput
+                placeholder={translate('auth.email')}
+                label="email"
+                type="email"
+                register={register}
+                onFocus={() => setShowErrors(false)}
+                required={true}
+                minLength={{ value: 1, message: translate('notificationMessages.emailNotEmpty') }}
+                error={errors.email}
+              />
+              {showErrors && (
+                <div className="-mt-2 flex flex-row items-start pb-3">
+                  <div className="flex h-5 flex-row items-center">
+                    <WarningCircle weight="fill" className="mr-1 h-4 text-red-std" />
+                  </div>
+                  <span className="font-base w-56 text-sm text-red-60">{emailErrors}</span>
+                </div>
+              )}
+            </label>
 
-          <div className="flex flex-col space-y-1">
-            <h1 className="text-2xl font-medium text-gray-100">{translate('auth.forgotPassword.title')}</h1>
-            <p className="font-regular text-sm text-gray-80">{translate('auth.forgotPassword.description')}</p>
+            <Button
+              disabled={isLoading}
+              text={isLoading ? translate('auth.forgotPassword.sending') : translate('auth.forgotPassword.continue')}
+              loading={isLoading}
+              style="button-primary"
+              disabledText="Send instructions"
+              className="mt-4 w-full"
+            />
+          </form>
+          <div className="mt-4 flex w-full justify-center">
+            <p className="font-regular mr-2 text-base">{translate('auth.forgotPassword.password')}</p>
+            <Link to="/login" className="cursor-pointer text-base font-medium text-primary no-underline">
+              {translate('auth.forgotPassword.login')}
+            </Link>
           </div>
         </div>
-
-        {step === 1 ? (
-          <>
-            <form className="w-full space-y-4" onSubmit={handleSubmit(onSubmit)}>
-              <label className="space-y-1">
-                <span>{translate('auth.email')}</span>
-                <TextInput
-                  placeholder={translate('auth.email')}
-                  label="email"
-                  type="email"
-                  register={register}
-                  onFocus={() => setShowErrors(false)}
-                  required={true}
-                  minLength={{ value: 1, message: translate('notificationMessages.emailNotEmpty') }}
-                  error={errors.email}
-                />
-                {showErrors && (
-                  <div className="-mt-2 flex flex-row items-start pb-3">
-                    <div className="flex h-5 flex-row items-center">
-                      <WarningCircle weight="fill" className="mr-1 h-4 text-red-std" />
-                    </div>
-                    <span className="font-base w-56 text-sm text-red-60">{emailErrors}</span>
-                  </div>
-                )}
-              </label>
-
-              <Button
-                disabled={isLoading}
-                text={
-                  isLoading
-                    ? translate('auth.forgotPassword.sending')
-                    : translate('auth.forgotPassword.sendInstructions')
-                }
-                loading={isLoading}
-                style="button-primary"
-                disabledText="Send instructions"
-                className="w-full"
-              />
-            </form>
-          </>
-        ) : (
-          <>
-            <div className="flex w-full flex-col items-center justify-center space-y-2 rounded-lg border border-gray-10 bg-gray-1 p-4 text-center text-sm">
-              <span className="w-full break-all">
-                <span className="whitespace-nowrap">Email sent to</span> <span className="font-medium">{getEmail}</span>
-              </span>
-
-              <p className="text-gray-50">
-                The email link access will expire in 24h. If you are not receiving the email, please check your
-                promotions or spam inbox.
-              </p>
-
-              <span
-                onClick={() => setStep(1)}
-                className="cursor-pointer font-medium text-primary hover:text-primary-dark"
-              >
-                Change email
-              </span>
-            </div>
-          </>
-        )}
-      </div>
-    </div>
+      ) : (
+        <div className="flex w-80 flex-col items-center justify-center rounded-2xl bg-white p-8 text-center shadow-soft">
+          <Envelope size={80} className="text-primary" weight="thin" />
+          <div>
+            <h4 className="mt-4 text-xl font-medium">{translate('auth.forgotPassword.successTitle')}</h4>
+            <p className="font-regular mt-1 w-64 text-base text-gray-60">
+              {translate('auth.forgotPassword.successDescription')}
+            </p>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
