c5fe41cc9a5bb6a8b6c43a68c883f93d6481a7a4
Ramon Candel <mameo@MBP-de-Ramon.home>
2023-08-01 11:35:50 +0200

    Changed comment

--- test/unit/services/keys.service.test.ts
+++ test/unit/services/keys.service.test.ts
@@ -16,66 +16,70 @@ describe('# keys service tests', () => {
     const plainPrivateKey = keys.privateKeyArmored;
 
     expect(isValid(plainPrivateKey)).toBeTruthy();
-
+    /** 
     const password = '1234';
+    
     const encryptedPrivateKey = aes.encrypt(plainPrivateKey, password, aesInit);
 
-    // const { update, newPrivKey, privkeyDecrypted } = await validateFormat(encryptedPrivateKey, password);
+    const { update, newPrivKey, privkeyDecrypted } = await validateFormat(encryptedPrivateKey, password);
 
-    // expect(update).toBeFalsy();
-    // expect(newPrivKey).toBeUndefined();
-    // expect(privkeyDecrypted).toStrictEqual(plainPrivateKey);
+    expect(update).toBeFalsy();
+    expect(newPrivKey).toBeUndefined();
+    expect(privkeyDecrypted).toStrictEqual(plainPrivateKey);
+    */
   });
 
-  // it('Should reencrypt with 2145 hops', async () => {
-  //   const keys = await generateNewKeys();
-  //   const plainPrivateKey = keys.privateKeyArmored;
+  /** 
+  it('Should reencrypt with 2145 hops', async () => {
+    const keys = await generateNewKeys();
+    const plainPrivateKey = keys.privateKeyArmored;
 
-  //   expect(isValid(plainPrivateKey)).toBeTruthy();
+    expect(isValid(plainPrivateKey)).toBeTruthy();
 
-  //   const password = '1234';
+    const password = '1234';
 
-  //   const oldEncryptionPrivateKey = aes.encrypt(plainPrivateKey, password, aesInit, 9999);
-  //   const newEncryptionPrivateKey = aes.encrypt(plainPrivateKey, password, aesInit);
+    const oldEncryptionPrivateKey = aes.encrypt(plainPrivateKey, password, aesInit, 9999);
+    const newEncryptionPrivateKey = aes.encrypt(plainPrivateKey, password, aesInit);
 
-  //   const { update, newPrivKey, privkeyDecrypted } = await validateFormat(oldEncryptionPrivateKey, password);
+    const { update, newPrivKey, privkeyDecrypted } = await validateFormat(oldEncryptionPrivateKey, password);
 
-  //   expect(update).toBeTruthy();
-  //   expect(newPrivKey).toStrictEqual(newEncryptionPrivateKey);
-  //   expect(privkeyDecrypted).toStrictEqual(plainPrivateKey);
-  // });
+    expect(update).toBeTruthy();
+    expect(newPrivKey).toStrictEqual(newEncryptionPrivateKey);
+    expect(privkeyDecrypted).toStrictEqual(plainPrivateKey);
+  });
 
-  // it('Should decode private key from base64 & reencrypt with 2145 hops (using 2145 hops)', async () => {
-  //   const keys = await generateNewKeys();
-  //   const password = '1234';
+  it('Should decode private key from base64 & reencrypt with 2145 hops (using 2145 hops)', async () => {
+    const keys = await generateNewKeys();
+    const password = '1234';
 
-  //   const plainPrivateKey = keys.privateKeyArmored;
-  //   const encryptedPrivateKeyUtf8 = aes.encrypt(plainPrivateKey, password, aesInit);
+    const plainPrivateKey = keys.privateKeyArmored;
+    const encryptedPrivateKeyUtf8 = aes.encrypt(plainPrivateKey, password, aesInit);
 
-  //   const base64PrivateKey = Buffer.from(plainPrivateKey).toString('base64');
-  //   const encryptedPrivateKeyBase64 = aes.encrypt(base64PrivateKey, password, aesInit);
+    const base64PrivateKey = Buffer.from(plainPrivateKey).toString('base64');
+    const encryptedPrivateKeyBase64 = aes.encrypt(base64PrivateKey, password, aesInit);
 
-  //   const { update, newPrivKey, privkeyDecrypted } = await validateFormat(encryptedPrivateKeyBase64, password);
+    const { update, newPrivKey, privkeyDecrypted } = await validateFormat(encryptedPrivateKeyBase64, password);
 
-  //   expect(update).toBeTruthy();
-  //   expect(newPrivKey).toStrictEqual(encryptedPrivateKeyUtf8);
-  //   expect(privkeyDecrypted).toStrictEqual(plainPrivateKey);
-  // });
+    expect(update).toBeTruthy();
+    expect(newPrivKey).toStrictEqual(encryptedPrivateKeyUtf8);
+    expect(privkeyDecrypted).toStrictEqual(plainPrivateKey);
+  });
 
-  // it('Should decode private key from base64 & reencrypt with 2145 hops (using 9999 hops)', async () => {
-  //   const keys = await generateNewKeys();
-  //   const password = '1234';
+  it('Should decode private key from base64 & reencrypt with 2145 hops (using 9999 hops)', async () => {
+    const keys = await generateNewKeys();
+    const password = '1234';
 
-  //   const plainPrivateKey = keys.privateKeyArmored;
-  //   const encryptedPrivateKeyUtf8 = aes.encrypt(plainPrivateKey, password, aesInit);
+    const plainPrivateKey = keys.privateKeyArmored;
+    const encryptedPrivateKeyUtf8 = aes.encrypt(plainPrivateKey, password, aesInit);
 
-  //   const base64PrivateKey = Buffer.from(plainPrivateKey).toString('base64');
+    const base64PrivateKey = Buffer.from(plainPrivateKey).toString('base64');
 
-  //   const encryptedPrivateKeyBase64 = aes.encrypt(base64PrivateKey, password, aesInit, 9999);
-  //   const { update, newPrivKey, privkeyDecrypted } = await validateFormat(encryptedPrivateKeyBase64, password);
+    const encryptedPrivateKeyBase64 = aes.encrypt(base64PrivateKey, password, aesInit, 9999);
+    const { update, newPrivKey, privkeyDecrypted } = await validateFormat(encryptedPrivateKeyBase64, password);
 
-  //   expect(update).toBeTruthy();
-  //   expect(newPrivKey).toStrictEqual(encryptedPrivateKeyUtf8);
-  //   expect(privkeyDecrypted).toStrictEqual(plainPrivateKey);
-  // });
+    expect(update).toBeTruthy();
+    expect(newPrivKey).toStrictEqual(encryptedPrivateKeyUtf8);
+    expect(privkeyDecrypted).toStrictEqual(plainPrivateKey);
+  });
+  */
 });
