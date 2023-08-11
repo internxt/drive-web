040c800f86c4e9228e3d81f46cc3d6454e73b0a9
Ramon Candel <mameo@MBP-de-Ramon.home>
2023-08-11 15:48:49 +0200

    Added check to staging

--- src/app/core/services/env.service.ts
+++ src/app/core/services/env.service.ts
@@ -1,4 +1,5 @@
 function isProduction(): boolean {
+  if (process.env.REACT_APP_NODE_ENV === 'staging') return false;
   return process.env.NODE_ENV === 'production' || process.env.REACT_APP_NODE_ENV === 'production';
 }
 
