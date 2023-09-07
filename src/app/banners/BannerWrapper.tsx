ac64e44a7b881a6cbbad33ee0362bc4b64a58495
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2023-09-07 18:23:49 +0200

    feat: add crowdcube banner

--- src/app/banners/BannerWrapper.tsx
+++ src/app/banners/BannerWrapper.tsx
@@ -5,32 +5,31 @@ import { PlanState } from 'app/store/slices/plan';
 import { userSelectors } from 'app/store/slices/user';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import Banner from './Banner';
+import CrowdcubeBanner from './CrowdcubeBanner';
 
 const BannerWrapper = (): JSX.Element => {
-  const [showBanner, setShowBanner] = useState(false);
+  const [showBanner, setShowBanner] = useState(true);
   const plan = useSelector<RootState, PlanState>((state) => state.plan);
   const isTutorialCompleted = localStorageService.get(STORAGE_KEYS.SIGN_UP_TUTORIAL_COMPLETED);
   const userPlan = plan.subscription?.type;
   const isNewAccount = useAppSelector(userSelectors.hasSignedToday);
-  const shouldShowBanner = userPlan === 'free' && !localStorageService.get(STORAGE_KEYS.SHOW_LIFETIME_BANNER);
+  // const shouldShowBanner = userPlan === 'free' && !localStorageService.get(STORAGE_KEYS.SHOW_LIFETIME_BANNER);
 
   const onCloseBanner = () => {
     setShowBanner(false);
-    localStorage.setItem(STORAGE_KEYS.SHOW_LIFETIME_BANNER, 'false');
   };
 
-  function handleBannerDisplay() {
-    if ((isNewAccount && isTutorialCompleted && shouldShowBanner) || (!isNewAccount && shouldShowBanner)) {
-      setShowBanner(true);
-    }
-  }
+  // function handleBannerDisplay() {
+  //   if ((isNewAccount && isTutorialCompleted) || !isNewAccount) {
+  //     setShowBanner(true);
+  //   }
+  // }
 
-  useEffect(() => {
-    handleBannerDisplay();
-  }, [isTutorialCompleted, userPlan, isNewAccount]);
+  // useEffect(() => {
+  //   handleBannerDisplay();
+  // }, [isTutorialCompleted, userPlan, isNewAccount]);
 
-  return <Banner showBanner={showBanner} onClose={onCloseBanner} />;
+  return <CrowdcubeBanner onCloseBanner={onCloseBanner} showBanner={showBanner} />;
 };
 
 export default BannerWrapper;
