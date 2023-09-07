7f61b998559f03858bc5639ceea41e6ea5152a42
Xavi <77491413+masterprog-cmd@users.noreply.github.com>
2023-09-07 18:47:09 +0200

    fix: manage modal with sessionStorage

--- src/app/banners/BannerWrapper.tsx
+++ src/app/banners/BannerWrapper.tsx
@@ -1,33 +1,23 @@
-import localStorageService, { STORAGE_KEYS } from 'app/core/services/local-storage.service';
-import { RootState } from 'app/store';
-import { useAppSelector } from 'app/store/hooks';
-import { PlanState } from 'app/store/slices/plan';
-import { userSelectors } from 'app/store/slices/user';
 import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
 import CrowdcubeBanner from './CrowdcubeBanner';
 
 const BannerWrapper = (): JSX.Element => {
-  const [showBanner, setShowBanner] = useState(true);
-  const plan = useSelector<RootState, PlanState>((state) => state.plan);
-  const isTutorialCompleted = localStorageService.get(STORAGE_KEYS.SIGN_UP_TUTORIAL_COMPLETED);
-  const userPlan = plan.subscription?.type;
-  const isNewAccount = useAppSelector(userSelectors.hasSignedToday);
-  // const shouldShowBanner = userPlan === 'free' && !localStorageService.get(STORAGE_KEYS.SHOW_LIFETIME_BANNER);
+  const [showBanner, setShowBanner] = useState(false);
 
   const onCloseBanner = () => {
     setShowBanner(false);
+    sessionStorage.setItem('showBanner', 'false');
   };
 
-  // function handleBannerDisplay() {
-  //   if ((isNewAccount && isTutorialCompleted) || !isNewAccount) {
-  //     setShowBanner(true);
-  //   }
-  // }
+  function handleBannerDisplay() {
+    if (!sessionStorage.getItem('showBanner')) {
+      setShowBanner(true);
+    }
+  }
 
-  // useEffect(() => {
-  //   handleBannerDisplay();
-  // }, [isTutorialCompleted, userPlan, isNewAccount]);
+  useEffect(() => {
+    handleBannerDisplay();
+  }, []);
 
   return <CrowdcubeBanner onCloseBanner={onCloseBanner} showBanner={showBanner} />;
 };
