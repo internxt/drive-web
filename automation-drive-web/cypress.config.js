89423f5c880c1b4a449b9dd2477db71c6fbdd2c0
savilashokworks <savila@shokworks.io>
2023-09-21 16:55:26 -0300

    [PB-984]terminal report dependency added. Example of performance testing

--- automation-drive-web/cypress.config.js
+++ automation-drive-web/cypress.config.js
@@ -2,15 +2,29 @@ const { defineConfig } = require("cypress");
 
 
 module.exports = defineConfig({
+ 
   projectId: "hcgjvq",
   viewportWidth: 1000,
   viewportHeight: 660,
   watchForFileChanges: false,
   chromeWebSecurity: false,
   e2e: {
+    
     specPattern: ['cypress/e2e/**/*.cy.{js,jsx,ts,tsx,}'],
 		baseUrl: 'https://internxt.com/es',
 		watchForFileChanges:false,
+  },
+  plugins: (on, config) => {
+    const logOptions = {
+      printLogsToFile: 'always',
+      printLogsToConsole: 'always',
+      outputRoot: config.projectRoot + '/logs/',
+      outputTarget: {
+        'performance-logs.txt': 'txt',
+      },
+    };
+    logOptions.collectTypes = ['cy:log'];
+    require('cypress-terminal-report/src/installLogsPrinter')(on, logOptions);
   },
 });
 
