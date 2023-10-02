8c04cd6c616522f3e09464186d4ef28cac7041c9
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2023-10-02 17:23:31 +0200

    fix: change fetch to axios (API call)

--- src/app/core/services/desktop.service.ts
+++ src/app/core/services/desktop.service.ts
@@ -1,20 +1,21 @@
 import axios from 'axios';
 import operatingSystemService from './operating-system.service';
 
-async function getDownloadAppUrl() {
-  const app = await fetch('https://internxt.com/api/download', {
-    method: 'GET',
-  });
+const INTERNXT_BASE_URL = 'https://internxt.com';
 
-  const platforms = await app.json();
+async function getDownloadAppUrl() {
+  const app = await axios({
+    method: 'GET',
+    url: `${INTERNXT_BASE_URL}/api/download`,
+  });
 
   switch (operatingSystemService.getOperatingSystem()) {
     case 'LinuxOS' || 'UNIXOS':
-      return platforms.platforms.Linux || 'https://internxt.com/downloads/drive.deb';
+      return app.data.platforms.Linux || 'https://internxt.com/downloads/drive.deb';
     case 'WindowsOS':
-      return platforms.platforms.Windows || 'https://internxt.com/downloads/drive.exe';
+      return app.data.platforms.Windows || 'https://internxt.com/downloads/drive.exe';
     case 'MacOS':
-      return platforms.platforms.MacOS || 'https://internxt.com/downloads/drive.dmg';
+      return app.data.platforms.MacOS || 'https://internxt.com/downloads/drive.dmg';
     default:
       break;
   }
