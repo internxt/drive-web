3b104e331e744b831b3ce5f3281e711b14ca1ae6
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2023-10-02 18:21:22 +0200

    fix: change axios to fetch

--- src/app/core/services/desktop.service.ts
+++ src/app/core/services/desktop.service.ts
@@ -4,19 +4,20 @@ import operatingSystemService from './operating-system.service';
 const INTERNXT_BASE_URL = 'https://internxt.com';
 
 async function getDownloadAppUrl() {
-  const app = await axios({
+  const app = await fetch(`${INTERNXT_BASE_URL}/api/download`, {
     method: 'GET',
-    url: `${INTERNXT_BASE_URL}/api/download`,
   });
+
+  const platforms = await app.json();
 
   switch (operatingSystemService.getOperatingSystem()) {
     case 'LinuxOS':
     case 'UNIXOS':
-      return app.data.platforms.Linux || `${INTERNXT_BASE_URL}/downloads/drive.deb`;
+      return platforms.platforms.Linux || `${INTERNXT_BASE_URL}/downloads/drive.deb`;
     case 'WindowsOS':
-      return app.data.platforms.Windows || `${INTERNXT_BASE_URL}/downloads/drive.exe`;
+      return platforms.platforms.Windows || `${INTERNXT_BASE_URL}/downloads/drive.exe`;
     case 'MacOS':
-      return app.data.platforms.MacOS || `${INTERNXT_BASE_URL}/downloads/drive.dmg`;
+      return platforms.platforms.MacOS || `${INTERNXT_BASE_URL}/downloads/drive.dmg`;
     default:
       break;
   }
