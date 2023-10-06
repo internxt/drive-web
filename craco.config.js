728e820b5fe5c7b6a92e7d96e943f0c94a180f8b
Andres Pinto <andres.pintosantiso@gmail.com>
2023-10-06 01:07:06 -0400

    chore: webpack-upgraded

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,8 @@
 const { when } = require('@craco/craco');
+
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const webpack = require('webpack');
 
 module.exports = {
   style: {
@@ -7,7 +10,36 @@ module.exports = {
       plugins: [require('tailwindcss'), require('autoprefixer')],
     },
   },
+
   webpack: {
-    plugins: [...when(Boolean(process.env.ANALYZE), () => [new BundleAnalyzerPlugin()], [])],
+    configure: {
+      resolve: {
+        fallback: {
+          fs: false,
+          assert: require.resolve('assert'),
+          buffer: require.resolve('buffer'),
+          path: require.resolve('path-browserify'),
+          crypto: require.resolve('crypto-browserify'),
+          http: require.resolve('stream-http'),
+          https: require.resolve('https-browserify'),
+          os: require.resolve('os-browserify/browser'),
+          process: require.resolve('process/browser'),
+          stream: require.resolve('stream-browserify'),
+          zlib: require.resolve('browserify-zlib'),
+          util: require.resolve('util'),
+          url: require.resolve('url'),
+
+        },
+      },
+    },
+
+    plugins: {
+      add: [
+        new webpack.ProvidePlugin({
+          Buffer: ['buffer', 'Buffer'],
+          process: require.resolve('process/browser'),
+        }),
+      ],
+    },
   },
 };
