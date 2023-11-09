c0ee347204bc0a960701a7f22e5b4c478a2e87bb
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2023-11-09 10:34:31 +0100

    fix: fi before make the impact call

--- src/app/analytics/services/analytics.service.ts
+++ src/app/analytics/services/analytics.service.ts
@@ -159,8 +159,9 @@ export async function trackSignUp(uuid, email) {
   try {
     window.rudderanalytics.identify(uuid, { email, uuid: uuid });
     window.rudderanalytics.track('User Signup', { email });
+    window.gtag('event', 'User Signup');
 
-    if (source !== 'direct') {
+    if (source || source !== 'direct') {
       await axios.post(IMPACT_API, {
         anonymousId: anonymousID,
         timestamp: dayjs().format('YYYY-MM-DDTHH:mm:ss.sssZ'),
@@ -338,7 +339,7 @@ export async function trackPaymentConversion() {
       ],
     });
 
-    if (!source || source !== 'direct') {
+    if (source || source !== 'direct') {
       axios
         .post(IMPACT_API, {
           anonymousId: anonymousID,
