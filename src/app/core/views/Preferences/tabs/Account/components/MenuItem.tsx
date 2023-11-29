e98ad578d90b903bf501c5ecc6ffa01b632762bf
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2023-11-29 19:39:29 +0100

    fix: use button instead of div

--- src/app/core/views/Preferences/tabs/Account/components/MenuItem.tsx
+++ src/app/core/views/Preferences/tabs/Account/components/MenuItem.tsx
@@ -2,14 +2,13 @@ import { ReactNode, forwardRef } from 'react';
 
 const MenuItem = forwardRef(({ children, onClick }: { children: ReactNode; onClick: () => void }) => {
   return (
-    <div
-      role="button"
+    <button
       onKeyDown={() => {}}
       className={'flex h-full w-full cursor-pointer px-3 py-2 text-gray-80 hover:bg-gray-5 active:bg-gray-10'}
       onClick={onClick}
     >
       {children}
-    </div>
+    </button>
   );
 });
 
