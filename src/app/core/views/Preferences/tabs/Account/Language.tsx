d17c669fb48b0f72cf01c06d480ef7e312b14bba
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2023-11-29 19:16:27 +0100

    fix: code smells

--- src/app/core/views/Preferences/tabs/Account/Language.tsx
+++ src/app/core/views/Preferences/tabs/Account/Language.tsx
@@ -37,6 +37,7 @@ export default function Language(): JSX.Element {
   const MenuItem = forwardRef(({ children, onClick }: { children: ReactNode; onClick: () => void }) => {
     return (
       <div
+        onKeyDown={() => {}}
         className={'flex h-full w-full cursor-pointer px-3 py-2 text-gray-80 hover:bg-gray-5 active:bg-gray-10'}
         onClick={onClick}
       >
