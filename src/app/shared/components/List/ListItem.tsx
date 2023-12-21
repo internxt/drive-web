2ef1ba9ffbf7d2af2f75dcfc43142ff5758722d0
Álvaro Hernández <34165099+aboredvaro@users.noreply.github.com>
2023-12-21 13:16:05 +0100

    Fix global background

--- src/app/shared/components/List/ListItem.tsx
+++ src/app/shared/components/List/ListItem.tsx
@@ -148,7 +148,7 @@ export default function ListItem<T extends { id: string }>({
       onContextMenu={handleContextMenuClick}
       ref={rootWrapperRef}
       className={`group relative flex h-14 flex-row items-center pl-14 pr-5 ${
-        selected ? 'bg-primary/10 text-gray-100' : 'focus-within:bg-gray-1 hover:bg-gray-1'
+        selected ? 'bg-primary/10 text-gray-100 dark:bg-primary/20' : 'focus-within:bg-gray-1 hover:bg-gray-1'
       }`}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
