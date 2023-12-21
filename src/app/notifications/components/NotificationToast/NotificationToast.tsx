5f6d0d538cb03eac40fe8ace8a17db39c3fb747d
Álvaro Hernández <34165099+aboredvaro@users.noreply.github.com>
2023-12-21 14:07:38 +0100

    Fix Notification Toast

--- src/app/notifications/components/NotificationToast/NotificationToast.tsx
+++ src/app/notifications/components/NotificationToast/NotificationToast.tsx
@@ -61,7 +61,7 @@ const NotificationToast = ({
       show={visible}
     >
       <div
-        className="flex max-w-xl items-center rounded-lg border border-gray-10 bg-white p-3 "
+        className="flex max-w-xl items-center rounded-lg border border-gray-10 bg-surface p-3 dark:bg-gray-5"
         style={{ minWidth: '300px' }}
       >
         {type === ToastType.Loading && <Spinner className="mr-1.5 h-6 w-6" />}
