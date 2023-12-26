55d89d86be3d773a5c6470821187a539f926a108
Álvaro Hernández <34165099+aboredvaro@users.noreply.github.com>
2023-12-26 19:19:35 +0100

    Fix Shared icon

--- src/app/share/views/SharedLinksView/SharedView.tsx
+++ src/app/share/views/SharedLinksView/SharedView.tsx
@@ -945,7 +945,7 @@ function SharedView(props: SharedViewProps): JSX.Element {
                 <div className={'flex h-full w-full flex-row items-center space-x-4 overflow-hidden'}>
                   <div className="relative flex h-10 w-10 shrink items-center justify-center">
                     <Icon className="flex h-full justify-center drop-shadow-soft" />
-                    <div className="absolute -bottom-0.5 -right-1.5 flex h-5 w-5 items-center justify-center rounded-full bg-primary text-white ring-2 ring-white">
+                    <div className="absolute -bottom-0.5 -right-1.5 flex h-5 w-5 items-center justify-center rounded-full bg-primary text-white ring-2 ring-surface">
                       <img src={usersIcon} width={13} alt="shared users" />
                     </div>
                   </div>
