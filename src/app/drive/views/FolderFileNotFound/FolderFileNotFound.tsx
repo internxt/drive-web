b538e62b31bd447edbc99a6f7ac4b5cb4081a610
Álvaro Hernández <34165099+aboredvaro@users.noreply.github.com>
2023-12-26 15:58:28 +0100

    Fixed FileNotFoundView

--- src/app/drive/views/FolderFileNotFound/FolderFileNotFound.tsx
+++ src/app/drive/views/FolderFileNotFound/FolderFileNotFound.tsx
@@ -1,5 +1,5 @@
 import { useTranslationContext } from 'app/i18n/provider/TranslationProvider';
-import bigLogo from 'assets/icons/big-logo.svg';
+import { ReactComponent as InternxtLogo } from 'assets/icons/big-logo.svg';
 import navigationService from 'app/core/services/navigation.service';
 import { WarningCircle } from '@phosphor-icons/react';
 
@@ -8,12 +8,12 @@ export default function FolderFileNotFound(): JSX.Element {
   const isFolder = navigationService.history.location.search.includes('folder');
 
   return (
-    <div className="flex h-full w-full flex-col overflow-auto bg-white">
+    <div className="flex h-full w-full flex-col overflow-auto bg-surface dark:bg-gray-1">
       <div className="flex shrink-0 flex-row justify-center py-10 sm:justify-start sm:pl-20">
-        <img src={bigLogo} width="120" alt="" />
+        <InternxtLogo className="h-auto w-28 text-gray-100" />
       </div>
       <div className="flex h-full flex-col items-center justify-center">
-        <div className="flex max-w-sm flex-col items-center rounded-2xl border border-gray-10 bg-gray-1 p-8 text-center">
+        <div className="flex max-w-sm flex-col items-center rounded-2xl border border-gray-10 bg-gray-1 p-8 text-center dark:bg-gray-5">
           <WarningCircle className=" mb-5 text-center text-red" size={80} weight="thin" />
           <h3 className="pb-1 text-xl font-medium text-gray-100">
             {isFolder ? translate('itemNotFound.folder.title') : translate('itemNotFound.file.title')}
