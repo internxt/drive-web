15e7b36a50e93986b29f4f1549fe76d6c519c91b
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-01-03 15:28:17 +0100

    fix: remove unused useState import

--- src/app/core/views/Preferences/tabs/Account/ContactSupport.tsx
+++ src/app/core/views/Preferences/tabs/Account/ContactSupport.tsx
@@ -2,7 +2,6 @@ import { useChat } from 'react-live-chat-loader';
 import Section from '../../components/Section';
 import Card from 'app/shared/components/Card';
 import Button from 'app/shared/components/Button/Button';
-import { useState } from 'react';
 import { useTranslationContext } from 'app/i18n/provider/TranslationProvider';
 
 const ContactSupport = () => {
