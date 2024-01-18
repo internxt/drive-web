2c14701e9e8649f63412f8a79e7968530aa13b0e
Ramon Candel <mameo@MBP-de-Ramon.home>
2024-01-18 08:06:56 +0100

    Removed console log

--- src/app/tasks/hooks/useOpen.tsx
+++ src/app/tasks/hooks/useOpen.tsx
@@ -10,7 +10,7 @@ export const useOpenItem = (notification: TaskNotification): OpenItem => {
   const openItem = useCallback(() => {
     const { item, taskId, action } = notification;
     const isFolderUpload = action === TaskType.UploadFolder;
-    console.log({ notification });
+
     if (isFolderUpload) {
       const folder = notification?.folderToUpload?.folder;
       const currentFolderId = notification?.folderToUpload?.parentFolderId;
