0bc351c0ec5ca271d7580775616769ec3fcca8dd
Ramon Candel <mameo@MacBook-Pro-de-Ramon.local>
2024-01-30 09:47:59 +0100

    Update logic to handle how uploaded files are added to the store through events or uploadManager

--- src/app/drive/components/DriveExplorer/DriveExplorer.tsx
+++ src/app/drive/components/DriveExplorer/DriveExplorer.tsx
@@ -161,6 +161,9 @@ const DriveExplorer = (props: DriveExplorerProps): JSX.Element => {
 
   // LISTEN NOTIFICATION STATES
   const [folderListenerList, setFolderListenerList] = useState<number[]>([]);
+  const inProcessNotifications = useTaskManagerGetNotifications({
+    status: [TaskStatus.InProcess, TaskStatus.Encrypting],
+  });
 
   // ONBOARDING TUTORIAL STATES
   const [currentTutorialStep, setCurrentTutorialStep] = useState(0);
@@ -170,6 +173,7 @@ const DriveExplorer = (props: DriveExplorerProps): JSX.Element => {
   const successNotifications = useTaskManagerGetNotifications({
     status: [TaskStatus.Success],
   });
+
   const showTutorial =
     useAppSelector(userSelectors.hasSignedToday) &&
     !isSignUpTutorialCompleted &&
@@ -197,7 +201,7 @@ const DriveExplorer = (props: DriveExplorerProps): JSX.Element => {
     if (data.event === SOCKET_EVENTS.FILE_CREATED) {
       const folderId = data.payload.folderId;
 
-      if (folderId === currentFolderId) {
+      if (folderId === currentFolderId && inProcessNotifications.length === 0) {
         dispatch(
           storageActions.pushItems({
             updateRecents: true,
