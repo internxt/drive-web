47fb6d994b94bce8a9a0c5dd9952a72c87328a48
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-02-27 10:55:29 +0100

    feat: add lifetime soft banner

--- src/app/banners/BannerWrapper.tsx
+++ src/app/banners/BannerWrapper.tsx
@@ -8,8 +8,8 @@ import { useSelector } from 'react-redux';
 import FeaturesBanner from './FeaturesBanner';
 import { UserSettings } from '@internxt/sdk/dist/shared/types/userSettings';
 
-const SHOW_BANNER_COOKIE_NAME = 'show_valentines_banner_sale';
-const OFFER_OFF_DAY = new Date('2024-02-25');
+const SHOW_BANNER_COOKIE_NAME = 'show_lifetime_soft_banner';
+const OFFER_OFF_DAY = new Date('2024-02-29');
 
 const BannerWrapper = (): JSX.Element => {
   const [showBanner, setShowBanner] = useState(false);
@@ -17,11 +17,13 @@ const BannerWrapper = (): JSX.Element => {
   const plan = useSelector<RootState, PlanState>((state) => state.plan);
   const isTutorialCompleted = localStorageService.hasCompletedTutorial(user.userId);
   const userPlan = plan.subscription?.type;
+
+  const isNewUser = userPlan === 'free';
   const isNewAccount = useAppSelector(userSelectors.hasSignedToday);
   const isLocalStorage = localStorageService.get(SHOW_BANNER_COOKIE_NAME);
   const isOfferOffDay = new Date() > OFFER_OFF_DAY;
 
-  const shouldShowBanner = userPlan === 'free' && !isLocalStorage && !isOfferOffDay;
+  const shouldShowBanner = isNewUser && !isLocalStorage && !isOfferOffDay;
 
   useEffect(() => {
     handleBannerDisplay();
