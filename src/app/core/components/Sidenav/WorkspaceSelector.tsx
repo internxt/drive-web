8201c0117b7e728aa9ecc7c1210fa9f2a5bedd00
Ramon Candel <mameo@MacBook-Pro-de-Ramon.local>
2024-03-15 11:18:51 +0100

    Styles adjustments

--- src/app/core/components/Sidenav/WorkspaceSelector.tsx
+++ src/app/core/components/Sidenav/WorkspaceSelector.tsx
@@ -33,7 +33,7 @@ const WorkspaceCard = ({
   };
 
   return (
-    <button className="w-full px-2 py-3 text-left hover:bg-gray-10" onClick={handleOnClick}>
+    <button className="w-full px-2 py-3 text-left hover:bg-gray-5 dark:hover:bg-gray-10" onClick={handleOnClick}>
       <div className="flex w-full flex-row items-center justify-between space-x-2">
         <Avatar
           diameter={28}
@@ -46,7 +46,7 @@ const WorkspaceCard = ({
             {translate(`workspaces.workspaceTypes.${workspace.type.toLocaleLowerCase()}`)}
           </text>
         </div>
-        <div className="h-4 w-4">{isSelected && <Icon color="white" weight="bold" size={16} />}</div>
+        <div className="h-4 w-4">{isSelected && <Icon colorRendering="bg-gray-100" weight="bold" size={16} />}</div>
       </div>
     </button>
   );
@@ -69,8 +69,11 @@ const WorkspaceSelector: React.FC<WorkspaceSelectorProps> = ({ workspaces, onCre
 
   return (
     <div className="relative mb-2 inline-block w-full">
+      {/* TOOGLE BUTTON */}
       <button
-        className="w-full justify-center rounded-lg border border-gray-10 bg-gray-5 p-3 text-left"
+        className={`w-full justify-center rounded-lg border border-gray-10 ${
+          isOpen ? 'bg-gray-1' : 'bg-surface'
+        } p-3 text-left dark:bg-gray-5`}
         onClick={toggleDropdown}
       >
         <div className="flex w-full flex-row items-center justify-between space-x-2">
@@ -86,17 +89,18 @@ const WorkspaceSelector: React.FC<WorkspaceSelectorProps> = ({ workspaces, onCre
             </text>
           </div>
           <div className="w-4">
-            <CaretUpDown color="white" weight="bold" size={16} />
+            <CaretUpDown colorRendering="bg-gray-100" weight="bold" size={16} />
           </div>
         </div>
       </button>
+      {/* DROPDOWN LIST */}
       <div
         className={`fixed left-2 z-50 w-72 transform transition-all duration-300 ease-in-out ${
           isOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'
         }`}
       >
         <div className="h-1"></div>
-        <div className="overflow-hidden rounded-lg border border-gray-10 bg-gray-5 shadow-xl">
+        <div className="overflow-hidden rounded-lg border border-gray-10 bg-surface shadow-xl dark:bg-gray-5">
           <p className="px-2 pt-3 text-sm font-medium text-gray-100">{translate('workspaces.workspaces')}</p>
           {workspaces.map((workspace) => (
             <WorkspaceCard
@@ -110,7 +114,7 @@ const WorkspaceSelector: React.FC<WorkspaceSelectorProps> = ({ workspaces, onCre
           ))}
           <div className="mx-3 h-px bg-gray-10"></div>
           <button
-            className="w-full rounded-b-lg px-2 py-3 text-left text-sm font-medium leading-4 text-gray-100 hover:bg-gray-10"
+            className="w-full rounded-b-lg px-2 py-3 text-left text-sm font-medium leading-4 text-gray-100 hover:bg-gray-5 dark:hover:bg-gray-10"
             onClick={onCreateWorkspaceButtonClicked}
           >
             {translate('workspaces.createWorkspace')}
