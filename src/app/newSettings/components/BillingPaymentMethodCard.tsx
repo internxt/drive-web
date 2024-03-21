9e024bc383f0158cdb02f2d027617c50a953ef76
Rafa <rafijv@gmail.com>
2024-03-21 10:50:33 +0100

    [PB-1816] add useDefaultPaymentMethod hook

--- src/app/newSettings/components/BillingPaymentMethodCard.tsx
+++ src/app/newSettings/components/BillingPaymentMethodCard.tsx
@@ -1,10 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { t } from 'i18next';
 import { PaymentMethod } from '@internxt/sdk/dist/drive/payments/types';
-import { Source } from '@stripe/stripe-js';
-
-import paymentService from 'app/payment/services/payment.service';
-
 import Spinner from 'app/shared/components/Spinner/Spinner';
 import Button from 'app/shared/components/Button/Button';
 import Card from 'app/shared/components/Card';
@@ -18,18 +14,11 @@ import mastercardIcon from '../../../assets/icons/card-brands/mastercard.png';
 import unionpayIcon from '../../../assets/icons/card-brands/unionpay.png';
 import unknownIcon from '../../../assets/icons/card-brands/unknown.png';
 import EditPaymentMethodModal from './EditPaymentMethodModal';
-
-interface StateProps {
-  tag: 'ready' | 'loading' | 'empty';
-  card?: PaymentMethod['card'];
-  type?: Source.Type;
-}
+import { useDefaultPaymentMethod } from '../hooks/useDefaultPaymentMethod';
 
 const BillingPaymentMethodCard = () => {
   const [isEditPaymentMethodModalOpen, setIsEditPaymentMethodModalOpen] = useState<boolean>(false);
-  const [state, setState] = useState<StateProps>({
-    tag: 'loading',
-  });
+  const defaultPaymentMethod = useDefaultPaymentMethod();
 
   const cardBrands: Record<PaymentMethod['card']['brand'], string> = {
     visa: visaIcon,
@@ -47,21 +36,6 @@ const BillingPaymentMethodCard = () => {
     sepa_debit: 'SEPA Direct Debit',
   };
 
-  useEffect(() => {
-    paymentService
-      .getDefaultPaymentMethod()
-      .then((data: PaymentMethod | Source) => {
-        if (data.card) {
-          setState({ tag: 'ready', card: data.card as PaymentMethod['card'] });
-        } else if ('type' in data) {
-          setState({ tag: 'ready', type: data.type });
-        } else {
-          setState({ tag: 'empty' });
-        }
-      })
-      .catch(() => setState({ tag: 'empty' }));
-  }, []);
-
   const onEditPaymentMethodButtonClicked = () => {
     setIsEditPaymentMethodModalOpen(true);
   };
@@ -75,27 +49,28 @@ const BillingPaymentMethodCard = () => {
         </Button>
       </div>
       <Card>
-        {(state.tag === 'ready' && state.card) || (state.tag === 'ready' && state.type) ? (
+        {(defaultPaymentMethod.tag === 'ready' && defaultPaymentMethod.card) ||
+        (defaultPaymentMethod.tag === 'ready' && defaultPaymentMethod.type) ? (
           <div className="flex">
-            {state.card ? (
+            {defaultPaymentMethod.card ? (
               <>
-                <img className="h-9 rounded-md" src={cardBrands[state.card.brand]} />
+                <img className="h-9 rounded-md" src={cardBrands[defaultPaymentMethod.card.brand]} />
                 <div className="ml-4 flex-1">
                   <div className="flex items-center text-gray-80">
                     <p style={{ lineHeight: 1 }} className="font-serif text-2xl">
                       {'····'}
                     </p>
-                    <p className="ml-1.5 text-sm">{state.card.last4}</p>
+                    <p className="ml-1.5 text-sm">{defaultPaymentMethod.card.last4}</p>
                   </div>
-                  <p className="text-xs text-gray-50">{`${state.card.exp_month}/${state.card.exp_year}`}</p>
+                  <p className="text-xs text-gray-50">{`${defaultPaymentMethod.card.exp_month}/${defaultPaymentMethod.card.exp_year}`}</p>
                 </div>
               </>
             ) : (
-              state.type && (
+              defaultPaymentMethod.type && (
                 <>
                   <div className="ml-4 flex-1">
                     <div className="flex items-center text-gray-100">
-                      <p className="text-base font-medium leading-tight">{paymentsTypes[state.type]}</p>
+                      <p className="text-base font-medium leading-tight">{paymentsTypes[defaultPaymentMethod.type]}</p>
                     </div>
                     <p className="text-sm text-gray-50">
                       {t('views.account.tabs.billing.paymentMethod.contactUs.description')}
@@ -105,7 +80,7 @@ const BillingPaymentMethodCard = () => {
               )
             )}
           </div>
-        ) : state.tag === 'loading' ? (
+        ) : defaultPaymentMethod.tag === 'loading' ? (
           <div className="flex h-10 items-center justify-center">
             <Spinner className="h-5 w-5" />
           </div>
