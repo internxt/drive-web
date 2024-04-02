ef010e99fe1e0f68423729fed99e37c77a6567d7
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-04-02 16:05:54 +0200

    feat: add world cloud security banner

--- src/app/banners/FeaturesBanner.tsx
+++ src/app/banners/FeaturesBanner.tsx
@@ -1,4 +1,4 @@
-import { CheckCircle, Flower, X } from '@phosphor-icons/react';
+import { CheckCircle, ShieldCheck, X } from '@phosphor-icons/react';
 import { useTranslationContext } from 'app/i18n/provider/TranslationProvider';
 
 const FeaturesBanner = ({ showBanner, onClose }: { showBanner: boolean; onClose: () => void }): JSX.Element => {
@@ -7,7 +7,7 @@ const FeaturesBanner = ({ showBanner, onClose }: { showBanner: boolean; onClose:
   const features = translateList('featuresBanner.features');
 
   const handleOnClick = () => {
-    window.open('https://internxt.com/pricing', '_blank', 'noopener noreferrer');
+    window.open('https://internxt.com/lifetime', '_blank', 'noopener noreferrer');
   };
 
   return (
@@ -25,14 +25,12 @@ const FeaturesBanner = ({ showBanner, onClose }: { showBanner: boolean; onClose:
         <button className="absolute  right-0 m-7 flex text-black" onClick={onClose}>
           <X size={32} />
         </button>
-        <div className="flex w-full max-w-[800px] flex-col space-x-10 py-14 lg:flex-row">
-          <div className="flex w-full flex-col  items-center justify-center space-y-3 text-center lg:items-start lg:justify-between lg:text-start">
-            <div className="flex rounded-lg bg-white px-3 py-1.5 ring-4 ring-primary/7">
-              <p className="text-2xl font-bold text-primary">{translate('featuresBanner.label')}</p>
+        <div className="flex w-full flex-col space-x-10 py-14 lg:flex-row">
+          <div className="flex  w-max max-w-[310px] flex-col  items-center justify-center space-y-3 text-center lg:items-start lg:justify-between lg:text-start">
+            <div className="flex rounded-lg bg-white">
+              <p className="text-xl font-bold text-primary">{translate('featuresBanner.label')}</p>
             </div>
-            <p className="w-full max-w-[380px] text-4xl font-bold leading-tight text-black">
-              {translate('featuresBanner.title')}
-            </p>
+            <p className="w-full text-4xl font-bold leading-tight text-black">{translate('featuresBanner.title')}</p>
 
             <div className="flex flex-col items-center space-y-3 lg:items-start">
               <button
@@ -43,7 +41,7 @@ const FeaturesBanner = ({ showBanner, onClose }: { showBanner: boolean; onClose:
               </button>
               <div className="flex flex-row items-center space-x-3 pt-2 text-gray-80 dark:text-gray-20">
                 <CheckCircle size={24} className="" />
-                <p className="whitespace-nowrap font-medium lg:text-lg">{translate('featuresBanner.guarantee')}</p>
+                <p className="font-medium lg:text-lg">{translate('featuresBanner.guarantee')}</p>
               </div>
 
               <p className="text-sm font-medium text-gray-50">{translate('featuresBanner.lastCta')}</p>
@@ -54,8 +52,8 @@ const FeaturesBanner = ({ showBanner, onClose }: { showBanner: boolean; onClose:
               <div className="flex flex-col space-y-8">
                 {features.map((card) => (
                   <div className="flex flex-row" key={card}>
-                    <Flower size={30} weight="fill" className="mr-4 text-primary" />
-                    <p className="whitespace-nowrap text-lg font-semibold text-gray-80 dark:text-gray-20">{card}</p>
+                    <ShieldCheck size={30} weight="fill" className="mr-4 text-primary" />
+                    <p className="whitespace-nowrap text-xl font-semibold text-gray-80 dark:text-gray-20">{card}</p>
                   </div>
                 ))}
               </div>
