1bc7a9f66a25c0ed1ce239c56a35963e3bd204ee
Rafa <rafijv@gmail.com>
2024-04-23 13:13:53 +0200

    [PB-1990] fix non interactive elements with event listener

--- src/app/shared/components/forms/RadioButton/RadioButton.tsx
+++ src/app/shared/components/forms/RadioButton/RadioButton.tsx
@@ -5,14 +5,15 @@ interface RadioButtonProps {
 
 const RadioButton = ({ checked, onClick }: RadioButtonProps) => {
   return (
-    <label className="flex cursor-pointer" onClick={onClick}>
-      <div
+    <label className="flex cursor-pointer">
+      <button
+        onClick={onClick}
         className={`flex h-5 w-5 items-center justify-center rounded-full border border-gray-40 ${
           checked && 'border-0 bg-primary'
         }`}
       >
         {checked && <div className="h-2.5 w-2.5 rounded-full bg-white"></div>}
-      </div>
+      </button>
       <input type="radio" className="h-0 w-0 appearance-none opacity-0" checked />
     </label>
   );
