9c78edd6eba29cddfd2bf6d113d0251edaf7b9c3
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-05-18 14:55:24 +0200

    fix: add wallpaper in onSuccess function.

--- src/app/core/views/Preferences/tabs/Account/Appearance.tsx
+++ src/app/core/views/Preferences/tabs/Account/Appearance.tsx
@@ -9,6 +9,7 @@ import { Theme, useThemeContext } from '../../../../../theme/ThemeProvider';
 import Section from '../../components/Section';
 import ItemsDropdown from './components/ItemsDropdown';
 import MenuItem from './components/MenuItem';
+import errorService from 'app/core/services/error.service';
 
 const initialAppearances: Theme[] = ['system', 'light', 'dark'];
 
@@ -20,7 +21,11 @@ const Appearance = () => {
   const [appearances, setAppearances] = useState<Theme[]>(initialAppearances);
 
   useEffect(() => {
-    isStarWarsThemeAvailable(plan)
+    isStarWarsThemeAvailable(plan, () => {
+      if (!appearances.includes('starwars')) {
+        setAppearances([...appearances, 'starwars']);
+      }
+    })
       .then((isStarWarsThemeAvailable) => {
         if (!appearances.includes('starwars') && isStarWarsThemeAvailable) {
           setAppearances([...appearances, 'starwars']);
@@ -28,7 +33,7 @@ const Appearance = () => {
       })
       .catch((err) => {
         const error = err as Error;
-        console.error(error.message);
+        errorService.reportError(error);
       });
   }, []);
 
