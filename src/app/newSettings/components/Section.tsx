1ed3cabd3bc2f1952f54dbbaf4096fa9b5690ecd
Álvaro Hernández <34165099+aboredvaro@users.noreply.github.com>
2024-05-27 20:31:44 +0200

    Update Section style

--- src/app/newSettings/components/Section.tsx
+++ src/app/newSettings/components/Section.tsx
@@ -16,11 +16,11 @@ const Section = ({
 }): JSX.Element => {
   return (
     <div className={`relative w-full rounded-tr-2xl ${className}`}>
-      <div className="fixed absolute z-50 flex w-full items-center justify-between rounded-tr-2xl bg-surface/85 p-2.5 pl-6 backdrop-blur-3xl">
-        <div className="flex">
+      <div className="absolute z-50 flex w-full items-center justify-between rounded-tr-2xl p-2.5 pl-6 before:absolute before:inset-0 before:-z-1 before:bg-surface/85 before:backdrop-blur-3xl">
+        <div className="flex items-center">
           {onBackButtonClicked && (
             <button onClick={onBackButtonClicked}>
-              <div className="mr-2.5 text-gray-100">
+              <div className="mr-2.5 flex h-9 w-9 items-center justify-center rounded-lg text-gray-100 hover:bg-highlight/4 active:bg-highlight/8">
                 <CaretLeft size={22} />
               </div>
             </button>
@@ -28,13 +28,13 @@ const Section = ({
           <h2 className="text-base font-medium text-gray-100">{title}</h2>
         </div>
         <button
-          className="rounded-md p-2 hover:bg-highlight/4 focus:bg-highlight/8"
+          className="flex h-9 w-9 items-center justify-center rounded-md hover:bg-highlight/4 active:bg-highlight/8"
           onClick={() => onClosePreferences()}
         >
           <X size={22} />
         </button>
       </div>
-      <div className="flex max-h-640 flex-col space-y-6 overflow-y-auto p-6 pt-16">{children}</div>
+      <div className="flex max-h-640 flex-col space-y-8 overflow-y-auto p-6 pt-16">{children}</div>
     </div>
   );
 };
