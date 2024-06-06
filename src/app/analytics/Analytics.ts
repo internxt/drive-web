26aabad05af1fe414e2c399960717b2834d5fa10
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-06-06 17:36:36 +0200

    feat: modifying context app name for rudder tracks

--- src/app/analytics/Analytics.ts
+++ src/app/analytics/Analytics.ts
@@ -1,5 +1,44 @@
+// analytics.ts
 import { sendAnalyticsError } from './utils';
 import errorService from '../core/services/error.service';
+
+const CONTEXT_APP_NAME = 'drive-web';
+
+class RudderAnalyticsWrapper {
+  constructor() {
+    const methods = [
+      'load',
+      'page',
+      'track',
+      'identify',
+      'alias',
+      'group',
+      'ready',
+      'reset',
+      'getAnonymousId',
+      'setAnonymousId',
+      'getUserId',
+      'getUserTraits',
+      'getGroupId',
+      'getGroupTraits',
+    ];
+
+    methods.forEach((method) => {
+      const originalMethod = window.rudderanalytics[method];
+      window.rudderanalytics[method] = (...args: any[]) => {
+        if (['track', 'page', 'identify', 'alias', 'group'].includes(method)) {
+          args[2] = args[2] || {};
+          args[2].context = args[2].context || {};
+          args[2].context.app = args[2].context.app || {};
+          args[2].context.app.name = CONTEXT_APP_NAME;
+        }
+        return originalMethod.apply(window.rudderanalytics, args);
+      };
+    });
+  }
+}
+
+new RudderAnalyticsWrapper();
 
 export default class Analytics {
   private static instance: Analytics;
@@ -20,7 +59,7 @@ export default class Analytics {
     return Analytics.instance;
   }
 
-  public track(eventName, properties) {
+  public track(eventName: string, properties: any) {
     try {
       Analytics.instance.track(eventName, properties);
     } catch (err) {
