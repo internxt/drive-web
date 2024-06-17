bf315e5da56de525a0ba31ebb337d80f79e424f2
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-06-17 15:28:52 +0200

    fix: use local storage instead of cookies

--- src/app/banners/BannerWrapper.tsx
+++ src/app/banners/BannerWrapper.tsx
@@ -9,12 +9,10 @@ import { useSelector } from 'react-redux';
 import { UserSettings } from '@internxt/sdk/dist/shared/types/userSettings';
 
 import FeaturesBanner from './FeaturesBanner';
-import { getCookie, setInternalCookie } from 'app/analytics/utils';
 
-const SHOW_BANNER_COOKIE_NAME = 'show_euro_banner';
+const SHOW_BANNER_NAME = 'show_banner';
+const EXPIRATION_DATE_NAME = 'expiration_banner_date';
 const OFFER_END_DAY = new Date('2024-07-14');
-
-const COOKIE_EXPIRE_DATE = OFFER_END_DAY.getDate() - new Date().getDate();
 
 const BannerWrapper = (): JSX.Element => {
   const [showBanner, setShowBanner] = useState(false);
@@ -23,19 +21,30 @@ const BannerWrapper = (): JSX.Element => {
   const isTutorialCompleted = localStorageService.hasCompletedTutorial(user.userId);
   const userPlan = plan.subscription?.type;
 
+  const expirationBannerInLocalStorage = localStorageService.get(EXPIRATION_DATE_NAME);
+  const nameBannerInLocalStorage = localStorageService.get(SHOW_BANNER_NAME);
+
   const isNewUser = userPlan === 'free';
   const isNewAccount = useAppSelector(userSelectors.hasSignedToday);
-  const showBannerCookieExists = getCookie(SHOW_BANNER_COOKIE_NAME);
   const isOfferOffDay = new Date() > OFFER_END_DAY;
+  const shouldRemoveLocalStorageItems =
+    new Date().toISOString() > JSON.parse(expirationBannerInLocalStorage as string) || isOfferOffDay;
 
-  const shouldShowBanner = isNewUser && !showBannerCookieExists && !isOfferOffDay;
+  const showBannerIfLocalStorageExists = !nameBannerInLocalStorage && !expirationBannerInLocalStorage;
+
+  const shouldShowBanner = isNewUser && showBannerIfLocalStorageExists && !isOfferOffDay;
 
   useEffect(() => {
     handleBannerDisplay();
+    if (shouldRemoveLocalStorageItems) {
+      localStorageService.removeItem(SHOW_BANNER_NAME);
+      localStorageService.removeItem(EXPIRATION_DATE_NAME);
+    }
   }, [isTutorialCompleted, userPlan, isNewAccount]);
 
   const onCloseBanner = () => {
-    setInternalCookie(SHOW_BANNER_COOKIE_NAME, 'false', COOKIE_EXPIRE_DATE);
+    localStorageService.set(SHOW_BANNER_NAME, 'false');
+    localStorageService.set(EXPIRATION_DATE_NAME, JSON.stringify(OFFER_END_DAY));
     setShowBanner(false);
   };
 
