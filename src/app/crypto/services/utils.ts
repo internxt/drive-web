f1d0be695f412ad7586c953346c88bef1eb4f07f
Ramon Candel <mameo@MacBook-Pro-de-Ramon.local>
2024-06-18 17:44:06 +0200

    Added TODO to check encryptFilename change

--- src/app/crypto/services/utils.ts
+++ src/app/crypto/services/utils.ts
@@ -1,8 +1,8 @@
+import { aes, items as itemUtils } from '@internxt/lib';
 import CryptoJS from 'crypto-js';
 import { DriveItemData } from '../../drive/types';
-import { aes, items as itemUtils } from '@internxt/lib';
-import { getAesInitFromEnv } from '../services/keys.service';
 import { AdvancedSharedItem } from '../../share/types';
+import { getAesInitFromEnv } from '../services/keys.service';
 
 interface PassObjectInterface {
   salt?: string | null;
@@ -51,6 +51,7 @@ function decryptTextWithKey(encryptedText: string, keyToDecrypt: string): string
   return bytes.toString(CryptoJS.enc.Utf8);
 }
 
+// TODO: BEFORE MERGE! - GET SURE IF THIS COULD BREAK SOMETHING OLD
 function encryptFilename(filename: string, folderId: string): string {
   const { REACT_APP_CRYPTO_SECRET2: CRYPTO_KEY } = process.env;
 
@@ -86,13 +87,13 @@ const getItemPlainName = (item: DriveItemData | AdvancedSharedItem) => {
 };
 
 export {
-  passToHash,
-  encryptText,
   decryptText,
-  encryptFilename,
-  encryptTextWithKey,
   decryptTextWithKey,
+  encryptFilename,
+  encryptText,
+  encryptTextWithKey,
   excludeHiddenItems,
-  renameFile,
   getItemPlainName,
+  passToHash,
+  renameFile,
 };
