10d46cc5e6e13c3d62426b59e85e5cc385e1bd81
Rafa <rafijv@gmail.com>
2024-07-11 12:18:47 +0200

    fix workspace breadcrumbs

--- src/app/shared/components/Breadcrumbs/Containers/BreadcrumbsDriveView.tsx
+++ src/app/shared/components/Breadcrumbs/Containers/BreadcrumbsDriveView.tsx
@@ -2,7 +2,8 @@ import navigationService from 'app/core/services/navigation.service';
 import { AppView } from 'app/core/types';
 import { FolderPath } from 'app/drive/types';
 import Breadcrumbs from 'app/shared/components/Breadcrumbs/Breadcrumbs';
-import { useAppDispatch } from 'app/store/hooks';
+import { useAppDispatch, useAppSelector } from 'app/store/hooks';
+import { RootState } from 'app/store';
 import storageThunks from 'app/store/slices/storage/storage.thunks';
 import { uiActions } from 'app/store/slices/ui';
 import { t } from 'i18next';
@@ -16,12 +17,16 @@ interface BreadcrumbsDriveViewProps {
 const BreadcrumbsDriveView = (props: BreadcrumbsDriveViewProps) => {
   const { namePath } = props;
   const dispatch = useAppDispatch();
+  const { selectedWorkspace } = useAppSelector((state: RootState) => state.workspaces);
 
   const breadcrumbDriveViewItems = (): BreadcrumbItemData[] => {
     const items: BreadcrumbItemData[] = [];
 
     if (namePath.length > 0) {
-      const firstPath = namePath[0];
+      const breadcrumbNamePath = [...namePath];
+      const parsedBreadcrumbNamePath =
+        selectedWorkspace && breadcrumbNamePath.length > 1 ? breadcrumbNamePath.slice(1) : breadcrumbNamePath;
+      const firstPath = parsedBreadcrumbNamePath[0];
 
       items.push({
         uuid: firstPath.uuid,
@@ -36,7 +41,7 @@ const BreadcrumbsDriveView = (props: BreadcrumbsDriveViewProps) => {
         },
       });
 
-      namePath.slice(1).forEach((path: FolderPath, i: number, namePath: FolderPath[]) => {
+      parsedBreadcrumbNamePath.slice(1).forEach((path: FolderPath, i: number, namePath: FolderPath[]) => {
         items.push({
           uuid: path.uuid,
           label: path.name,
