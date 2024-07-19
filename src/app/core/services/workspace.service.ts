5066f06258769cc36b78bdfd6c3bac39b1ea6695
Ramon Candel <mameo@MBP-de-Ramon.home>
2024-07-19 12:40:12 +0200

    Added reactivate member modal - WIP

--- src/app/core/services/workspace.service.ts
+++ src/app/core/services/workspace.service.ts
@@ -347,6 +347,14 @@ export function deactivateMember(workspaceId: string, memberId: string): Promise
   });
 }
 
+export function reactivateMember(workspaceId: string, memberId: string): Promise<void> {
+  const workspaceClient = SdkFactory.getNewApiInstance().createWorkspacesClient();
+  // ADD REACTIVATION CALL
+  // return workspaceClient.deactivateMember(workspaceId, memberId).catch((error) => {
+  //   throw errorService.castError(error);
+  // });
+}
+
 export function getUsage(workspaceId: string): Promise<GetMemberUsageResponse> {
   const workspaceClient = SdkFactory.getNewApiInstance().createWorkspacesClient();
   return workspaceClient.getMemberUsage(workspaceId).catch((error) => {
@@ -391,6 +399,7 @@ const workspacesService = {
   getWorkspaceFolders,
   getWorkspaceFiles,
   deactivateMember,
+  reactivateMember,
   getUsage,
 };
 
