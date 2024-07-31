8a9a85333849e0df4daa9407620881351c1d5540
Rafa <rafijv@gmail.com>
2024-07-31 16:16:19 +0200

    [PB-2406] add list teams

--- src/app/newSettings/Sections/Workspace/Teams/TeamsSection.tsx
+++ src/app/newSettings/Sections/Workspace/Teams/TeamsSection.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { t } from 'i18next';
+import { WorkspaceTeamResponse } from '@internxt/sdk/dist/workspaces/types';
 
 import { useAppSelector } from 'app/store/hooks';
 import workspacesSelectors from 'app/store/slices/workspaces/workspaces.selectors';
 import workspacesService from 'app/core/services/workspace.service';
 import errorService from 'app/core/services/error.service';
+import notificationsService, { ToastType } from 'app/notifications/services/notifications.service';
 
 import Section from 'app/newSettings/components/Section';
 import TeamsList from './components/TeamsList';
@@ -12,34 +14,77 @@ import CreateTeamDialog from './components/CreateTeamDialog';
 
 const TeamsSection = ({ onClosePreferences }: { onClosePreferences: () => void }) => {
   const selectedWorkspace = useAppSelector(workspacesSelectors.getSelectedWorkspace);
+  const isCurrentUserWorkspaceOwner = useAppSelector(workspacesSelectors.isWorkspaceOwner);
 
+  const [teams, setTeams] = useState<WorkspaceTeamResponse>([]);
   const [createTeamDialogOpen, setCreateTeamDialogOpen] = useState(false);
   const [newTeamName, setNewTeamName] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    getTeams();
+  }, []);
+
+  const getTeams = async () => {
+    if (selectedWorkspace) {
+      try {
+        const teams = await workspacesService.getWorkspaceTeams(selectedWorkspace.workspaceUser.workspaceId);
+        setTeams(teams);
+      } catch (err) {
+        const castedError = errorService.castError(err);
+        errorService.reportError(castedError);
+        notificationsService.show({
+          text: castedError.message,
+          type: ToastType.Error,
+        });
+      }
+    }
+  };
 
   const createTeam = async () => {
     setIsLoading(true);
     if (selectedWorkspace) {
       try {
+        const nameExists = teams.some((team) => team.team.name === newTeamName);
+
+        if (nameExists) {
+          notificationsService.show({
+            text: t('preferences.workspace.teams.createTeamDialog.nameExists'),
+            type: ToastType.Error,
+          });
+          return;
+        }
+
         await workspacesService.createTeam({
           workspaceId: selectedWorkspace.workspaceUser.workspaceId,
           name: newTeamName,
           managerId: selectedWorkspace.workspaceUser.memberId,
         });
+        getTeams();
       } catch (err) {
-        const error = errorService.castError(err);
-        errorService.reportError(error);
+        const castedError = errorService.castError(err);
+        errorService.reportError(castedError);
+        notificationsService.show({
+          text: castedError.message,
+          type: ToastType.Error,
+        });
       } finally {
         setIsLoading(false);
-        setCreateTeamDialogOpen(false);
-        setNewTeamName('');
       }
+
+      setIsLoading(false);
+      setCreateTeamDialogOpen(false);
+      setNewTeamName('');
     }
   };
 
   return (
     <Section title={t('preferences.workspace.teams.title')} onClosePreferences={onClosePreferences}>
-      <TeamsList setCreateTeamDialogOpen={setCreateTeamDialogOpen} />
+      <TeamsList
+        setCreateTeamDialogOpen={setCreateTeamDialogOpen}
+        teams={teams}
+        isCurrentUserWorkspaceOwner={isCurrentUserWorkspaceOwner}
+      />
       <CreateTeamDialog
         isOpen={createTeamDialogOpen}
         onClose={() => setCreateTeamDialogOpen(false)}
--- src/app/newSettings/Sections/Workspace/Teams/components/TeamsList.tsx
+++ src/app/newSettings/Sections/Workspace/Teams/components/TeamsList.tsx
@@ -1,28 +1,51 @@
-import Button from 'app/shared/components/Button/Button';
 import { t } from 'i18next';
+import { WorkspaceTeamResponse } from '@internxt/sdk/dist/workspaces/types';
+
+import Button from 'app/shared/components/Button/Button';
 
 interface TeamsListProps {
   setCreateTeamDialogOpen: (value: boolean) => void;
+  teams: WorkspaceTeamResponse;
+  isCurrentUserWorkspaceOwner: boolean;
 }
 
-const TeamsList: React.FC<TeamsListProps> = ({ setCreateTeamDialogOpen }) => {
+const TeamsList: React.FC<TeamsListProps> = ({ setCreateTeamDialogOpen, teams, isCurrentUserWorkspaceOwner }) => {
   return (
     <>
       <div className="mt-2 flex items-center justify-between">
-        <h2 className="text-xl font-medium text-gray-100">1 team</h2>
-        <Button variant="primary" onClick={() => setCreateTeamDialogOpen(true)}>
-          {t('preferences.workspace.members.invite')}
-        </Button>
+        <h2 className="text-xl font-medium text-gray-100">
+          <span>{teams.length} </span>
+          <span>
+            {teams.length != 1 ? t('preferences.workspace.teams.title') : t('preferences.workspace.teams.team')}
+          </span>
+        </h2>
+        {isCurrentUserWorkspaceOwner && (
+          <Button variant="primary" onClick={() => setCreateTeamDialogOpen(true)}>
+            {t('preferences.workspace.teams.createTeam')}
+          </Button>
+        )}
       </div>
       <div className="rounded-xl">
         <div className="grid h-12 grid-cols-3 rounded-t-xl border border-gray-10 bg-gray-1 py-2 text-base font-medium">
-          <div className="col-span-2 flex items-center border-r border-gray-10 pl-5">Team</div>
-          <div className="col-span-1 flex items-center pl-5">Members</div>
+          <div className="col-span-2 flex items-center border-r border-gray-10 pl-5">
+            {t('preferences.workspace.teams.team')}
+          </div>
+          <div className="col-span-1 flex items-center pl-5">{t('preferences.workspace.teams.members')}</div>
         </div>
-        <div className="grid h-12 grid-cols-3 rounded-b-xl border-x border-b border-gray-10 bg-surface py-2 text-base font-medium">
-          <div className="col-span-2 flex items-center pl-5">Development</div>
-          <div className="font-regular col-span-1 flex items-center pl-5">5</div>
-        </div>
+        {teams.map((team) => {
+          const isLastTeam = teams.length === teams.indexOf(team) + 1;
+          return (
+            <div
+              key={team.team.id}
+              className={`grid h-12 cursor-pointer grid-cols-3 border-x border-b border-gray-10 bg-surface py-2 text-base font-medium hover:bg-gray-5 ${
+                isLastTeam && 'rounded-b-xl'
+              }`}
+            >
+              <div className="col-span-2 flex items-center pl-5">{team.team.name}</div>
+              <div className="font-regular col-span-1 flex items-center pl-5">{team.membersCount}</div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
