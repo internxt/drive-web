db4b545cd9cb5caed5622152b8f8a978c2186513
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-08-08 09:54:24 +0200

    feat: summer banner

--- src/app/banners/FeaturesBanner.tsx
+++ src/app/banners/FeaturesBanner.tsx
@@ -27,51 +27,38 @@ const FeaturesBanner = ({ showBanner, onClose }: { showBanner: boolean; onClose:
         <button className="absolute  right-0 m-7 flex text-black hover:bg-white/5" onClick={onClose}>
           <X size={32} />
         </button>
-        <div className="flex w-full flex-col space-x-10 py-14 lg:flex-row">
-          <div className="flex  w-max max-w-[400px] flex-col  items-center justify-center space-y-3 text-center lg:items-start lg:justify-between lg:text-start">
-            <div className="flex items-center gap-1 rounded-xl bg-primary/10 px-3 py-1.5 font-bold text-primary">
-              {/* <p className="text-xs">{translate('featuresBanner.label.upTo')}</p> */}
-              <p className="text-2xl">{translate('featuresBanner.label.discount')}</p>
+        <div className="flex w-max max-w-[900px] flex-col space-x-10 py-14 text-black lg:flex-row">
+          <div className="flex w-full flex-col  items-center justify-center space-y-3 text-center lg:items-start lg:justify-between lg:text-start">
+            <div className="flex rounded-lg border-4 border-primary/7 px-3 py-1.5">
+              <p className="text-2xl font-bold text-primary">{translate('featuresBanner.label')}</p>
             </div>
-            <p className="w-full text-5xl font-bold leading-tight text-gray-80 dark:text-gray-20">
+            <p className="w-full max-w-[400px] text-5xl font-bold leading-tight ">
               {translate('featuresBanner.title')}
             </p>
-
-            {/* <p className="w-full text-lg font-semibold text-gray-80 dark:text-gray-20">
-              {translate('featuresBanner.subtitle.line1')}{' '}
-              <span className="font-bold">
-                {translate('featuresBanner.subtitle.line2.normal1')}
-                <button onClick={handleCopyOnClipboardAction} className="cursor-pointer text-primary hover:underline">
-                  {translate('featuresBanner.subtitle.line2.blue')}
-                </button>
-                {translate('featuresBanner.subtitle.line2.normal2')}
-              </span>
-            </p> */}
-
+            <p className="w-full max-w-[328px] text-2xl font-bold leading-tight ">
+              {translate('featuresBanner.subtitle')}
+            </p>
             <div className="flex flex-col items-center space-y-3 lg:items-start">
               <button
                 onClick={handleOnClick}
-                className="flex w-max items-center rounded-lg bg-primary px-5 py-3 text-lg font-medium text-white hover:bg-primary-dark"
+                className="flex w-max items-center rounded-lg bg-primary px-5 py-3 text-lg font-medium text-white  hover:bg-primary-dark"
               >
                 {translate('featuresBanner.cta')}
               </button>
-              <div className="flex flex-row space-x-3 pt-2 text-gray-80 dark:text-gray-20">
-                <div className="flex h-6 w-6">
-                  <CheckCircle size={24} className="mt-0.5 w-max text-primary" />
-                </div>
-                <p className="font-medium lg:text-lg">{translate('featuresBanner.guarantee')}</p>
+              <div className="flex flex-row items-center space-x-3 pt-2 ">
+                <CheckCircle size={24} className="" />
+                <p className="whitespace-nowrap font-medium lg:text-lg">{translate('featuresBanner.guarantee')}</p>
               </div>
-
-              {/* <p className="text-sm font-medium text-gray-50">{translate('featuresBanner.lastCta')}</p> */}
+              <p className="text-sm font-medium text-gray-50">{translate('featuresBanner.lastCta')}</p>
             </div>
           </div>
-          <div className="hidden items-center lg:flex">
+          <div className="hidden w-full items-center lg:flex">
             <div className="flex flex-col">
               <div className="flex flex-col space-y-8">
                 {features.map((card) => (
-                  <div className="flex flex-row items-center space-x-4" key={card}>
-                    <TreePalm weight="fill" size={32} className="text-primary" />
-                    <p className="whitespace-nowrap text-xl font-semibold text-gray-80 dark:text-gray-20">{card}</p>
+                  <div className="flex flex-row space-x-4" key={card}>
+                    <TreePalm size={32} className="text-primary" weight="fill" />
+                    <p className="text-lg font-semibold ">{card}</p>
                   </div>
                 ))}
               </div>
