c32578cd041773fe17db4fd89e2d6b1540dccf72
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-08-19 11:47:06 +0200

    feat: update features icon and OFFER_END_DAY date

--- src/app/banners/FeaturesBanner.tsx
+++ src/app/banners/FeaturesBanner.tsx
@@ -1,4 +1,4 @@
-import { CheckCircle, TreePalm, X } from '@phosphor-icons/react';
+import { CheckCircle, LockKey, X } from '@phosphor-icons/react';
 
 import { useTranslationContext } from 'app/i18n/provider/TranslationProvider';
 
@@ -57,7 +57,7 @@ const FeaturesBanner = ({ showBanner, onClose }: { showBanner: boolean; onClose:
               <div className="flex flex-col space-y-8">
                 {features.map((card) => (
                   <div className="flex flex-row space-x-4" key={card}>
-                    <TreePalm size={32} className="text-primary" weight="fill" />
+                    <LockKey size={32} className="text-primary" weight="fill" />
                     <p className="text-lg font-semibold ">{card}</p>
                   </div>
                 ))}
