a05042ab42e0d98f305ad770afc1f2f5f352cda0
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-09-02 08:22:21 +0200

    feat: back to work banner

--- src/app/banners/FeaturesBanner.tsx
+++ src/app/banners/FeaturesBanner.tsx
@@ -1,14 +1,19 @@
-import { CheckCircle, LockKey, X } from '@phosphor-icons/react';
+import { CheckCircle, ShieldCheck, X } from '@phosphor-icons/react';
 
 import { useTranslationContext } from 'app/i18n/provider/TranslationProvider';
 
-const FeaturesBanner = ({ showBanner, onClose }: { showBanner: boolean; onClose: () => void }): JSX.Element => {
+interface FeaturesBannerProps {
+  showBanner: boolean;
+  onClose: () => void;
+}
+
+const FeaturesBanner = ({ showBanner, onClose }: FeaturesBannerProps): JSX.Element => {
   const { translate, translateList } = useTranslationContext();
 
   const features = translateList('featuresBanner.features');
 
   const handleOnClick = () => {
-    window.open('https://internxt.com/pricing', '_blank', 'noopener noreferrer');
+    window.open('https://internxt.com/lifetime', '_blank', 'noopener noreferrer');
     onClose();
   };
 
@@ -57,7 +62,7 @@ const FeaturesBanner = ({ showBanner, onClose }: { showBanner: boolean; onClose:
               <div className="flex flex-col space-y-8">
                 {features.map((card) => (
                   <div className="flex flex-row space-x-4" key={card}>
-                    <LockKey size={32} className="text-primary" weight="fill" />
+                    <ShieldCheck size={32} className="text-primary" weight="fill" />
                     <p className="text-lg font-semibold ">{card}</p>
                   </div>
                 ))}
