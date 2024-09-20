cec628ad6e732f02b7e8af05e047c949887bdb08
Internxt <internxt@Internxts-MacBook-Air.local>
2024-09-20 17:16:21 +0200

    revert changes to jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 /*eslint-disable*/
 module.exports = {
-  preset: 'ts-jest/presets/js-with-ts-esm',
-  transformIgnorePatterns: [
-      'node_modules/(?!(@dashlane/pqc-kem-kyber512-browser)/)' 
-  ],
+  preset: 'ts-jest',
   testEnvironment: 'node',
 };
