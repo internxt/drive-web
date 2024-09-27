31caea6f5c4fd18793daf986f7f5efc88c96246e
Internxt <internxt@Internxts-MacBook-Air.local>
2024-09-27 10:33:39 +0200

    exclude failing tests (playwright and cypress)

--- vitest.config.js
+++ vitest.config.js
@@ -3,18 +3,17 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   tsetupFiles: ['./tests/setup.js'],
   test: {
+    exclude: ['./tests-examples/demo-todo-app.spec.ts', './tests/example.spec.ts', 'test/e2e/*', './node_modules/*'],
     globals: true,
     environment: 'jsdom',
     browser: {
       provider: 'playwright',
       enabled: true,
       name: 'chromium',
+      headless: true,
     },
     reporters: ['default'],
   },
-  define: {
-    global: {},
-    'process.env': {},
-  },
+
   optimizeDeps: { exclude: ['fsevents'] },
 });
