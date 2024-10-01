d290615b395c92cf41e9b800cac55e94ba0e8c0a
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-10-01 12:05:13 +0200

    fix: import the service

--- src/app/newSettings/components/Invoices/InvoicesList.tsx
+++ src/app/newSettings/components/Invoices/InvoicesList.tsx
@@ -5,6 +5,7 @@ import dateService from '../../../core/services/date.service';
 import { bytesToString } from '../../../drive/services/size.service';
 import { useTranslationContext } from '../../../i18n/provider/TranslationProvider';
 import Spinner from '../../../shared/components/Spinner/Spinner';
+import currencyService from 'app/payment/services/currency.service';
 
 const InvoicesList = ({ invoices, state }: { invoices: Invoice[]; state: 'loading' | 'empty' | 'ready' }) => {
   const [hoverItemIndex, setHoverItemIndex] = useState<string | null>(null);
