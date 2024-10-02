cea58d790f5f9002525eb6c68824d3385526597b
Jonatan Pérez <jonatan@internxt.com>
2024-10-02 18:31:23 +0200

    Add submit functionality when pressing enter key on a field

--- src/app/newSettings/Sections/Account/Account/components/AccountDetailsModal.tsx
+++ src/app/newSettings/Sections/Account/Account/components/AccountDetailsModal.tsx
@@ -47,7 +47,8 @@ const AccountDetailsModal = ({
     return value.length > 0 && value.length < 20;
   };
 
-  const onSave = async () => {
+  const onSave = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     if (!validate(nameValue)) {
       setStatus({ tag: 'error', type: 'NAME_INVALID' });
     } else if (!validate(lastnameValue)) {
@@ -66,55 +67,57 @@ const AccountDetailsModal = ({
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <div className="flex flex-col space-y-5">
-        <h1 className="text-2xl font-medium text-gray-80">
-          {translate('views.account.tabs.account.accountDetails.editProfile.title')}
-        </h1>
-        <div className="flex space-x-3">
-          <Input
-            disabled={status.tag === 'loading'}
-            label={translate('views.account.tabs.account.accountDetails.name')}
-            value={nameValue}
-            onChange={setNameValue}
-            accent={nameIsInvalid ? 'error' : undefined}
-            message={nameIsInvalid ? 'This name is invalid' : undefined}
-            name="firstName"
-          />
-          <Input
-            disabled={status.tag === 'loading'}
-            label={translate('views.account.tabs.account.accountDetails.lastname')}
-            value={lastnameValue}
-            onChange={setLastnameValue}
-            accent={lastnameIsInvalid ? 'error' : undefined}
-            message={lastnameIsInvalid ? 'This lastname is invalid' : undefined}
-            name="lastName"
-          />
-        </div>
+      <form onSubmit={onSave}>
+        <div className="flex flex-col space-y-5">
+          <h1 className="text-2xl font-medium text-gray-80">
+            {translate('views.account.tabs.account.accountDetails.editProfile.title')}
+          </h1>
+          <div className="flex space-x-3">
+            <Input
+              disabled={status.tag === 'loading'}
+              label={translate('views.account.tabs.account.accountDetails.name')}
+              value={nameValue}
+              onChange={setNameValue}
+              accent={nameIsInvalid ? 'error' : undefined}
+              message={nameIsInvalid ? 'This name is invalid' : undefined}
+              name="firstName"
+            />
+            <Input
+              disabled={status.tag === 'loading'}
+              label={translate('views.account.tabs.account.accountDetails.lastname')}
+              value={lastnameValue}
+              onChange={setLastnameValue}
+              accent={lastnameIsInvalid ? 'error' : undefined}
+              message={lastnameIsInvalid ? 'This lastname is invalid' : undefined}
+              name="lastName"
+            />
+          </div>
 
-        <div className="flex items-end space-x-3">
-          <Input
-            disabled
-            className="flex-1"
-            label={translate('views.account.tabs.account.accountDetails.card.email')}
-            value={emailValue}
-            name="email"
-          />
-          <div className="flex h-11 items-center">
-            <Button disabled={status.tag === 'loading'} variant="secondary" onClick={openEditEmail}>
-              {translate('actions.change')}
+          <div className="flex items-end space-x-3">
+            <Input
+              disabled
+              className="flex-1"
+              label={translate('views.account.tabs.account.accountDetails.card.email')}
+              value={emailValue}
+              name="email"
+            />
+            <div className="flex h-11 items-center">
+              <Button disabled={status.tag === 'loading'} variant="secondary" onClick={openEditEmail}>
+                {translate('actions.change')}
+              </Button>
+            </div>
+          </div>
+
+          <div className="flex justify-end">
+            <Button disabled={status.tag === 'loading'} variant="secondary" onClick={onClose}>
+              {translate('actions.cancel')}
+            </Button>
+            <Button type="submit" loading={status.tag === 'loading'} className="ml-2">
+              {status.tag === 'loading' ? translate('actions.saving') : translate('actions.save')}
             </Button>
           </div>
         </div>
-
-        <div className="flex justify-end">
-          <Button disabled={status.tag === 'loading'} variant="secondary" onClick={onClose}>
-            {translate('actions.cancel')}
-          </Button>
-          <Button loading={status.tag === 'loading'} className="ml-2" onClick={onSave}>
-            {status.tag === 'loading' ? translate('actions.saving') : translate('actions.save')}
-          </Button>
-        </div>
-      </div>
+      </form>
     </Modal>
   );
 };
