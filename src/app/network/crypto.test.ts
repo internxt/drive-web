1e41b8c4bf47f0e48353485dfd6824ce654f81e6
Internxt <internxt@Internxts-MacBook-Air.local>
2024-11-14 12:53:15 +0100

    return acidently deleted function, add tests for generateHMAC and getEncryptedFile

--- src/app/network/crypto.test.ts
+++ src/app/network/crypto.test.ts
@@ -2,11 +2,13 @@
  * @jest-environment jdom
  */
 
-import { describe, expect, it } from 'vitest';
-import { encryptFilename } from './crypto';
+import { describe, expect, it, vi } from 'vitest';
+import { encryptFilename, generateHMAC, getEncryptedFile } from './crypto';
+import { getHmacSha512 } from '../crypto/services/utils';
 import { Buffer } from 'buffer';
+import crypto from 'crypto';
 
-describe('Test file encryption', () => {
+describe('Test crypto.ts functions', () => {
   globalThis.Buffer = Buffer;
   it('encryptFilename should generate a ciphertext', async () => {
     const mnemonic =
@@ -15,5 +17,48 @@ describe('Test file encryption', () => {
     const filename = 'test filename';
     const result = await encryptFilename(mnemonic, bucketId, filename);
     expect(result).toBeDefined();
+  });
+
+  it('generateHMAC should generate hmac', async () => {
+    const encryptionKey = crypto.randomBytes(16);
+    const shardMeta = {
+      challenges_as_str: [],
+      hash: '',
+      index: 0,
+      parity: false,
+      size: 0,
+      tree: [],
+    };
+    const result = await generateHMAC([shardMeta], encryptionKey);
+    const expectedResult = await getHmacSha512(encryptionKey, ['']);
+    expect(result).toBeDefined();
+    expect(result).toBe(expectedResult);
+  });
+
+  it('getEncryptedFile should generate encrypted text and hash', async () => {
+    function createMockFile(name: string, size = 0, type = ''): File {
+      return {
+        name,
+        size,
+        type,
+        slice: vi.fn(),
+        stream: vi.fn().mockReturnValueOnce(
+          new ReadableStream({
+            start(controller) {
+              // You can push chunks of data into the stream, e.g.:
+              controller.enqueue(new TextEncoder().encode('file contents'));
+              controller.close();
+            },
+          }),
+        ),
+      } as unknown as File;
+    }
+    const encryptionKey = crypto.randomBytes(32);
+    const iv = crypto.randomBytes(16);
+    const file = createMockFile(`file.txt`, 13, 'text/plain');
+    const cipher = crypto.createCipheriv('aes-256-ctr', encryptionKey, iv);
+    const [encryptedFile, hash] = await getEncryptedFile(file, cipher);
+    expect(encryptedFile).toBeDefined();
+    expect(hash).toBeDefined();
   });
 });
