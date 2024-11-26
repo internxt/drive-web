c8aff92bd5d338c999ff10ac9dd7182a218fd8d0
Ramon Candel <mameo@MBP-de-Ramon.home>
2024-11-26 08:43:57 +0100

    Added playwright example test

--- test/e2e/tests/example.spec.ts
+++ test/e2e/tests/example.spec.ts
@@ -1,33 +1,18 @@
-import { Browser, BrowserContext, chromium, expect, Page } from '@playwright/test';
-import { afterAll, beforeAll, describe, test } from 'vitest';
+import { expect, test } from '@playwright/test';
 
-describe('playwright meets vitest', () => {
-  let page: Page;
-  let browser: Browser;
-  let context: BrowserContext;
-  beforeAll(async () => {
-    browser = await chromium.launch();
-    const context = await browser.newContext();
-    page = await context.newPage();
-  });
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
 
-  afterAll(async () => {
-    await browser.close();
-  });
-  test('has title', async () => {
-    await page.goto('https://playwright.dev/');
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
 
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Playwright/);
-  });
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
 
-  test('get started link', async () => {
-    await page.goto('https://playwright.dev/');
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
 
-    // Click the get started link.
-    await page.getByRole('link', { name: 'Get started' }).click();
-
-    // Expects page to have a heading with the name of Installation.
-    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-  });
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
