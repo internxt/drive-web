025fb5de755de39458cbbd16604765fa505c46f9
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-11-28 11:13:11 +0100

    fix: vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -36,12 +36,8 @@ export default defineConfig({
       provider: 'istanbul',
       reporter: ['text', 'lcov'],
       reportsDirectory: './coverage',
-      include: ['src/**/*.{js,ts,jsx,tsx}'],
-      exclude: [
-        'src/app/drive/components/FileViewer/viewers/FileDocumentViewer/**',
-        'src/**/*.test.ts',
-        'test/unit/**/*.test.ts',
-      ],
+      include: ['src/**/*.{js,ts,jsx,tsx}', 'test/unit/**/*.{js,ts,jsx,tsx}'],
+      exclude: ['src/app/drive/components/FileViewer/viewers/FileDocumentViewer/**'],
     },
   },
   optimizeDeps: {
