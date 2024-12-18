2190fe449a1f90b90c39fc127db1ae4c6796f65d
Xavier Abad <xabadgo01@gmail.com>
2024-12-18 10:09:27 +0100

    fix: revert useless changes

--- src/app/newSettings/Sections/Workspace/Members/MembersSection.tsx
+++ src/app/newSettings/Sections/Workspace/Members/MembersSection.tsx
@@ -7,7 +7,7 @@ import { bytesToString } from '../../../../drive/services/size.service';
 import { useTranslationContext } from '../../../../i18n/provider/TranslationProvider';
 import UsageBar from '../../../../newSettings/components/Usage/UsageBar';
 import { getMemberRole, searchMembers } from '../../../../newSettings/utils/membersUtils';
-import { Button, TableCell, TableRow } from '@internxt/ui';
+import { Button } from '@internxt/internxtui';
 import Card from '../../../../shared/components/Card';
 import Input from '../../../../shared/components/Input';
 import { RootState } from '../../../../store';
@@ -16,45 +16,22 @@ import workspacesSelectors from '../../../../store/slices/workspaces/workspaces.
 import UserCard from './components/UserCard';
 import InviteDialogContainer from './containers/InviteDialogContainer';
 import MemberDetailsContainer from './containers/MemberDetailsContainer';
-import { ScrollableTable } from 'app/shared/tables/ScrollableTable';
 
 const PENDING_INVITATIONS_LIMIT = 25;
 const PENDING_INVITATIONS_OFFSET = 0;
-
-interface HeaderListProps {
-  label: string;
-  separator: boolean;
-  width: string;
-}
 
 const MembersSection = ({ onClosePreferences }: { onClosePreferences: () => void }) => {
   const { translate } = useTranslationContext();
   const selectedWorkspace = useAppSelector((state: RootState) => state.workspaces.selectedWorkspace);
   const isCurrentUserWorkspaceOwner = useAppSelector(workspacesSelectors.isWorkspaceOwner);
   const [searchedMemberName, setSearchedMemberName] = useState('');
+  const [hoverItemIndex, setHoverItemIndex] = useState<number | null>(null);
   const [isInviteDialogOpen, setIsInviteDialogOpen] = useState(false);
   const [selectedMember, setSelectedMember] = useState<WorkspaceUser | null>(null);
   const [members, setMembers] = useState<WorkspaceUser[] | null>(null);
   const [displayedMembers, setDisplayedMembers] = useState(members);
   const [isCurrentMemberOwner, setIsCurrentMemberOwner] = useState<boolean>(false);
   const [guestUsers, setGuestUsers] = useState<number>(0);
-  const headerList: HeaderListProps[] = [
-    {
-      label: translate('preferences.workspace.members.list.user'),
-      separator: true,
-      width: '40%',
-    },
-    {
-      label: translate('preferences.workspace.members.list.usage'),
-      separator: true,
-      width: '40%',
-    },
-    {
-      label: translate('preferences.workspace.members.list.storage'),
-      separator: false,
-      width: '20%',
-    },
-  ];
 
   useEffect(() => {
     refreshWorkspaceMembers();
@@ -103,65 +80,6 @@ const MembersSection = ({ onClosePreferences }: { onClosePreferences: () => void
     selectedWorkspaceId && (await getWorkspacesMembers(selectedWorkspaceId));
     await getWorkspacePendingInvitations(selectedWorkspace?.workspaceUser.workspaceId);
   };
-
-  const renderHeader = (headerList: HeaderListProps[]) => (
-    <TableRow className="rounded-t-lg bg-gray-5">
-      {headerList.map((header) => (
-        <TableCell
-          key={header.label}
-          isHeader
-          style={{
-            width: header.width,
-          }}
-          className="text-left"
-        >
-          <div className="flex h-full w-full justify-between py-2.5 pl-5">
-            <p className="text-base font-normal">{header.label}</p>
-            {header.separator && <div className="border-[0.5px] border-gray-10" />}
-          </div>
-        </TableCell>
-      ))}
-    </TableRow>
-  );
-
-  const renderBody = (displayedMembers: WorkspaceUser[] | null, setSelectedMember: (member: WorkspaceUser) => void) => (
-    <>
-      {displayedMembers?.map((member) => {
-        const role = getMemberRole(member);
-        const { id, name, lastname, email } = member.member;
-        const { spaceLimit, usedSpace, backupsUsage, driveUsage } = member;
-        const usedSpaceBytes = bytesToString(Number(usedSpace));
-
-        return (
-          <TableRow
-            key={id}
-            className="cursor-pointer border-b border-gray-10 bg-surface hover:bg-gray-5 dark:bg-gray-1 dark:hover:bg-gray-5"
-            onClick={() => setSelectedMember(member)}
-          >
-            {/* LEFT COLUMN */}
-            <TableCell className="max-w-[256px] overflow-hidden py-2 pl-5">
-              <div className="flex w-full items-center justify-between font-medium text-gray-100">
-                <UserCard name={name} lastName={lastname} role={role} email={email} avatarSrc="" />
-              </div>
-            </TableCell>
-            {/* CENTER COLUMN */}
-            <TableCell className="py-2 pl-5">
-              <div className="flex w-full items-center justify-between text-gray-60">
-                <UsageBar backupsUsage={backupsUsage} driveUsage={driveUsage} spaceLimit={spaceLimit} height="h-4" />
-                <span className="ml-4">{usedSpaceBytes ?? '0Bytes'}</span>
-              </div>
-            </TableCell>
-            {/* RIGHT COLUMN */}
-            <TableCell className="py-2 pl-5">
-              <div className="flex w-full items-center justify-between text-gray-60">
-                <span className="text-base font-medium leading-5">{bytesToString(Number(spaceLimit))}</span>
-              </div>
-            </TableCell>
-          </TableRow>
-        );
-      })}
-    </>
-  );
 
   return (
     <Section
@@ -219,17 +137,102 @@ const MembersSection = ({ onClosePreferences }: { onClosePreferences: () => void
                 </Button>
               )}
             </div>
-
-            {/* Table */}
-            <ScrollableTable
-              renderHeader={() => renderHeader(headerList)}
-              renderBody={() => renderBody(displayedMembers, setSelectedMember)}
-              containerClassName="rounded-xl border border-gray-10"
-              tableHeaderClassName="sticky top-0 z-10 border-b border-gray-10 bg-gray-1 text-gray-100"
-              tableClassName="rounded-xl overflow-hidden w-full"
-              tableBodyClassName="bg-none"
-              numOfColumnsForSkeleton={3}
-            />
+            <div className="flex">
+              {/* LEFT COLUMN */}
+              <div className="flex grow flex-col">
+                <div className="flex grow flex-col">
+                  <span
+                    className={
+                      'flex h-12 flex-row items-center justify-between rounded-tl-xl border-b border-l border-t border-gray-10 bg-gray-5 px-5 py-2 '
+                    }
+                  >
+                    {translate('preferences.workspace.members.list.user')}
+                  </span>
+                </div>
+                {displayedMembers?.map((member, i) => {
+                  const role = getMemberRole(member);
+                  const { id, name, lastname, email } = member.member;
+                  return (
+                    <button
+                      key={id}
+                      className={`flex h-14 flex-row justify-between border-l border-gray-10 px-5 py-2 text-base  font-medium text-gray-100 dark:bg-gray-1 ${
+                        members && i === members.length - 1 ? 'rounded-bl-xl border-b' : ' border-b'
+                      }
+              ${hoverItemIndex === id ? 'bg-gray-5 dark:bg-gray-5' : 'bg-surface'}`}
+                      onMouseEnter={() => setHoverItemIndex(id)}
+                      onMouseLeave={() => setHoverItemIndex(null)}
+                      onClick={() => setSelectedMember(member)}
+                    >
+                      <UserCard name={name} lastName={lastname} role={role} email={email} avatarSrc={''} />
+                    </button>
+                  );
+                })}
+              </div>
+              {/* CENTER COLUMN */}
+              <div className="flex w-full max-w-[256px] grow flex-col">
+                <div className="flex h-12 items-center border-b border-t border-gray-10 bg-gray-5 py-2">
+                  <span
+                    className={
+                      'flex w-full items-center justify-between border-l border-r border-gray-10 bg-gray-5 pl-5 pr-1 '
+                    }
+                  >
+                    {translate('preferences.workspace.members.list.usage')}
+                  </span>
+                </div>
+                {displayedMembers?.map((member, i) => {
+                  const { spaceLimit, usedSpace, backupsUsage, driveUsage } = member;
+                  const { id } = member.member;
+                  const usedSpaceBytes = bytesToString(Number(usedSpace));
+                  return (
+                    <button
+                      key={id}
+                      className={`flex h-14 items-center justify-between border-gray-10 px-5 py-2 text-base font-normal text-gray-60 dark:bg-gray-1 ${
+                        members && i === members.length - 1 ? 'border-b' : ' border-b'
+                      } ${hoverItemIndex === id ? 'bg-gray-5 dark:bg-gray-5' : 'bg-surface'}`}
+                      onMouseEnter={() => setHoverItemIndex(id)}
+                      onMouseLeave={() => setHoverItemIndex(null)}
+                      onClick={() => setSelectedMember(member)}
+                    >
+                      <div className="flex w-full items-center justify-between">
+                        <UsageBar
+                          backupsUsage={backupsUsage}
+                          driveUsage={driveUsage}
+                          spaceLimit={spaceLimit}
+                          height={'h-4'}
+                        />
+                        <span className="ml-4">{usedSpaceBytes ?? '0Bytes'}</span>
+                      </div>
+                    </button>
+                  );
+                })}
+              </div>
+              {/* RIGHT COLUMN */}
+              <div className="flex w-28 flex-col rounded-tr-xl">
+                <div>
+                  <div className="flex h-12 items-center truncate rounded-tr-xl border-b border-t border-gray-10 bg-gray-5 p-5">
+                    <span className={'truncate rounded-tr-xl'}>
+                      {translate('preferences.workspace.members.list.storage')}
+                    </span>
+                  </div>
+                </div>
+                {displayedMembers?.map((member, i) => {
+                  const { spaceLimit } = member;
+                  const { id } = member.member;
+                  return (
+                    <button
+                      key={id}
+                      className={`flex h-14 flex-row items-center justify-between border-r border-gray-10 py-2 pl-5 text-base font-normal text-gray-60 dark:bg-gray-1 ${
+                        members && i === members.length - 1 ? 'rounded-br-xl border-b' : 'border-b'
+                      } ${hoverItemIndex === id ? 'bg-gray-5 dark:bg-gray-5' : ''}`}
+                      onMouseEnter={() => setHoverItemIndex(id)}
+                      onMouseLeave={() => setHoverItemIndex(null)}
+                    >
+                      <span className=" text-base font-medium leading-5">{bytesToString(Number(spaceLimit))}</span>
+                    </button>
+                  );
+                })}
+              </div>
+            </div>
           </div>
           <InviteDialogContainer isOpen={isInviteDialogOpen} onClose={() => setIsInviteDialogOpen(false)} />
         </>
