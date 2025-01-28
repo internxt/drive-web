2bb560d10cbd04484c048ec34306c6335e5d85a3
jaaaaavier <javipiagui13@gmail.com>
2025-01-28 09:33:01 +0100

    feat: superbowl campaign

--- src/app/banners/BannerWrapper.tsx
+++ src/app/banners/BannerWrapper.tsx
@@ -8,10 +8,10 @@ import FeaturesBanner from './FeaturesBanner';
 import { BannerManager } from './BannerManager';
 import { useEffect, useState } from 'react';
 
-const OFFER_END_DAY = new Date('2025-01-07');
+const OFFER_END_DAY = new Date('2025-02-09');
 
 const BannerWrapper = (): JSX.Element => {
-  const [showBanner, setShowBanner] = useState<boolean>(true);
+  const [showBanner, setShowBanner] = useState<boolean>(false);
   const user = useSelector((state: RootState) => state.user.user) as UserSettings;
   const plan = useSelector<RootState, PlanState>((state) => state.plan);
 
