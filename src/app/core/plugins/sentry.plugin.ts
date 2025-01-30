fba3434ffb0d94043413b904ba5eeffa52dbfd10
toma finogina <tamara.finogina@skolkovotech.ru>
2025-01-30 13:14:08 +0100

    fix some of the yarn audit vulnerabilities

--- src/app/core/plugins/sentry.plugin.ts
+++ src/app/core/plugins/sentry.plugin.ts
@@ -1,5 +1,4 @@
 import * as Sentry from '@sentry/react';
-import { BrowserTracing } from '@sentry/tracing';
 import packageJson from '../../../../package.json';
 import { AppPlugin } from '../../core/types';
 
@@ -7,7 +6,7 @@ const sentryPlugin: AppPlugin = {
   install(): void {
     Sentry.init({
       dsn: process.env.REACT_APP_SENTRY_DSN,
-      integrations: [new BrowserTracing()],
+      integrations: [new Sentry.BrowserTracing()],
       tracesSampleRate: 0.3,
       debug: process.env.NODE_ENV !== 'production' && process.env.REACT_APP_DEBUG === 'true',
       environment: process.env.NODE_ENV,
