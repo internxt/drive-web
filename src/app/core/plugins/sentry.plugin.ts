36102fcce87c36c061f3ddbcdc8f34c33ce61760
toma finogina <tamara.finogina@skolkovotech.ru>
2025-01-31 12:39:07 +0100

    remove all changes to dependencies

--- src/app/core/plugins/sentry.plugin.ts
+++ src/app/core/plugins/sentry.plugin.ts
@@ -1,4 +1,5 @@
 import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import packageJson from '../../../../package.json';
 import { AppPlugin } from '../../core/types';
 
@@ -6,7 +7,7 @@ const sentryPlugin: AppPlugin = {
   install(): void {
     Sentry.init({
       dsn: process.env.REACT_APP_SENTRY_DSN,
-      integrations: [Sentry.browserTracingIntegration()],
+      integrations: [new BrowserTracing()],
       tracesSampleRate: 0.3,
       debug: process.env.NODE_ENV !== 'production' && process.env.REACT_APP_DEBUG === 'true',
       environment: process.env.NODE_ENV,
