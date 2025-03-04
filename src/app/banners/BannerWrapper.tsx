3d25556f93bb308badcbd164573c7c9ebf093e9b
jaaaaavier <javipiagui13@gmail.com>
2025-03-04 15:00:42 +0100

    feat: new Subscription and lifetimes banner

--- src/app/banners/BannerWrapper.tsx
+++ src/app/banners/BannerWrapper.tsx
@@ -5,25 +5,57 @@ import { useSelector } from 'react-redux';
 import { UserSettings } from '@internxt/sdk/dist/shared/types/userSettings';
 
 import FeaturesBanner from './FeaturesBanner';
+import SubscriptionBanner from './SubscriptionBanner';
+import LifetimeBanners from './LifetimeBanners';
 import { BannerManager } from './BannerManager';
 import { useEffect, useState } from 'react';
 
 const OFFER_END_DAY = new Date('2025-03-17');
 
 const BannerWrapper = (): JSX.Element => {
-  const [showBanner, setShowBanner] = useState<boolean>(false);
+  const [showBanners, setShowBanners] = useState({
+    features: false,
+    subscriptions: false,
+    lifetimes: false,
+  });
+
   const user = useSelector((state: RootState) => state.user.user) as UserSettings;
   const plan = useSelector<RootState, PlanState>((state) => state.plan);
-
   const bannerManager = new BannerManager(user, plan, OFFER_END_DAY);
 
   useEffect(() => {
-    bannerManager.handleBannerDisplay(setShowBanner);
+    const banners = {
+      features: 'free',
+      subscriptions: 'subscription',
+      lifetimes: 'lifetime',
+    };
+
+    Object.entries(banners).forEach(([key, type]) => {
+      bannerManager.handleBannerDisplayByType(type as 'free' | 'subscription' | 'lifetime', (show) => {
+        setShowBanners((prev) => ({ ...prev, [key]: show }));
+      });
+    });
   }, [user, plan]);
 
-  const onCloseBanner = () => bannerManager.onCloseBanner(setShowBanner);
+  const onCloseBanner = (key: keyof typeof showBanners) => {
+    bannerManager.onCloseBanner((show) => {
+      setShowBanners((prev) => ({ ...prev, [key]: show }));
+    });
+  };
 
-  return <>{showBanner && <FeaturesBanner showBanner={showBanner} onClose={onCloseBanner} />}</>;
+  return (
+    <>
+      {showBanners.features && (
+        <FeaturesBanner showBanner={showBanners.features} onClose={() => onCloseBanner('features')} />
+      )}
+      {showBanners.subscriptions && (
+        <SubscriptionBanner showBanner={showBanners.subscriptions} onClose={() => onCloseBanner('subscriptions')} />
+      )}
+      {showBanners.lifetimes && (
+        <LifetimeBanners showBanner={showBanners.lifetimes} onClose={() => onCloseBanner('lifetimes')} />
+      )}
+    </>
+  );
 };
 
 export default BannerWrapper;
