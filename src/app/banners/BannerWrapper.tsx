f26789c83b455b8e9e3789453fbc81a95610dfe1
Ramon Candel Segura <57987155+CandelR@users.noreply.github.com>
2025-03-05 11:10:28 +0100

    Revert "[MKT_502]: feat/SpringCampaign"

--- src/app/banners/BannerWrapper.tsx
+++ src/app/banners/BannerWrapper.tsx
@@ -5,51 +5,25 @@ import { useSelector } from 'react-redux';
 import { UserSettings } from '@internxt/sdk/dist/shared/types/userSettings';
 
 import FeaturesBanner from './FeaturesBanner';
-import SubscriptionBanner from './SubscriptionBanner';
-import LifetimeBanners from './LifetimeBanners';
 import { BannerManager } from './BannerManager';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const OFFER_END_DAY = new Date('2025-03-17');
+const OFFER_END_DAY = new Date('2025-02-25');
 
 const BannerWrapper = (): JSX.Element => {
-  const [showBanners, setShowBanners] = useState({
-    features: false,
-    subscriptions: false,
-    lifetimes: true,
-  });
-
+  const [showBanner, setShowBanner] = useState<boolean>(false);
   const user = useSelector((state: RootState) => state.user.user) as UserSettings;
   const plan = useSelector<RootState, PlanState>((state) => state.plan);
-  const bannerManager = useMemo(() => new BannerManager(user, plan, OFFER_END_DAY), [user, plan]);
+
+  const bannerManager = new BannerManager(user, plan, OFFER_END_DAY);
 
   useEffect(() => {
-    const banners = {
-      features: 'free',
-      subscriptions: 'subscription',
-      lifetimes: 'lifetime',
-    };
-
-    Object.entries(banners).forEach(([key, type]) => {
-      bannerManager.handleBannerDisplayByType(type as 'free' | 'subscription' | 'lifetime', (show) => {
-        setShowBanners((prev) => ({ ...prev, [key]: show }));
-      });
-    });
+    bannerManager.handleBannerDisplay(setShowBanner);
   }, [user, plan]);
 
-  const onCloseBanner = (key: keyof typeof showBanners) => {
-    bannerManager.onCloseBanner((show) => {
-      setShowBanners((prev) => ({ ...prev, [key]: show }));
-    });
-  };
+  const onCloseBanner = () => bannerManager.onCloseBanner(setShowBanner);
 
-  return (
-    <>
-      <FeaturesBanner showBanner={showBanners.features} onClose={() => onCloseBanner('features')} />
-      <SubscriptionBanner showBanner={showBanners.subscriptions} onClose={() => onCloseBanner('subscriptions')} />
-      <LifetimeBanners showBanner={showBanners.lifetimes} onClose={() => onCloseBanner('lifetimes')} />
-    </>
-  );
+  return <>{showBanner && <FeaturesBanner showBanner={showBanner} onClose={onCloseBanner} />}</>;
 };
 
 export default BannerWrapper;
