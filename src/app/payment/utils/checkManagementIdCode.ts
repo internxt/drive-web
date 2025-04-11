ce7be2780af8c10a888129415453e04a0708816b
Xavier Abad <xabadgo01@gmail.com>
2025-04-11 10:09:40 +0200

    fix(theme): hide id management theme when necessary

--- src/app/payment/utils/checkManagementIdCode.ts
+++ src/app/payment/utils/checkManagementIdCode.ts
@@ -1,29 +1,28 @@
 import localStorageService from 'app/core/services/local-storage.service';
 import { PlanState } from 'app/store/slices/plan';
 import paymentService from '../services/payment.service';
-import errorService from 'app/core/services/error.service';
 
 export const MANAGEMENTID_THEME_AVAILABLE_LOCAL_STORAGE_KEY = 'managementid_theme_enabled';
+export const ID_MANAGEMENT_THEME_AVAILABLE_LOCAL_STORAGE_KEY = 'managementid_theme_enabled';
 
 export const isManagementIdThemeAvailable = async (plan: PlanState, onSuccess?: () => void): Promise<boolean> => {
-  const managementIdInLocalStorage = localStorageService.get(MANAGEMENTID_THEME_AVAILABLE_LOCAL_STORAGE_KEY);
+  const managementIdInLocalStorage = localStorageService.get(ID_MANAGEMENT_THEME_AVAILABLE_LOCAL_STORAGE_KEY);
 
   if (managementIdInLocalStorage === 'true') return true;
   try {
     const coupons = ['IDENTITY82', 'IDENTITY82AFF'];
 
     const couponUsedResults = await Promise.all(coupons.map((code) => paymentService.isCouponUsedByUser(code)));
-    const couponUsed = couponUsedResults.some((result) => result);
+    const couponUsed = couponUsedResults.some((result) => result.couponUsed);
 
     if (couponUsed) {
       onSuccess?.();
-      localStorageService.set(MANAGEMENTID_THEME_AVAILABLE_LOCAL_STORAGE_KEY, 'true');
+      localStorageService.set(ID_MANAGEMENT_THEME_AVAILABLE_LOCAL_STORAGE_KEY, 'true');
       return true;
     }
 
     return false;
   } catch (err) {
-    errorService.reportError(err);
     return false;
   }
 };
