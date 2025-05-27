b961c3237e0abe2aeca153416474936606af4c54
Xavier Abad <xabadgo01@gmail.com>
2025-05-27 09:44:51 +0200

    fix(core): remove useless config

--- vitest.workspace.js
+++ vitest.workspace.js
@@ -1,28 +1,4 @@
 import { defineWorkspace } from 'vitest/config';
-import replace from '@rollup/plugin-replace';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import svgr from '@svgr/rollup';
-
-const sharedPlugins = [
-  react(),
-  replace({
-    preventAssignment: true,
-    'process.browser': true,
-  }),
-  svgr({
-    svgrOptions: { native: true },
-    include: '**/*.svg',
-  }),
-];
-
-const sharedAliases = {
-  app: path.resolve(__dirname, './src/app'),
-  assets: path.resolve(__dirname, './src/assets'),
-  crypto: 'crypto-browserify',
-  stream: 'stream-browserify',
-  path: 'path-browserify',
-};
 
 export default defineWorkspace([
   {
