6d216466faa27c8440539212b933da1b8225267c
Xavier Abad <xabadgo01@gmail.com>
2025-05-27 11:22:46 +0200

    tests(config): fix coverage for tests

--- vitest.config.js
+++ vitest.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vitest/config';
 import svgr from '@svgr/rollup';
-
 export default defineConfig({
   plugins: [
     react(),
@@ -18,11 +17,27 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
+      // eslint-disable-next-line no-undef
       app: path.resolve(__dirname, './src/app'),
       assets: path.resolve(__dirname, './src/assets'),
       crypto: 'crypto-browserify',
       stream: 'stream-browserify',
       path: 'path-browserify',
+    },
+  },
+  test: {
+    enabled: true,
+    workspace: './vitest.workspace.js',
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'lcov'],
+      reportsDirectory: './coverage',
+      include: ['src/**/*.{js,ts,jsx,tsx}'],
+      exclude: [
+        'src/app/drive/components/FileViewer/viewers/FileDocumentViewer/**',
+        'src/**/*.test.{js,ts,jsx,tsx}',
+        'test/unit/**/*.{js,ts,jsx,tsx}',
+      ],
     },
   },
   optimizeDeps: {
