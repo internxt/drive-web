6f81216fb41c582840842a81756a836476e30cb7
jaaaaavier <javipiagui13@gmail.com>
2025-06-05 15:42:12 +0200

    Update googleSheet.service.ts

--- src/app/analytics/googleSheet.service.ts
+++ src/app/analytics/googleSheet.service.ts
@@ -1,15 +1,20 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet';
-import creds from './googleSheetCredentials.json';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const SPREADSHEET_ID = '16KKMYDbLtgcvSyRu5SFf6oNSfBulaUAj5HhzuFKFEks';
+const creds = {
+  client_email: process.env.GS_CLIENT_EMAIL,
+  private_key: process.env.GS_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+};
 
 function formatDateToCustomTimezoneString(date: Date, offsetHours: number): string {
   const adjusted = new Date(date.getTime() + offsetHours * 60 * 60 * 1000);
 
-  const year = adjusted.getUTCFullYear();
-  const month = String(adjusted.getUTCMonth() + 1).padStart(2, '0');
-  const day = String(adjusted.getUTCDate()).padStart(2, '0');
-  const hours = String(adjusted.getUTCHours()).padStart(2, '0');
+  const year = adjusted.getFullYear();
+const month = String(adjusted.getMonth() + 1).padStart(2, '0');
+const day = String(adjusted.getDate()).padStart(2, '0');
+const hours = String(adjusted.getHours()).padStart(2, '0');
   const minutes = String(adjusted.getUTCMinutes()).padStart(2, '0');
   const seconds = String(adjusted.getUTCSeconds()).padStart(2, '0');
 
