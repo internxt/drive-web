7d05d5dadda085dee303d2b96e1fab63a02c52ae
Xavier Abad <xabadgo01@gmail.com>
2025-06-06 13:58:42 +0200

    tests: using spyOn instead of using real function

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -19,6 +19,9 @@ export default defineWorkspace([
         name: 'chromium',
         headless: true,
       },
+      sequence: {
+        hooks: 'list',
+      },
     },
     extends: './vitest.shared.ts',
   },
