f1a945857cf99a9f349325db8170c0609b8408dc
tamarafinogina <tamara@internxt.com>
2025-06-24 16:59:35 +0200

    fix limit and coupones tests

--- src/app/drive/services/limit.service.test.ts
+++ src/app/drive/services/limit.service.test.ts
@@ -1,26 +1,36 @@
-import { describe, it, expect, vi, Mock } from 'vitest';
+import { describe, it, expect, vi, beforeEach } from 'vitest';
 import { SdkFactory } from '../../core/factory/sdk';
 import limitService from './limit.service';
+
+vi.mock('app/core/components/Sidenav/Sidenav', () => ({
+  HUNDRED_TB: 109951162777600,
+}));
 
 vi.mock('../../core/factory/sdk', () => ({
   SdkFactory: {
     getNewApiInstance: vi.fn(),
   },
 }));
-
+const spaceLimitMock = vi.fn();
 describe('limitService', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+    const storageClientMock = {
+      spaceLimitV2: spaceLimitMock,
+    };
+
+    vi.spyOn(SdkFactory, 'getNewApiInstance').mockReturnValue({
+      createNewStorageClient: () => storageClientMock,
+    } as any);
+  });
   describe('fetchLimit', () => {
     it('should fetch the space limit from the storage client', async () => {
       const expectedLimit = 50000;
-      const mockResponse = vi.fn().mockResolvedValue({ maxSpaceBytes: expectedLimit });
-      const mockStorageClient = { spaceLimitV2: mockResponse };
-      (SdkFactory.getNewApiInstance as Mock).mockReturnValue({
-        createNewStorageClient: () => mockStorageClient,
-      });
+      spaceLimitMock.mockResolvedValue({ maxSpaceBytes: expectedLimit });
 
       const result = await limitService.fetchLimit();
 
-      expect(mockStorageClient.spaceLimitV2).toHaveBeenCalled();
+      expect(spaceLimitMock).toHaveBeenCalled();
       expect(result).toEqual(expectedLimit);
     });
   });
