fbd929be81f5af2298b13f5af6841ad66b4a3ad9
Ramon Candel Segura <ramoncandelsegura@gmail.com>
2025-07-02 11:41:59 +0200

    Refactor upload folder management and file preparation to include option for not uploading hidden files

--- src/app/store/slices/storage/fileUtils/prepareFilesToUpload.ts
+++ src/app/store/slices/storage/fileUtils/prepareFilesToUpload.ts
@@ -1,9 +1,14 @@
 import { DriveFileData } from '@internxt/sdk/dist/drive/storage/types';
+import { FileToUpload } from '../../../../drive/services/file.service/types';
 import { checkDuplicatedFiles } from './checkDuplicatedFiles';
 import { processDuplicateFiles } from './processDuplicateFiles';
-import { FileToUpload } from '../../../../drive/services/file.service/types';
 
 const BATCH_SIZE = 200;
+
+const isHiddenFile = (fileName: string): boolean => {
+  if (fileName.startsWith('.')) console.log('isHiddenFile', fileName);
+  return fileName.startsWith('.');
+};
 
 export const prepareFilesToUpload = async ({
   files,
@@ -11,13 +16,17 @@ export const prepareFilesToUpload = async ({
   disableDuplicatedNamesCheck = false,
   fileType,
   disableExistenceCheck = false,
+  notUploadHiddenFiles = false,
 }: {
   files: File[];
   parentFolderId: string;
   disableDuplicatedNamesCheck?: boolean;
   fileType?: string;
   disableExistenceCheck?: boolean;
+  notUploadHiddenFiles?: boolean;
 }): Promise<{ filesToUpload: FileToUpload[]; zeroLengthFilesNumber: number }> => {
+  const filteredFiles = notUploadHiddenFiles ? files.filter((file) => !isHiddenFile(file.name)) : files;
+
   let filesToUpload: FileToUpload[] = [];
   let zeroLengthFilesNumber = 0;
 
@@ -34,7 +43,6 @@ export const prepareFilesToUpload = async ({
       disableDuplicatedNamesCheck: disableDuplicatedNamesCheckOverride,
       duplicatedFiles,
     });
-
     filesToUpload = newFilesToUpload;
     zeroLengthFilesNumber += zeroLengthFiles;
   };
@@ -53,8 +61,8 @@ export const prepareFilesToUpload = async ({
     }
   };
 
-  for (let i = 0; i < files.length; i += BATCH_SIZE) {
-    const batch = files.slice(i, i + BATCH_SIZE);
+  for (let i = 0; i < filteredFiles.length; i += BATCH_SIZE) {
+    const batch = filteredFiles.slice(i, i + BATCH_SIZE);
     await processFilesBatch(batch);
   }
 
