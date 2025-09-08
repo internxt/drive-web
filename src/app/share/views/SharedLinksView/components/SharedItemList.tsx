fe0a9169d5d7e1d8df763df0f1d552f86fd781d5
Xavier Abad <xabadgo01@gmail.com>
2025-09-08 09:47:35 +0200

    fix: remove useless components

--- src/app/share/views/SharedLinksView/components/SharedItemList.tsx
+++ src/app/share/views/SharedLinksView/components/SharedItemList.tsx
@@ -1,13 +1,8 @@
-import usersIcon from 'assets/icons/users.svg';
 import { t } from 'i18next';
 import { useCallback } from 'react';
-import dateService from '../../../../core/services/date.service';
 import { OrderDirection } from '../../../../core/types';
-import iconService from '../../../../drive/services/icon.service';
-import sizeService from '../../../../drive/services/size.service';
-import { DriveFileData } from '../../../../drive/types';
 import { AdvancedSharedItem } from '../../../types';
-import { Avatar, List, MenuItemType } from '@internxt/ui';
+import { List, MenuItemType } from '@internxt/ui';
 import { SharedListItem } from './SharedListItem';
 
 const skinSkeleton = [
@@ -142,112 +137,3 @@ export const SharedItemList = ({
     />
   );
 };
-
-const getSharedListItemComposition = ({
-  onItemDoubleClicked,
-  onNameClicked,
-  user,
-  getOwnerAvatarSrc,
-  checkIfIsItemSelected,
-}) => [
-  (shareItem: AdvancedSharedItem) => (
-    <ShareItemNameField shareItem={shareItem} onItemDoubleClicked={onItemDoubleClicked} onNameClicked={onNameClicked} />
-  ),
-  (shareItem: AdvancedSharedItem) => (
-    <ShareItemOwnerField
-      shareItem={shareItem}
-      user={user}
-      ownerAvatarSrc={getOwnerAvatarSrc(shareItem)}
-      isItemSelected={checkIfIsItemSelected(shareItem)}
-    />
-  ),
-  (shareItem: AdvancedSharedItem) => <ShareItemSizeField shareItem={shareItem} />,
-  (shareItem: AdvancedSharedItem) => (
-    <ShareItemCreatedField shareItem={shareItem} isItemSelected={checkIfIsItemSelected(shareItem)} />
-  ),
-];
-
-const ShareItemNameField = ({ shareItem, onItemDoubleClicked, onNameClicked }) => {
-  const Icon = iconService.getItemIcon(shareItem.isFolder, (shareItem as unknown as DriveFileData)?.type);
-
-  return (
-    <div className={'flex h-full w-full flex-row items-center space-x-4 file-list-item group overflow-hidden'}>
-      <div className="relative flex h-10 w-10 shrink items-center justify-center">
-        <Icon className="flex h-full justify-center drop-shadow-soft" />
-        {shareItem.dateShared && (
-          <div className="absolute -bottom-0.5 -right-1.5 flex h-5 w-5 items-center justify-center rounded-full bg-primary text-white ring-2 ring-surface">
-            <img src={usersIcon} width={13} alt="shared users" />
-          </div>
-        )}
-      </div>
-      <button
-        className="w-full max-w-full truncate pr-16 text-left"
-        onDoubleClick={() => onItemDoubleClicked(shareItem)}
-      >
-        <button
-          onClick={(e) => {
-            e.stopPropagation();
-            onNameClicked(shareItem);
-          }}
-          className="w-fit max-w-full flex-1 cursor-pointer flex-row truncate whitespace-nowrap text-left"
-          title={shareItem.plainName}
-        >
-          {shareItem.plainName}
-          {!shareItem.isFolder && shareItem.type && '.' + shareItem.type}
-        </button>
-      </button>
-    </div>
-  );
-};
-
-const ShareItemOwnerField = ({
-  shareItem,
-  user,
-  ownerAvatarSrc,
-  isItemSelected,
-}: {
-  shareItem: AdvancedSharedItem;
-  user: AdvancedSharedItem['user'] | undefined;
-  ownerAvatarSrc: string | null;
-  isItemSelected: boolean;
-}) => (
-  <div className="flex flex-row items-center justify-center">
-    <div className="mr-2">
-      <Avatar
-        diameter={28}
-        fullName={
-          shareItem.user?.name
-            ? `${shareItem.user?.name} ${shareItem.user?.lastname}`
-            : `${user?.name} ${user?.lastname}`
-        }
-        src={ownerAvatarSrc}
-      />
-    </div>
-    <span className={`${isItemSelected ? 'text-gray-100' : 'text-gray-60'}`}>
-      {shareItem.user ? (
-        <span>{`${shareItem.user?.name} ${shareItem.user?.lastname}`}</span>
-      ) : (
-        <span>{`${user?.name} ${user?.lastname}`}</span>
-      )}{' '}
-    </span>
-  </div>
-);
-
-const ShareItemSizeField = ({ shareItem }: { shareItem: AdvancedSharedItem }) =>
-  shareItem.isFolder ? (
-    <span className="opacity-25">—</span>
-  ) : (
-    <span>{`${sizeService.bytesToString(shareItem?.size ? shareItem.size : 0, false)}`}</span>
-  );
-
-const ShareItemCreatedField = ({
-  shareItem,
-  isItemSelected,
-}: {
-  shareItem: AdvancedSharedItem;
-  isItemSelected: boolean;
-}) => (
-  <span className={`${isItemSelected ? 'text-gray-100' : 'text-gray-60'}`}>
-    {dateService.format(shareItem.createdAt, 'D MMM YYYY')}
-  </span>
-);
