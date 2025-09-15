ef453b8536ce62f24ecaf033a233057546fea384
Xavier Abad <xabadgo01@gmail.com>
2025-09-15 09:48:33 +0200

    WIP (using the Staging URL to check if it works)

--- src/app/auth/services/vpnAuth.service.test.ts
+++ src/app/auth/services/vpnAuth.service.test.ts
@@ -3,7 +3,7 @@ import { describe, it, expect, afterAll, beforeEach, afterEach, vi } from 'vites
 import { logIn, logOut, postMessageToVpn } from './vpnAuth.service';
 import envService from 'app/core/services/env.service';
 
-const mockHostname = 'https://example.com';
+const mockHostname = 'https://fix-vpn-login.drive-web.pages.dev';
 
 describe('Tests for VPN auth', () => {
   const originalPostMessage = window.postMessage;
--- src/app/auth/services/vpnAuth.service.ts
+++ src/app/auth/services/vpnAuth.service.ts
@@ -2,7 +2,7 @@ import envService from 'app/core/services/env.service';
 
 export const postMessageToVpn = (payload: Record<string, any>, source = 'drive-web') => {
   const targetUrl = envService.getVariable('hostname');
-  window.postMessage({ source: source, payload }, targetUrl);
+  window.postMessage({ source: source, payload }, 'https://fix-vpn-login.drive-web.pages.dev');
 };
 
 export const logIn = (userToken: string) => {
