56b8385ed96f467a3f34a626b704d9966d3001e5
Xavier Abad <xabadgo01@gmail.com>
2025-10-21 09:00:42 +0200

    feat: improve the creation of chunks to download

--- src/app/utils/networkUtils.ts
+++ src/app/utils/networkUtils.ts
@@ -4,22 +4,40 @@ export class NetworkUtils {
   public static readonly instance: NetworkUtils = new NetworkUtils();
 
   public readonly createDownloadChunks = (fileSize: number, chunkSize: number, maxChunkRetires: number) => {
-    let start = 0;
-    let chunkIndex = 0;
-    const chunks: Array<DownloadChunkTask> = [];
+    const chunks: DownloadChunkTask[] = [];
+    let pos = 0;
+    let index = 0;
 
-    // Split the file into chunks
-    while (start < fileSize) {
-      const end = Math.min(start + chunkSize - 1, fileSize - 1);
+    // Start with small chunks so the first few chunks are downloaded quickly and the UI is updated
+    const initialSmallChunks = 8;
+    for (let i = 1; i <= initialSmallChunks && pos < fileSize; i++) {
+      const size = i * 128 * 1024;
+      const end = Math.min(pos + size, fileSize);
+
       chunks.push({
-        index: chunkIndex,
-        chunkStart: start,
-        chunkEnd: end,
+        index: index++,
+        chunkStart: pos,
+        chunkEnd: end - 1,
         attempt: 0,
         maxRetries: maxChunkRetires,
       });
-      start = end + 1;
-      chunkIndex++;
+
+      pos = end;
+    }
+
+    // Then, download the rest of the chunks with the normal chunk size
+    while (pos < fileSize) {
+      const end = Math.min(pos + chunkSize, fileSize);
+
+      chunks.push({
+        index: index++,
+        chunkStart: pos,
+        chunkEnd: end - 1,
+        attempt: 0,
+        maxRetries: maxChunkRetires,
+      });
+
+      pos = end;
     }
 
     return chunks;
