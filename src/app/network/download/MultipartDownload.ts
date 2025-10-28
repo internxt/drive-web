e6c4f004160cb62b04fede04de007b6728448f0f
Xavier Abad <xabadgo01@gmail.com>
2025-10-28 12:39:35 +0100

    fix: remove useless assertion

--- src/app/network/download/MultipartDownload.ts
+++ src/app/network/download/MultipartDownload.ts
@@ -251,7 +251,7 @@ export class MultipartDownload {
       while (true) {
         const { done, value } = await reader.read();
         if (done) return;
-        yield value!;
+        yield value;
       }
     } finally {
       reader.releaseLock();
