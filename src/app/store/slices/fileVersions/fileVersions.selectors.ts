3c1d8f9a5227d984d1820a5a3e1fb1c5d1f04f8c
Francis Terrero <terrero0611@gmail.com>
2025-12-17 12:15:06 -0400

    refactor: remove unused selectors from fileVersionsSelectors

--- src/app/store/slices/fileVersions/fileVersions.selectors.ts
+++ src/app/store/slices/fileVersions/fileVersions.selectors.ts
@@ -5,17 +5,11 @@ const fileVersionsSelectors = {
   getLimits(state: RootState): FileLimitsResponse | null {
     return state.fileVersions.limits;
   },
-  isLimitsLoading(state: RootState): boolean {
-    return state.fileVersions.isLimitsLoading;
-  },
   getVersionsByFileId(state: RootState, fileId: NonNullable<FileVersion['fileId']>): FileVersion[] | undefined {
     return state.fileVersions.versionsByFileId[fileId];
   },
   isLoadingByFileId(state: RootState, fileId: NonNullable<FileVersion['fileId']>): boolean {
     return state.fileVersions.isLoadingByFileId[fileId] ?? false;
-  },
-  getErrorByFileId(state: RootState, fileId: NonNullable<FileVersion['fileId']>): string | null | undefined {
-    return state.fileVersions.errorsByFileId[fileId];
   },
 };
 
