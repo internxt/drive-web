5e0da13374af16bbf383b19b68b2d021ddc459d9
Xavier Abad <xabadgo01@gmail.com>
2026-01-09 13:00:56 +0100

    fix: prevent default extension for files without it

--- public/streamsaver/stream-saver.js
+++ public/streamsaver/stream-saver.js
@@ -80,6 +80,7 @@ self.onfetch = (event) => {
 
   const responseHeaders = new Headers({
     'Content-Type': 'application/octet-stream; charset=utf-8',
+    DefaultType: 'None',
     'Content-Security-Policy': "default-src 'none'",
     'X-Content-Security-Policy': "default-src 'none'",
     'X-WebKit-CSP': "default-src 'none'",
