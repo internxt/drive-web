1a1652ab42efe225c54c997dcebfbcd5177b3c66
Xavier Abad <xabadgo01@gmail.com>
2026-04-29 09:58:09 +0200

    fix: dialog styles

--- src/app/drive/components/DownloadBackupKeysDialog/index.tsx
+++ src/app/drive/components/DownloadBackupKeysDialog/index.tsx
@@ -24,31 +24,32 @@ export const DownloadBackupKeysDialog = () => {
       title={translate('modals.downloadBackupKeys.title')}
       dialogRounded={true}
       panelClasses="w-screen max-w-lg"
-      titleClasses="font-medium text-left"
-      closeClass="shrink-0 flex items-center justify-center h-10 w-10 hover:bg-black/2 rounded-md focus:bg-black/5"
+      titleClasses="font-medium pt-1.5 text-left"
+      closeClass="self-center shrink-0 flex items-center justify-center h-10 w-10 hover:bg-black/2 rounded-md focus:bg-black/5"
       onClose={onCloseButtonClicked}
       weightIcon="light"
       dataTest="backup-keys-dialog"
     >
-      <div className="flex flex-col w-full p-5 gap-5">
+      <div className="flex flex-col w-full p-5 pt-2 gap-5">
         <p className="text-gray-80">{translate('modals.downloadBackupKeys.description')}</p>
 
-        {/* Download backup key */}
+        {/* Download backup key + info */}
         <div className="flex flex-col items-center">
-          <Button variant="secondary" onClick={onDownloadBackupKeysButtonClicked}>
-            {translate('modals.downloadBackupKeys.download')}
-            <DownloadSimple size={24} className="ml-2" />
-          </Button>
-        </div>
-
-        {/* Info */}
-        <div className="flex flex-row items-center justify-center gap-3">
-          <Info size={24} className="text-primary" />
-          <div className="flex flex-col">
-            <p className="text-gray-60">{translate('modals.downloadBackupKeys.info.text')}</p>
-            <p className="text-gray-60 font-semibold">{translate('modals.downloadBackupKeys.info.path')}</p>
+          <div className="flex flex-col w-fit items-stretch gap-5">
+            <Button variant="secondary" className="w-full" onClick={onDownloadBackupKeysButtonClicked}>
+              {translate('modals.downloadBackupKeys.download')}
+              <DownloadSimple size={24} className="ml-2" />
+            </Button>
+            <div className="flex flex-row items-center gap-1">
+              <Info size={24} className="text-primary" />
+              <div className="flex flex-col">
+                <p className="text-gray-60 text-sm">{translate('modals.downloadBackupKeys.info.text')}</p>
+                <p className="text-gray-60 text-sm font-semibold">{translate('modals.downloadBackupKeys.info.path')}</p>
+              </div>
+            </div>
           </div>
         </div>
+
         <div className="flex flex-row gap-3 items-center justify-end pt-5">
           <Button variant="secondary" onClick={onRemindMeLaterButtonClicked}>
             {translate('actions.remindMeLater')}
