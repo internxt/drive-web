803eb66afb44cf0d59dd3d744e301e617e53b494
jaaaaavier <javipiagui13@gmail.com>
2026-05-07 16:51:11 +0200

    feat: add dynamic default fallback for storage plans

--- src/views/Checkout/components/CheckoutProductCard.tsx
+++ src/views/Checkout/components/CheckoutProductCard.tsx
@@ -79,11 +79,18 @@ export const CheckoutProductCard = ({
 
   const planType = isBusiness ? 'businessPlanFeaturesList' : 'planFeaturesList';
 
-  const productLabel = translate(`preferences.account.plans.${planType}.${bytes}.title`) ?? bytes;
-  const featureKeys =
-    translateList(`preferences.account.plans.${planType}.${bytes ?? 'freeFeatures'}.features`, {
-      returnObjects: true,
-    }) ?? translateList('preferences.account.plans.planFeaturesList.1GB.features');
+  const specificTitleKey = `preferences.account.plans.${planType}.${bytes}.title`;
+  const productLabel =
+    translate(specificTitleKey) !== specificTitleKey
+      ? translate(specificTitleKey)
+      : translate('preferences.account.plans.planFeaturesList.default.bytesTitle', { bytes });
+
+  const specificFeatures = translateList(`preferences.account.plans.${planType}.${bytes}.features`, {
+    returnObjects: true,
+  });
+  const featureKeys = Array.isArray(specificFeatures)
+    ? specificFeatures
+    : translateList('preferences.account.plans.planFeaturesList.default.features', { bytes, returnObjects: true });
 
   const featuresList = Array.isArray(
     translateList(`preferences.account.plans.${planType}.${bytes}.comingSoonFeatures`),
