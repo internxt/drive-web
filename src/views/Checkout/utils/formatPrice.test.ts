516220a05033557d98089a1e6be96b446765c239
jaaaaavier <javipiagui13@gmail.com>
2026-05-29 09:17:15 +0200

    update test

--- src/views/Checkout/utils/formatPrice.test.ts
+++ src/views/Checkout/utils/formatPrice.test.ts
@@ -6,26 +6,22 @@ describe('Formatting the price to have 2 decimals', () => {
     expect(formatPrice(10)).toBe('10');
   });
 
-  describe('The value has less or exactly 2 decimals', () => {
-    it('When the user has a price with 1 decimal, the function returns 2 decimals (100.5 -> 100.50)', () => {
-      expect(formatPrice(100.5)).toBe('100.50');
-    });
+  it('When the user has a price with 1 decimal, the function returns 2 decimals (100.5 -> 100.50)', () => {
+    expect(formatPrice(100.5)).toBe('100.50');
+  });
 
-    it('When the user has a price with 2 decimals, the function returns 2 decimals (99.99 -> 99.99)', () => {
-      expect(formatPrice(99.99)).toBe('99.99');
-    });
+  it('When the user has a price with 2 decimals, the function returns 2 decimals (99.99 -> 99.99)', () => {
+    expect(formatPrice(99.99)).toBe('99.99');
   });
 
   it('When the price is just 1 decimal and it is a 0, then the function returns without .00', () => {
     expect(formatPrice(20.0)).toBe('20');
   });
 
-  describe('The price has more than 2 decimals', () => {
-    it('When the price has more than 2 decimals, then the function returns the price rounded to 2 decimals (10.456 -> 10.46 - 10.001 -> 10 - 1.999 -> 2)', () => {
-      expect(formatPrice(10.456)).toBe('10.46');
-      expect(formatPrice(10.001)).toBe('10');
-      expect(formatPrice(1.999)).toBe('2');
-    });
+  test('When the price has more than 2 decimals, then the function returns the price rounded to 2 decimals (10.456 -> 10.46 - 10.001 -> 10 - 1.999 -> 2)', () => {
+    expect(formatPrice(10.456)).toBe('10.46');
+    expect(formatPrice(10.001)).toBe('10');
+    expect(formatPrice(1.999)).toBe('2');
   });
 
   test('When the value is nearly integer due to float error, then it returns without decimals', () => {
