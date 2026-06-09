b1e7d3ec6ee20dd75d1dac29e84a4ce5744c0258
jaaaaavier <javipiagui13@gmail.com>
2026-06-09 12:21:25 +0200

    Update loginPage.ts

--- test/e2e/tests/pages/loginPage.ts
+++ test/e2e/tests/pages/loginPage.ts
@@ -24,8 +24,8 @@ export class LoginPage {
   constructor(page: Page) {
     this.page = page;
     this.loginTitle = this.page.getByRole('heading', { name: 'Log in' });
-    this.emailInput = this.page.getByPlaceholder('Email address', { exact: true });
-    this.passwordInput = this.page.getByPlaceholder('Password', { exact: true });
+    this.emailInput = this.page.locator('[data-cy="emailInput"]');
+    this.passwordInput = this.page.locator('[data-cy="passwordInput"]');
     this.loginButton = this.page.getByRole('button', { name: 'Log in' });
     this.loginButtonText = this.page.locator('[data-cy="loginButton"] div');
     this.forgotPassword = this.page.getByText('I forgot my password');
@@ -47,13 +47,10 @@ export class LoginPage {
 
   async typeEmail(user: string | any) {
     await expect(this.emailInput).toBeVisible();
-    const emailPlaceholder = await this.emailInput.getAttribute('placeholder');
-    expect(emailPlaceholder).toEqual('Email address');
     await this.emailInput.fill(user);
   }
   async typePassword(password: string | any) {
-    const passPlaceholder = await this.passwordInput.getAttribute('placeholder');
-    expect(passPlaceholder).toEqual('Password');
+    await expect(this.passwordInput).toBeVisible();
     await this.passwordInput.fill(password);
   }
   async clickLogIn() {
