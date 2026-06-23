197652566da03488a58fb80248f2e45897b7fbb7
jaaaaavier <javipiagui13@gmail.com>
2026-06-23 12:02:17 +0200

    disable alert for unused var

--- src/app/drive/components/FileViewer/viewers/FileTxtViewer/FileTxtViewer.tsx
+++ src/app/drive/components/FileViewer/viewers/FileTxtViewer/FileTxtViewer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { FormatFileViewerProps } from '../../FileViewer';
 
