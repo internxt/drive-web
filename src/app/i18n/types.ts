f653ec44120e296bddddaf41a97ab15b8b4078c6
Xavier Abad <xabadgo01@gmail.com>
2026-07-07 13:27:29 +0200

    fix: remove useless text and add i18n validation

--- src/app/i18n/types.ts
+++ src/app/i18n/types.ts
@@ -1,5 +1,16 @@
-export type Translate = (key: string, props?: Record<string, unknown>) => string;
-export type TranslateArray = (key: string, props?: Record<string, unknown>) => string[];
+import en from './locales/en.json';
+
+type DotNotation<T, Prefix extends string = ''> = {
+  [K in keyof T]: T[K] extends string
+    ? `${Prefix}${K & string}`
+    : T[K] extends string[]
+      ? `${Prefix}${K & string}`
+      : DotNotation<T[K], `${Prefix}${K & string}.`>;
+}[keyof T];
+
+export type TranslationKey = DotNotation<typeof en> | string;
+export type Translate = (key: TranslationKey, props?: Record<string, unknown>) => string;
+export type TranslateArray = (key: TranslationKey, props?: Record<string, unknown>) => string[];
 
 export enum Locale {
   German = 'de',
