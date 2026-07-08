03c0cc39e5297b5abb16f8dfb1714f60488d4938
tamarafinogina <tamara@internxt.com>
2026-07-08 17:34:29 +0200

    fix encrypted storage test

--- src/services/encrypted-storage.service.test.ts
+++ src/services/encrypted-storage.service.test.ts
@@ -1,45 +1,37 @@
-import { afterAll, beforeEach, describe, expect, it, vi } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { LocalStorageProtectedItem } from 'app/core/types';
 import encryptedStorageService from './encrypted-storage.service';
 
 beforeEach(() => {
-  vi.clearAllMocks();
-  vi.resetModules();
-  localStorage.clear();
-});
-
-afterAll(() => {
+  vi.restoreAllMocks();
   localStorage.clear();
 });
 
 describe('Testing the local storage service', () => {
   describe('Get and set encrypted values', () => {
+    const value = 'test-value';
+    const key = LocalStorageProtectedItem.EncryptedToken;
+
     it('When sets protected value, then the value is stored encrypted', async () => {
       const setFromLocalStorageSpy = vi.spyOn(Storage.prototype, 'setItem');
       const cryptoSpy = vi.spyOn(window.crypto.subtle, 'encrypt');
 
-      const key = LocalStorageProtectedItem.EncryptedToken;
-      const value = 'test-value';
       await encryptedStorageService.setToken(value);
-
       const localStorageItem = localStorage.getItem(key);
 
-      expect(setFromLocalStorageSpy).toHaveBeenCalled();
       expect(cryptoSpy).toHaveBeenCalled();
       expect(setFromLocalStorageSpy).toHaveBeenCalledWith(key, expect.any(String));
       expect(localStorageItem).not.toEqual(value);
     });
 
-    it('When gets a protected value, then the result is decrypted', async () => {
-      const cryptoSpy = vi.spyOn(window.crypto.subtle, 'encrypt');
-
-      const value = 'test-value';
+    it('When hydrates encrypted storage, then the result is decrypted', async () => {
+      localStorage.clear();
       await encryptedStorageService.setToken(value);
+      const cryptoSpy = vi.spyOn(window.crypto.subtle, 'decrypt');
 
-      const localStorageItem = await encryptedStorageService.getToken();
+      await encryptedStorageService.hydrateEncryptedStorageCache();
 
       expect(cryptoSpy).toHaveBeenCalled();
-      expect(localStorageItem).toBe(value);
     });
   });
 });
