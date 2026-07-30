abab43c4e08daf62cb9cd568ef6e8f98cb6cfff9
victor-ferro <victorfernandezrobles.dev@gmail.com>
2026-07-30 18:32:48 +0200

    fix: apply sort by to the favorites list

--- src/views/Favorites/services/fetchFavorites.service.ts
+++ src/views/Favorites/services/fetchFavorites.service.ts
@@ -2,18 +2,31 @@ import { StorageTypes } from '@internxt/sdk/dist/drive';
 
 import { SdkFactory } from 'app/core/factory/sdk';
 
-export const fetchFavoriteFolders = (limit: number, offset: number): Promise<StorageTypes.FavoriteFolderDto[]> => {
+export interface FavoritesSortOptions {
+  sort: 'plainName' | 'updatedAt';
+  order: 'ASC' | 'DESC';
+}
+
+export const fetchFavoriteFolders = (
+  limit: number,
+  offset: number,
+  sortOptions?: FavoritesSortOptions,
+): Promise<StorageTypes.FavoriteFolderDto[]> => {
   const storageClient = SdkFactory.getNewApiInstance().createNewStorageClient();
 
-  const [foldersPromise] = storageClient.getFavorites('folder', { limit, offset });
+  const [foldersPromise] = storageClient.getFavorites('folder', { limit, offset, ...sortOptions });
 
   return foldersPromise;
 };
 
-export const fetchFavoriteFiles = (limit: number, offset: number): Promise<StorageTypes.FavoriteFileDto[]> => {
+export const fetchFavoriteFiles = (
+  limit: number,
+  offset: number,
+  sortOptions?: FavoritesSortOptions,
+): Promise<StorageTypes.FavoriteFileDto[]> => {
   const storageClient = SdkFactory.getNewApiInstance().createNewStorageClient();
 
-  const [filesPromise] = storageClient.getFavorites('file', { limit, offset });
+  const [filesPromise] = storageClient.getFavorites('file', { limit, offset, ...sortOptions });
 
   return filesPromise;
 };
