b97af952b71efc9babc5d9b6256d5a6a9af313bb
tamarafinogina <tamara@internxt.com>
2026-08-03 16:44:05 +0200

    remove unused import

--- src/app/drive/components/ItemDetailsDialog/ItemDetailsDialog.tsx
+++ src/app/drive/components/ItemDetailsDialog/ItemDetailsDialog.tsx
@@ -9,7 +9,6 @@ import { useTranslationContext } from 'app/i18n/provider/TranslationProvider';
 import iconService from 'app/drive/services/icon.service';
 import { Button, Modal } from '@internxt/ui';
 import { bytesToString } from 'app/drive/services/size.service';
-import localStorageService from 'services/local-storage.service';
 import { DriveItemData, DriveItemDetails, ItemDetailsProps } from 'app/drive/types';
 import newStorageService from 'app/drive/services/new-storage.service';
 import errorService from 'services/error.service';
