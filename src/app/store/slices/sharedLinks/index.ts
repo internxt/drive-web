a58db07cfa4141717aade508b3a168f50edc1a4c
tamarafinogina <tamara@internxt.com>
2026-08-20 10:02:51 +0200

    up the version

--- src/app/store/slices/sharedLinks/index.ts
+++ src/app/store/slices/sharedLinks/index.ts
@@ -13,8 +13,7 @@ import { t } from 'i18next';
 import userService from 'services/user.service';
 import { encryptBucketKey } from 'app/share/services/share.crypto';
 
-export const HYBRID_ALGORITHM = 'hybrid';
-export const HYBRID_ALGORITHM_WITH_BUCKET_KEY = 'hybrid';
+export const HYBRID_ALGORITHM_WITH_BUCKET_KEY = 'hybrid-v2';
 export const STANDARD_ALGORITHM = 'ed25519';
 
 export interface ShareLinksState {
